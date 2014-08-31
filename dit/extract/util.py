7ed31c88feffd3cf6e57ca03be72b92b18f5d6a0
Friedrich Lindenberg <friedrich@pudo.org>
2014-08-31 12:22:04 +0200

    Yeah, no - that was too traditional.

--- dit/extract/util.py
+++
@@ -1,9 +0,0 @@
-from os import path
-
-
-def file_empty(file_name):
-    if not path.exists(file_name):
-        return True
-    with open(file_name, 'rb') as fh:
-        fh.seek(0, 2)
-        return fh.tell() < 10
