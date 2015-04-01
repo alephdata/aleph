590e9707996def0a9bca051580ce447ac01bff64
Friedrich Lindenberg <friedrich@pudo.org>
2015-04-01 14:25:49 +0200

    get rid of cruft

--- aleph/static/js/ctrl.js
+++ aleph/static/js/ctrl.js
@@ -10,14 +10,6 @@ aleph.controller('AppCtrl', ['$scope', '$rootScope', '$location', '$route', '$ht
     $scope.queryContext = context;
   });
 
-  // $scope.showListFacet = function(id) {
-  //   return Query.load().listfacet.indexOf(id) == -1;
-  // };
-
-  // $scope.showAttributeFacet = function(attr) {
-  //   return Query.load().attributefacet.indexOf(attr) == -1;
-  // };
-
   Session.get(function(session) {
     $scope.session = session;
   });
