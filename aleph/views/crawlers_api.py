fab3eb93450f844e0c6fddc6f1843a9fbe04c12c
Friedrich Lindenberg <friedrich@pudo.org>
2015-12-19 21:24:54 +0100

    Remove broken doccloud support.

--- aleph/views/crawlers_api.py
+++ aleph/views/crawlers_api.py
@@ -1,11 +1,8 @@
-from flask import Blueprint, request
-from apikit import jsonify, obj_or_404
+from flask import Blueprint
+from apikit import jsonify
 
-from aleph import authz
-from aleph.model import Source
 from aleph.views.cache import etag_cache_keygen
 from aleph.crawlers import get_crawlers
-from aleph.crawlers.doccloud import DocumentCloudCrawler
 
 
 blueprint = Blueprint('crawlers', __name__)
@@ -22,20 +19,3 @@ def index():
         })
     etag_cache_keygen([c['name'] for c in crawlers])
     return jsonify({'results': crawlers, 'total': len(crawlers)})
-
-
-@blueprint.route('/api/1/crawlers/dc_projects')
-def dc_projects():
-    slug = request.args.get('source')
-    authz.require(authz.source_read(slug))
-    source = obj_or_404(Source.by_slug(slug))
-    if not isinstance(source.crawler_instance, DocumentCloudCrawler):
-        return jsonify({'credentials': False})
-    username = request.args.get('username')
-    password = request.args.get('password')
-    projects = source.crawler_instance.get_projects(username, password)
-    if projects is False:
-        return jsonify({'credentials': False})
-    else:
-        return jsonify({'credentials': True,
-                        'projects': projects})
