288e07e136bcd235cae5317c5d00e17d34131ea4
Friedrich Lindenberg <friedrich@pudo.org>
2015-12-27 15:52:30 +0100

    Re-analysis of documents backend.

--- aleph/analyze/__init__.py
+++ aleph/analyze/__init__.py
@@ -1,9 +1,11 @@
+import json
 import logging
 
 from aleph.core import celery
 from aleph.ext import get_analyzers
 from aleph.model import Document
 from aleph.index import index_document
+from aleph.search import raw_iter, construct_query
 
 
 log = logging.getLogger(__name__)
@@ -11,12 +13,24 @@ log = logging.getLogger(__name__)
 
 @celery.task()
 def analyze_source(source_id):
-    pass
+    query = {'term': {'source_id': source_id}}
+    analyze_matches(query)
+
+
+@celery.task()
+def analyze_terms(terms):
+    q = ' OR '.join(map(json.dumps, terms))
+    q = construct_query({'q': q})
+    analyze_matches(q)
 
 
 @celery.task()
 def analyze_matches(query):
-    pass
+    if 'query' not in query:
+        query = {'query': query}
+    query['_source'] = []
+    for row in raw_iter(query):
+        analyze_document.delay(row.get('_id'))
 
 
 @celery.task()
