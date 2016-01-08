c15b7479939615949fa66d0a7b4946caf363c668
Friedrich Lindenberg <friedrich@pudo.org>
2016-01-08 14:08:36 +0100

    Delete existing Watchlist editor functionality.

--- aleph/static/js/config.js
+++ aleph/static/js/config.js
@@ -36,25 +36,6 @@ aleph.config(['$routeProvider', '$locationProvider', 'cfpLoadingBarProvider',
     }
   });
 
-  $routeProvider.when('/watchlists/new', {
-    templateUrl: 'watchlists_new.html',
-    controller: 'WatchlistsNewCtrl',
-    loginRequired: true
-  });
-
-  $routeProvider.when('/watchlists/:id', {
-    templateUrl: 'watchlists_edit.html',
-    controller: 'WatchlistsEditCtrl',
-    loginRequired: true
-  });
-
-  $routeProvider.when('/watchlists/:id/entities', {
-    templateUrl: 'watchlists_entities.html',
-    controller: 'WatchlistsEntitiesCtrl',
-    reloadOnSearch: false,
-    loginRequired: true
-  });
-
   $routeProvider.otherwise({
     redirectTo: '/search',
     loginRequired: false
