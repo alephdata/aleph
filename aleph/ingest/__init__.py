9d7053d7c61d97a094ba4e16e38e0d6ec205420b
Friedrich Lindenberg <friedrich@pudo.org>
2016-01-24 21:01:20 +0100

    handle exceptions

--- aleph/ingest/__init__.py
+++ aleph/ingest/__init__.py
@@ -10,16 +10,6 @@ from aleph.ingest.ingestor import Ingestor
 
 log = logging.getLogger(__name__)
 
-# dispatch
-#   URLIngestor / URLCrawler
-#   RarFileIngestor
-#   ZipFileIngestor
-#   OCRIngestor
-#   PDFIngestor
-#   WordIngestor
-#   HTMLIngestor
-#   TableIngestor
-#
 # https://bugzilla.redhat.com/show_bug.cgi?id=191060#c1
 # https://github.com/deanmalmgren/textract/blob/master/textract/parsers/pptx_parser.py
 # https://github.com/chardet/chardet
@@ -27,7 +17,6 @@ log = logging.getLogger(__name__)
 # http://www.unixuser.org/~euske/python/pdfminer/index.html
 # https://mstamy2.github.io/PyPDF2/#documentation
 # http://pybrary.net/pyPdf/pythondoc-pyPdf.pdf.html
-#
 # https://svn.apache.org/viewvc/httpd/httpd/branches/2.2.x/docs/conf/mime.types?view=annotate
 
 
@@ -35,22 +24,19 @@ log = logging.getLogger(__name__)
 def ingest_url(source_id, metadata, url):
     clear_session()
     meta = Metadata(data=metadata)
-    try:
-        with NamedTemporaryFile() as fh:
-            log.info("Ingesting URL: %r", url)
-            res = requests.get(url, stream=True)
-            meta.source_url = res.url
-            for chunk in res.iter_content(chunk_size=1024):
-                if chunk:
-                    fh.write(chunk)
-            fh.flush()
+    with NamedTemporaryFile() as fh:
+        log.info("Ingesting URL: %r", url)
+        res = requests.get(url, stream=True)
+        meta.source_url = res.url
+        for chunk in res.iter_content(chunk_size=1024):
+            if chunk:
+                fh.write(chunk)
+        fh.flush()
 
-            if not meta.has('file_name'):
-                meta.file_name = meta.file_name
-            meta.headers = res.headers
-            ingest_file(source_id, meta, fh.name)
-    except Exception as ex:
-        log.exception(ex)
+        if not meta.has('file_name'):
+            meta.file_name = meta.file_name
+        meta.headers = res.headers
+        ingest_file(source_id, meta, fh.name)
 
 
 def ingest_file(source_id, meta, file_name, move=False):
@@ -66,7 +52,4 @@ def ingest_file(source_id, meta, file_name, move=False):
 def ingest(source_id, metadata):
     clear_session()
     meta = Metadata(data=metadata)
-    try:
-        Ingestor.dispatch(source_id, meta)
-    except Exception as ex:
-        log.exception(ex)
+    Ingestor.dispatch(source_id, meta)
