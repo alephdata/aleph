c017a34ce11b4ccde151822550d36d32ec9929d4
Friedrich Lindenberg <friedrich@pudo.org>
2016-01-31 20:35:08 +0100

    make web crawler ready for a test run

--- aleph/crawlers/web.py
+++ aleph/crawlers/web.py
@@ -2,6 +2,7 @@ import os
 import cgi
 import yaml
 import logging
+from lxml import html
 from tempfile import mkstemp
 from krauler import Krauler
 
@@ -17,20 +18,30 @@ class AlephKrauler(Krauler):
         self.source = source
         super(AlephKrauler, self).__init__(config)
 
+    def get_content(self, page):
+        if not page.is_html:
+            return page.content
+
+        for path in self.config.get('remove_paths', []):
+            for el in page.doc.findall(path):
+                el.drop_tree()
+
+        return html.tostring(page.doc)
+
     def emit(self, page):
         meta = self.crawler.metadata()
         meta.data.update(self.config.get('meta', {}))
         meta.source_url = page.normalized_url
         meta.foreign_id = page.id
+        if page.file_name:
+            meta.file_name = page.file_name
+        meta.mime_type = page.mime_type
         meta.headers = page.response.headers
-
-        from pprint import pprint
-        pprint(meta.to_dict())
 
         fh, file_path = mkstemp(suffix='.%s' % meta.extension)
         try:
             fh = os.fdopen(fh, 'w')
-            fh.write(page.response.content)
+            fh.write(self.get_content(page))
             fh.close()
             self.crawler.emit_file(self.source, meta,
                                    file_path, move=True)
