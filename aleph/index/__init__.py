b211b9a12555a1d4e92811610cfd10b91734997e
Friedrich Lindenberg <friedrich@pudo.org>
2016-02-16 20:35:35 +0100

    fail more openly when indexing isnt successful

--- aleph/index/__init__.py
+++ aleph/index/__init__.py
@@ -168,13 +168,10 @@ def index_document(document_id):
              id=document.id)
     clear_children(document)
 
-    try:
-        if document.type == Document.TYPE_TEXT:
-            bulk(es, generate_pages(document), stats_only=True,
-                 chunk_size=2000, request_timeout=60.0)
+    if document.type == Document.TYPE_TEXT:
+        bulk(es, generate_pages(document), stats_only=True,
+             chunk_size=2000, request_timeout=60.0)
 
-        if document.type == Document.TYPE_TABULAR:
-            bulk(es, generate_records(document), stats_only=True,
-                 chunk_size=2000, request_timeout=60.0)
-    except Exception as ex:
-        log.exception(ex)
+    if document.type == Document.TYPE_TABULAR:
+        bulk(es, generate_records(document), stats_only=True,
+             chunk_size=2000, request_timeout=60.0)
