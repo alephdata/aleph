7093b50ffab8c37eae3e665ea41cd6a75fd7dc00
Friedrich Lindenberg <friedrich@pudo.org>
2016-02-23 12:48:50 +0100

    pin requirement

--- aleph/__init__.py
+++ aleph/__init__.py
@@ -1,11 +1,12 @@
 import os
 import logging
+from sqlalchemy.exc import SAWarning
 
 # shut up useless SA warning:
 import warnings
 warnings.filterwarnings('ignore',
                         'Unicode type received non-unicode bind param value.')
-from sqlalchemy.exc import SAWarning
+
 warnings.filterwarnings('ignore', category=SAWarning)
 
 # loggers.
