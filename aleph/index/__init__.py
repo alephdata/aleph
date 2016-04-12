1ed79df097532ec48d2b17868c18c4a5fd34fd35
Friedrich Lindenberg <friedrich@pudo.org>
2016-04-12 12:49:04 +0300

    Refactor indexing code into multiple files.

--- aleph/index/__init__.py
+++ aleph/index/__init__.py
@@ -1,64 +1,16 @@
 import logging
-from hashlib import sha1
 from elasticsearch.helpers import bulk, scan
 
 from aleph.core import celery, get_es, get_es_index
 from aleph.model import Document
 from aleph.util import latinize_text
+from aleph.index.admin import init_search, upgrade_search  # noqa
+from aleph.index.admin import delete_index, optimize_search  # noqa
+from aleph.index.records import generate_records, clear_records
 from aleph.index.mapping import TYPE_DOCUMENT, TYPE_RECORD
-from aleph.index.mapping import DOCUMENT_MAPPING, RECORD_MAPPING
+from aleph.index.mapping import DOCUMENT_MAPPING, RECORD_MAPPING  # noqa
 
 log = logging.getLogger(__name__)
-
-
-def init_search():
-    log.info("Creating ElasticSearch index and uploading mapping...")
-    get_es().indices.create(get_es_index(), body={
-        'mappings': {
-            TYPE_DOCUMENT: DOCUMENT_MAPPING,
-            TYPE_RECORD: RECORD_MAPPING
-        }
-    })
-    get_es().indices.open(index=get_es_index())
-
-
-def upgrade_search():
-    """Add any missing properties to the index mappings."""
-    get_es().indices.put_mapping(index=get_es_index(), body=DOCUMENT_MAPPING,
-                                 doc_type=TYPE_DOCUMENT)
-    get_es().indices.put_mapping(index=get_es_index(), body=RECORD_MAPPING,
-                                 doc_type=TYPE_RECORD)
-
-
-def optimize_search():
-    get_es().indices.optimize(index=get_es_index())
-
-
-def delete_index():
-    get_es().indices.delete(get_es_index(), ignore=[404])
-
-
-def clear_children(document):
-    """Delete all records associated with the given document."""
-    q = {'query': {'term': {'document_id': document.id}},
-         '_source': ['_id', 'document_id']}
-
-    def gen_deletes():
-            for res in scan(get_es(), query=q, index=get_es_index(),
-                            doc_type=[TYPE_RECORD]):
-                yield {
-                    '_op_type': 'delete',
-                    '_index': get_es_index(),
-                    '_parent': res.get('_source', {}).get('document_id'),
-                    '_type': res.get('_type'),
-                    '_id': res.get('_id')
-                }
-
-    try:
-        bulk(get_es(), gen_deletes(), stats_only=True, chunk_size=2000,
-             request_timeout=60.0)
-    except Exception as ex:
-        log.exception(ex)
 
 
 def delete_source(source_id):
@@ -94,52 +46,6 @@ def delete_source(source_id):
         log.exception(ex)
 
 
-def generate_pages(document):
-    for page in document.pages:
-        tid = sha1(str(document.id))
-        tid.update(str(page.id))
-        tid = tid.hexdigest()
-        yield {
-            '_id': tid,
-            '_type': TYPE_RECORD,
-            '_index': get_es_index(),
-            '_parent': document.id,
-            '_source': {
-                'type': 'page',
-                'content_hash': document.content_hash,
-                'document_id': document.id,
-                'source_id': document.source_id,
-                'page': page.number,
-                'text': page.text,
-                'text_latin': latinize_text(page.text)
-            }
-        }
-
-
-def generate_records(document):
-    for record in document.records:
-        text = record.text
-        latin = [latinize_text(t) for t in text]
-        latin = [t for t in latin if t not in text]
-        yield {
-            '_id': record.tid,
-            '_type': TYPE_RECORD,
-            '_index': get_es_index(),
-            '_parent': unicode(document.id),
-            '_source': {
-                'type': 'row',
-                'content_hash': document.content_hash,
-                'document_id': document.id,
-                'source_id': document.source_id,
-                'row_id': record.row_id,
-                'sheet': record.sheet,
-                'text': text,
-                'text_latin': latin,
-                'raw': record.data
-            }
-        }
-
-
 def generate_entities(document):
     entities = []
     for reference in document.references:
@@ -168,11 +74,6 @@ def index_document(document_id):
     get_es().index(index=get_es_index(), doc_type=TYPE_DOCUMENT, body=data,
                    id=document.id)
 
-    clear_children(document)
-    if document.type == Document.TYPE_TEXT:
-        bulk(get_es(), generate_pages(document), stats_only=True,
-             chunk_size=2000, request_timeout=60.0)
-
-    if document.type == Document.TYPE_TABULAR:
-        bulk(get_es(), generate_records(document), stats_only=True,
-             chunk_size=2000, request_timeout=60.0)
+    clear_records(document)
+    bulk(get_es(), generate_records(document), stats_only=True,
+         chunk_size=2000, request_timeout=60.0)
