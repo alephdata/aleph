9ba45a33acbe93e1d625388d9d2ee35f3b330b1e
Friedrich Lindenberg <friedrich@pudo.org>
2016-05-03 20:58:24 +0200

    simplify again

--- aleph/static/js/services/Authz.js
+++ aleph/static/js/services/Authz.js
@@ -1,22 +1,18 @@
 
 aleph.factory('Authz', ['$rootScope', function($rootScope) {
-  var checkCollection = function(right, collection_id) {
-    if ($rootScope.session.permissions.collections && $rootScope.session.permissions.collections[right]) {
-        return $rootScope.session.permissions.collections[right].indexOf(collection_id) != -1;
+  return {
+    collection: function(right, collection_id) {
+      if ($rootScope.session.permissions.collections && $rootScope.session.permissions.collections[right]) {
+          return $rootScope.session.permissions.collections[right].indexOf(collection_id) != -1;
+        }
+        return false;
+    },
+    source: function(right, source_id) {
+      if ($rootScope.session.permissions.sources && $rootScope.session.permissions.sources[right]) {
+        return $rootScope.session.permissions.sources[right].indexOf(source_id) != -1;
       }
       return false;
-  }
-
-  var checkSource = function(right, source_id) {
-    if ($rootScope.session.permissions.sources && $rootScope.session.permissions.sources[right]) {
-      return $rootScope.session.permissions.sources[right].indexOf(source_id) != -1;
-    }
-    return false;
-  }
-
-  return {
-    collection: checkCollection,
-    source: checkSource,
+    },
     READ: 'read',
     WRITE: 'write'
   }
