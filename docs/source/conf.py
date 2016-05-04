a48474dc8d3a5c2b8899bdfffbb089127dc91d33
Andrew Kamau <andkamau@gmail.com>
2016-05-04 18:01:57 +0300

    mock modules with dependencies - to enable rtd build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['https://github.com/pudo/extractors/tarball/master', 'extractors', 'flanker']
+MOCK_MODULES = ['https://github.com/pudo/extractors/tarball/master', 'extractors', 'flanker', 'tesserwrap']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
