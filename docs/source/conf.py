c938cd075c11fc5c5ea5d140a1140a1eaaebedd6
Andrew Kamau <andkamau@gmail.com>
2016-05-04 17:55:01 +0300

    mock modules with dependencies - to enable rtd build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,6 +16,16 @@ import sys
 import os
 import sphinx_rtd_theme
 
+import sys
+from mock import Mock as MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return Mock()
+
+MOCK_MODULES = ['https://github.com/pudo/extractors/tarball/master', 'extractors', 'flanker']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
 # If extensions (or modules to document with autodoc) are in another directory,
