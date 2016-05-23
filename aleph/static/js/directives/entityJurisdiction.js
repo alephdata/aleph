21ac94bda2c95b50170f3d537f91a9bc73c8485e
Friedrich Lindenberg <friedrich@pudo.org>
2016-05-23 14:32:56 +0200

    Fix up the jurisdiction selector.

--- aleph/static/js/directives/entityJurisdiction.js
+++ aleph/static/js/directives/entityJurisdiction.js
@@ -1,4 +1,25 @@
 aleph.directive('entityJurisdiction', ['Metadata', function(Metadata) {
+  var jurisdictions = [], countries = {};
+
+  Metadata.get().then(function(metadata) {
+    countries = metadata.countries;
+    jurisdictions.push({
+      label: 'No country selected',
+      value: null
+    });
+    for (var value in metadata.countries) {
+      jurisdictions.push({
+        value: value,
+        label: metadata.countries[value]
+      });
+    }
+    jurisdictions = jurisdictions.sort(function(a, b) {
+      if (a.value == null) return -1;
+      if (b.value == null) return 1;
+      return a.label.localeCompare(b.label);
+    });
+  });
+
   return {
     restrict: 'E',
     transclude: true,
@@ -7,25 +28,17 @@ aleph.directive('entityJurisdiction', ['Metadata', function(Metadata) {
     },
     templateUrl: 'templates/entity_jurisdiction.html',
     link: function (scope, element, attrs, model) {
-      scope.jurisdictions = [];
+      scope.getJurisdictions = function() {
+        return jurisdictions;
+      };
 
-      Metadata.get().then(function(metadata) {
-        var jurisdictions = [{
-          label: 'No country selected',
-          value: null
-        }];
-        for (var value in metadata.countries) {
-          jurisdictions.push({
-            value: value,
-            label: metadata.countries[value]
-          });
+      scope.value = function(code) {
+        if (arguments.length) {
+          scope.entity.jurisdiction_code = code;
+        } else {
+          return countries[scope.entity.jurisdiction_code];  
         }
-        scope.jurisdictions = jurisdictions.sort(function(a, b) {
-          if (a.value == null) return -1;
-          if (b.value == null) return 1;
-          return a.label.localeCompare(b.label);
-        });
-      });
+      };
 
       scope.isPerson = function() {
         return scope.entity.$schema == '/entity/person.json#';
