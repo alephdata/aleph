6f95f7d1a5a9195c52e0c7355b4cb69a397630d8
Friedrich Lindenberg <friedrich@pudo.org>
2016-05-29 21:16:30 +0200

    Extract keywords and remove links.

--- aleph/ingest/html.py
+++ aleph/ingest/html.py
@@ -17,8 +17,8 @@ class HtmlIngestor(DocumentIngestor):
     EXTENSIONS = ['html', 'htm', 'asp', 'aspx', 'jsp']
 
     cleaner = Cleaner(scripts=True, javascript=True, style=True, links=True,
-                      embedded=True, forms=True, annoying_tags=True,
-                      meta=False)
+                      embedded=True, forms=True, frames=True,
+                      annoying_tags=True, meta=False, remove_tags=['a'])
 
     def generate_pdf_version(self, html_path):
         """OK, this is weirder. Converting HTML to PDF via WebKit."""
@@ -34,18 +34,26 @@ class HtmlIngestor(DocumentIngestor):
         fh, out_path = mkstemp(suffix='.htm')
         os.close(fh)
         with open(local_path, 'rb') as fh:
-            doc = html.fromstring(fh.read())
-            if not meta.has('title'):
-                title = doc.findtext('.//title')
-                if title is not None:
-                    meta.title = title.strip()
+            data = fh.read()
+        doc = html.fromstring(data)
+        if not meta.has('title'):
+            title = doc.findtext('.//title')
+            if title is not None:
+                meta.title = title.strip()
 
-            if not meta.has('summary'):
-                summary = doc.find('.//meta[@name="description"]')
-                if summary is not None and summary.get('content'):
-                    meta.summary = summary.get('content')
+        if not meta.has('summary'):
+            summary = doc.find('.//meta[@name="description"]')
+            if summary is not None and summary.get('content'):
+                meta.summary = summary.get('content')
 
-            self.cleaner(doc)
+        for field in ['keywords', 'news_keywords']:
+            value = doc.find('.//meta[@name="%s"]' % field)
+            if value is not None:
+                value.get('content') or ''
+                for keyword in value.split(','):
+                    meta.add_keyword(keyword.strip())
+
+        self.cleaner(doc)
         try:
             with open(out_path, 'w') as fh:
                 fh.write(etree.tostring(doc))
