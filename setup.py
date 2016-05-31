2b1146a1741262a9c5acec9a56dfa3e45202f1df
Friedrich Lindenberg <friedrich@pudo.org>
2016-05-31 14:23:24 +0200

    set version 1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aleph',
-    version='0.2',
+    version='1.0',
     description="Document sifting web frontend",
     long_description="",
     classifiers=[
