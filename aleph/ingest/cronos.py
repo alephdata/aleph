154ce51a2b3a160d84145a793638e9a77b18cc99
Friedrich Lindenberg <friedrich@pudo.org>
2016-07-15 16:02:50 +0200

    cronos import hack

--- aleph/ingest/cronos.py
+++ aleph/ingest/cronos.py
@@ -1,12 +1,36 @@
 from __future__ import absolute_import
 import os
 import logging
+import unicodedata
+from collections import defaultdict
 from cronos.parser import parse_structure, parse_data
 
+from aleph.core import db
 from aleph.ingest.bundler import BundleIngestor
 from aleph.ingest.tabular import TabularIngestor
 
 log = logging.getLogger(__name__)
+
+
+def check_row(row):
+    # HACK HACK HACK
+    # In an attempt to find invalid cronos parser results, this
+    # will discard any row in which latin or cyrillic text does
+    # not at least make up 80% of the contents.
+
+    # stats = defaultdict(int)
+    good = 0.0
+    total = 0.0
+    for cell in row:
+        if cell is None or isinstance(cell, (int, float)):
+            continue
+        for char in unicode(cell):
+            total += 1
+            cls = unicodedata.name(char, 'BAD')[:4]
+            if cls in ['CYRI', 'DIGI', 'SPAC', 'LATI', 'LEFT', 'RIGH']:
+                good += 1
+            # stats[cls] += 1
+    return total > 2 and (good / total) > 0.84
 
 
 class CronosIngestor(TabularIngestor, BundleIngestor):
@@ -35,11 +59,11 @@ class CronosIngestor(TabularIngestor, BundleIngestor):
         stru_dat = self.find_file(data_dir, 'CroStru.dat')
         data_tad = self.find_file(data_dir, 'CroBank.tad')
         data_dat = self.find_file(data_dir, 'CroBank.dat')
-        db, tables = parse_structure(os.path.join(data_dir, stru_dat))
+        database, tables = parse_structure(os.path.join(data_dir, stru_dat))
         tables = [t for t in tables if t['abbr'] != 'FL']
         if not len(tables):
             return
-        meta.title = db['BankName']
+        meta.title = database['BankName']
         document = self.create_document(meta)
         tabulars = []
         sheet = 0
@@ -55,14 +79,20 @@ class CronosIngestor(TabularIngestor, BundleIngestor):
                                       os.path.join(data_dir, data_dat),
                                       table.get('id'), columns):
                     table['length'] += 1
-                    yield {c.name: v for c, v in zip(columns, row)}
+                    if check_row(row):
+                        yield {c.name: v for c, v in zip(columns, row)}
 
             document.insert_records(sheet, generate_rows())
             if table['length'] > 0:
                 tabulars.append(tabular)
                 sheet += 1
+            else:
+                log.warning("No valid rows: %s", table['name'])
 
         if len(tabulars):
             meta.tables = tabulars
             document.meta = meta
             self.emit(document)
+        else:
+            log.warning("No vali data: %s", database['BankName'])
+            db.session.rollback()
