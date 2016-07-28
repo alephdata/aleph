6bab45b0d3fe612c567c2aef693101b1440713d3
Friedrich Lindenberg <friedrich@pudo.org>
2016-07-28 09:50:53 +0200

    Analyze documents after collection changes.

--- aleph/logic/documents.py
+++ aleph/logic/documents.py
@@ -1,6 +1,7 @@
 import logging
 
 from aleph.index import index_document
+from aleph.analyze import analyze_document_id
 
 log = logging.getLogger(__name__)
 
@@ -8,4 +9,5 @@ log = logging.getLogger(__name__)
 def update_document(document):
     # These are operations that should be executed after each
     # write to a document or its metadata.
+    analyze_document_id.delay(document.id)
     index_document(document, index_records=False)
