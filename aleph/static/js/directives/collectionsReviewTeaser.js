0d0c6fe2ba1d3d2e2bdaf067ead860692fac31c0
Friedrich Lindenberg <friedrich@pudo.org>
2016-08-24 17:48:23 +0200

    Factor out entity teaser.

---
+++ aleph/static/js/directives/collectionsReviewTeaser.js
@@ -0,0 +1,10 @@
+aleph.directive('collectionsReviewTeaser', [function() {
+  return {
+    restrict: 'E',
+    scope: {
+      'collection': '='
+    },
+    templateUrl: 'templates/collections/review_teaser.html',
+    link: function (scope, element, attrs) {}
+  };
+}]);
