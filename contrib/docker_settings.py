b3c17e3e66874b5cd9d1ee9e8aee0f27e5d106c5
Greg Kempe <greg@kempe.net>
2016-10-06 12:23:10 +0200

    Make peeking a config option

--- contrib/docker_settings.py
+++ contrib/docker_settings.py
@@ -59,3 +59,7 @@ OCR_PDF_PAGES = OCR_PDF_PAGES.strip().lower() == "true"
 
 MAX_CONTENT_LENGTH = int(os.environ.get('ALEPH_MAX_CONTENT_LENGTH',
                                         500 * 1024 * 1024))
+
+# Tell users to email the admins if their search has results in collections
+# that are hidden from them?
+ALLOW_PEEKING = True
