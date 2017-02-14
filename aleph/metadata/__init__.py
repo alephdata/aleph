b7bd92104e16a95837c647cea01ee54b6c8b3f06
Greg Kempe <greg@kempe.net>
2017-02-14 15:06:21 +0200

    Stash publication dates into dates metadata field

--- aleph/metadata/__init__.py
+++ aleph/metadata/__init__.py
@@ -234,6 +234,7 @@ class Metadata(object):
     @publication_date.setter
     def publication_date(self, publication_date):
         self._publication_date = parse_date(publication_date)
+        self.add_date(self._publication_date)
 
     @property
     def dates(self):
