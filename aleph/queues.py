c722a15e89a468cfbe14caad6ab671411c63c9e9
Friedrich Lindenberg <friedrich@pudo.org>
2017-06-30 17:22:59 +0200

    Clean up imports.

--- aleph/queues.py
+++ aleph/queues.py
@@ -1,10 +1,10 @@
 from aleph.core import create_app, celery as app  # noqa
 
-from aleph.ingest import ingest_url, ingest  # noqa
+from aleph.ingest import ingest  # noqa
 from aleph.analyze import analyze_document_id  # noqa
-from aleph.index import index_document_id  # noqa
-from aleph.logic import reindex_entities, analyze_collection  # noqa
-from aleph.logic import check_alerts, load_rows  # noqa
+from aleph.index.documents import index_document_id  # noqa
+from aleph.logic.entities import reindex_entities  # noqa
+from aleph.logic.alerts import check_alerts  # noqa
 from aleph.views.events import save_event  # noqa
 
 flask_app = create_app()
