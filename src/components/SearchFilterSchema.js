714b42f32d9995ba4d6a9f24c5786bef36c7edcb
Will Franklin <will@willpf.co.uk>
2017-08-11 15:54:27 +0100

    Much nicer way of handling All Results case

--- src/components/SearchFilterSchema.js
+++ src/components/SearchFilterSchema.js
@@ -2,25 +2,19 @@ import React from 'react';
 import { FormattedMessage, FormattedNumber } from 'react-intl';
 import { Tab2, Tabs2 } from '@blueprintjs/core';
 
-const ALL_SCHEMAS = 'AllSchemas';
-
-const SearchFilterSchema = ({ onChange, result, value }) => {
-  const _onChange = schemaId => onChange(schemaId === ALL_SCHEMAS ? null : schemaId);
-
-  return (
-    <Tabs2 id="schemaTypes" className="pt-large pt-dark" onChange={_onChange}
-      selectedTabId={value}>
-      <Tab2 id={ALL_SCHEMAS}>
-        <FormattedMessage id="search.schema.all" defaultMessage="All Results"/>
-        {' '}(<FormattedNumber value={result.total} />)
+const SearchFilterSchema = ({ onChange, result, value }) => (
+  <Tabs2 id="schemaTypes" className="pt-large pt-dark" onChange={onChange}
+    selectedTabId={value}>
+    <Tab2 id={null}>
+      <FormattedMessage id="search.schema.all" defaultMessage="All Results"/>
+      {' '}(<FormattedNumber value={result.total} />)
+    </Tab2>
+    {result.facets.schema.values.map(schema => (
+      <Tab2 id={schema.id} key={schema.id}>
+        {schema.label} (<FormattedNumber value={schema.count} />)
       </Tab2>
-      {result.facets.schema.values.map(schema => (
-        <Tab2 id={schema.id} key={schema.id}>
-          {schema.label} (<FormattedNumber value={schema.count} />)
-        </Tab2>
-      ))}
-    </Tabs2>
-  );
-}
+    ))}
+  </Tabs2>
+);
 
 export default SearchFilterSchema;
