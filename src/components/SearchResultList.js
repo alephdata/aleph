bdd93a5ea176965d03abeddd8d0c4e09df178d6c
Will Franklin <will@willpf.co.uk>
2017-08-14 17:35:41 +0100

    Some styling

--- src/components/SearchResultList.js
+++ src/components/SearchResultList.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-import SearchResultListItem from './SearchResultListItem'
+import SearchResultListItem from './SearchResultListItem';
+
+import './SearchResultList.css';
 
 const SearchResultList = ({ result }) => (
   <div>
     { result.isFetching && <div className='spinner'>Loading...</div> }
-    <table className="pt-table pt-bordered">
+    <table className="results pt-table pt-striped">
       <tbody>
         {result.results.map(item => <SearchResultListItem key={item.id} result={item} />)}
       </tbody>
