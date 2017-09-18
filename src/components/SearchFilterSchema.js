c794605f032778d2bf56ab97b9be2292d26c08cc
Will Franklin <will@willpf.co.uk>
2017-09-18 16:00:41 +0200

    Sum schemas for total

--- src/components/SearchFilterSchema.js
+++ src/components/SearchFilterSchema.js
@@ -2,19 +2,23 @@ import React from 'react';
 import { FormattedMessage, FormattedNumber } from 'react-intl';
 import { Tab2, Tabs2 } from '@blueprintjs/core';
 
-const SearchFilterSchema = ({ onChange, result, value }) => (
-  <Tabs2 id="schemaTypes" className="pt-large pt-dark" onChange={onChange}
-    selectedTabId={value}>
-    <Tab2 id={null}>
-      <FormattedMessage id="search.schema.all" defaultMessage="All Results"/>
-      {' '}(<FormattedNumber value={result.total} />)
-    </Tab2>
-    {result.facets.schema.values.map(schema => (
-      <Tab2 id={schema.id} key={schema.id}>
-        {schema.label} (<FormattedNumber value={schema.count} />)
+const SearchFilterSchema = ({ onChange, result, value }) => {
+  const schemas = result.facets.schema.values.sort((a, b) => a.label < b.label ? -1 : 1);
+
+  return (
+    <Tabs2 id="schemaTypes" className="pt-large pt-dark" onChange={onChange}
+      selectedTabId={value}>
+      <Tab2 id={null}>
+        <FormattedMessage id="search.schema.all" defaultMessage="All Results"/>
+        {' '}(<FormattedNumber value={schemas.reduce((total, schema) => total + schema.count, 0)} />)
       </Tab2>
-    ))}
-  </Tabs2>
-);
+      {schemas.map(schema => (
+        <Tab2 id={schema.id} key={schema.id}>
+          {schema.label} (<FormattedNumber value={schema.count} />)
+        </Tab2>
+      ))}
+    </Tabs2>
+  );
+}
 
 export default SearchFilterSchema;
