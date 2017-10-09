a8f0fd0b307353d5cc0969891d14b8a26f7d0963
Will Franklin <will@willpf.co.uk>
2017-10-09 18:26:27 +0200

    Work on collections dialog UI

--- src/components/SearchFilterCollections.js
+++ src/components/SearchFilterCollections.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { FormattedMessage, FormattedNumber } from 'react-intl';
-import { Button, Popover, Position, Spinner } from '@blueprintjs/core';
+import { Button, Dialog, Spinner } from '@blueprintjs/core';
 
 const mapStateToProps = ({ collections },  { collection }) => ({
   // Use more detailed collection data if we have it, fallback to basic
@@ -12,27 +12,80 @@ const mapStateToProps = ({ collections },  { collection }) => ({
 
 const SearchFilterCollectionsItem = connect(mapStateToProps)(({ collection }) => (
   <li>
-    <p>{ collection.label }</p>
+    <h6>{ collection.label }</h6>
     <p>{ collection.summary }</p>
   </li>
 ));
 
-const SearchFilterCollections = ({ loaded, collections, currentValue, onOpen, onChange }) => (
-  <Popover position={Position.BOTTOM} popoverWillOpen={onOpen} inline>
-      <Button rightIconName="caret-down">
+const SearchFilterCollectionsList = ({ collections }) => (
+  <div className="search-filter-collections-col">
+    <div className="search-filter-collections-col__blah">
+      <input type="search" />
+    </div>
+    <div className="search-filter-collections-col__fill">
+      <ul className="search-filter-collections-list">
+        {collections.map(collection => (
+          <SearchFilterCollectionsItem collection={collection} key={collection.id} />
+        ))}
+      </ul>
+    </div>
+  </div>
+);
+
+const SearchFilterCollectionsFilter = () => (
+  <div className="search-filter-collections-col">
+    <div className="search-filter-collections-col__fill">
+      <h4>Categories</h4>
+      stuff here
+    </div>
+    <div className="search-filter-collections-col__fill">
+      <h4>Collections</h4>
+      here too
+    </div>
+  </div>
+);
+
+class SearchFilterCollections extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isOpen: false
+    };
+
+    this.toggleDialog = this.toggleDialog.bind(this);
+  }
+
+  toggleDialog() {
+    const isOpen = !this.state.isOpen;
+    this.setState({ isOpen });
+
+    if (isOpen) {
+      this.props.onOpen();
+    }
+  }
+
+  render() {
+    const { isOpen } = this.state;
+    const { loaded, collections } = this.props;
+
+    return (
+    <div>
+      <Button rightIconName="caret-down" onClick={this.toggleDialog}>
         <FormattedMessage id="search.collections" defaultMessage="Collections"/>
         {loaded && <span> (<FormattedNumber value={collections.length} />)</span>}
       </Button>
-      <div className="search-filter-collections">
+      <Dialog isOpen={isOpen} onClose={this.toggleDialog} className="search-filter-collections">
         {loaded ?
-          <ul className="search-filter-collections-list">
-            {collections.map(collection => (
-              <SearchFilterCollectionsItem collection={collection} key={collection.id} />
-            ))}
-          </ul> :
+          [
+            <SearchFilterCollectionsList collections={collections} key={1} />,
+            <SearchFilterCollectionsFilter key={2} />
+          ] :
           <Spinner className="pt-large" />}
-      </div>
-  </Popover>
-);
+      </Dialog>
+    </div>
+    );
+  }
+}
 
 export default SearchFilterCollections;
