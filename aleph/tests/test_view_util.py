31c58be864cdcd66cd9b3a13c0874792e929f961
Will Franklin <will@willpf.co.uk>
2017-10-31 11:19:38 +0000

    Remove extract_next_url

--- aleph/tests/test_view_util.py
+++ aleph/tests/test_view_util.py
@@ -1,6 +1,6 @@
 from flask import Request
 
-from aleph.views.util import extract_next_url, get_best_next_url
+from aleph.views.util import get_best_next_url
 from aleph.tests.util import TestCase
 
 
@@ -8,19 +8,6 @@ class ViewUtilTest(TestCase):
 
     def setUp(self):
         super(ViewUtilTest, self).setUp()
-
-    def test_extract_next_url_blank(self):
-        req = Request.from_values('')
-        self.assertEqual('http://localhost:5000/', extract_next_url(req))
-
-    def test_extract_next_url_unsafe(self):
-        req = Request.from_values('/?next={}'.format(self.fake.url()))
-        self.assertEqual('http://localhost:5000/', extract_next_url(req))
-
-    def test_extract_next_url_safe(self):
-        headers = {'Referer': '/blah'}
-        req = Request.from_values('/?next=/help', headers=headers)
-        self.assertEqual('/help', extract_next_url(req))
 
     def test_get_best_next_url_blank(self):
         self.assertEqual('http://localhost:5000/', get_best_next_url(''))
