405ec799186b246d0eb34e3e7cbb9fa0a9408aa0
Will Franklin <will@willpf.co.uk>
2017-10-31 11:49:36 +0000

    Remove API proxy

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import queryString from 'query-string';
 
 export const endpoint = axios.create({
-  baseURL: 'http://localhost:3000/api/2',
+  baseURL: 'http://localhost:5000/api/2',
   headers: {},
   // Use non-bracket array params format
   paramsSerializer: queryString.stringify
