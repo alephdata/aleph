b423d74f21e2333299f56e655f1693e938324271
Will Franklin <will@willpf.co.uk>
2017-11-03 13:56:19 +0000

    Add tests for QueryParser

---
+++ aleph/tests/test_search_parser.py
@@ -0,0 +1,44 @@
+from unittest import TestCase
+from aleph.search.parser import QueryParser
+
+args = QueryParser([
+    ('offset', '5'),
+    ('filter:key1', 'foo1'),
+    ('filter:key1', 'foo2'),
+    ('filter:key2', 'foo3'),
+    ('filter:key3', 'foo4'),
+    ('filter:key3', 'foo4'),
+    ('filter:key2', 'foo5'),
+    ('myint', 100),
+    ('myint', 105),
+    ('mybadint', 'six'),
+    ('mybool', 't'),
+    ('mybadbool', 'oui')
+], None)
+
+class QueryParserTestCase(TestCase):
+    def test_offset(self):
+        self.assertEqual(args.offset, 5)
+
+    def test_filters(self):
+        self.assertItemsEqual(args.filters.keys(), ['key1', 'key2', 'key3'])
+        self.assertEqual(args.filters['key1'], set(['foo1', 'foo2']))
+        self.assertEqual(args.filters['key2'], set(['foo3', 'foo5']))
+        self.assertEqual(args.filters['key3'], set(['foo4']))
+
+    def test_getintlist(self):
+        self.assertEqual(args.getintlist('myint'), [100, 105])
+        self.assertEqual(args.getintlist('notakey'), [])
+        self.assertEqual(args.getintlist('notakey', [8]), [8])
+
+    def test_getint(self):
+        self.assertEqual(args.getint('myint'), 100)
+        self.assertEqual(args.getint('mybadint'), None)
+        self.assertEqual(args.getint('notakey'), None)
+        self.assertEqual(args.getint('notakey', 8), 8)
+
+    def test_getbool(self):
+        self.assertEqual(args.getbool('mybool'), True)
+        self.assertEqual(args.getbool('mybadbool'), False)
+        self.assertEqual(args.getbool('notakey'), False)
+        self.assertEqual(args.getbool('notakey', True), True)
