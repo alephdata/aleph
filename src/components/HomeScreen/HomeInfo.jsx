15d0bb9e12d1029f4647778339650bd3b93c3ed0
Ben <ben@occrp.org>
2017-11-29 12:54:00 +0100

    Split up HomeScreen into files.

---
+++ src/components/HomeScreen/HomeInfo.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Icon } from '@blueprintjs/core';
+
+import DualPane from 'components/common/DualPane';
+import Breadcrumbs from 'components/common/Breadcrumbs';
+
+import './HomeInfo.css';
+
+class HomeInfo extends Component {
+  render() {
+    return (
+      <DualPane.InfoPane>
+        <Breadcrumbs root>
+          <Link to={'/'}><Icon iconName="folder-open" /></Link>
+        </Breadcrumbs>
+        <h1>
+          <Link to={'/'}>
+            Aleph
+          </Link>
+        </h1>
+        <p className="tagline"><em>93,801,670</em><br />leads for your investigations</p>
+        <p>Search millions of documents and datasets, from public sources, leaks and investigations.</p>
+      </DualPane.InfoPane>
+    );
+  }
+}
+
+export default HomeInfo;
