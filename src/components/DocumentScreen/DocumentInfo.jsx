e64795a47024207168fd4cd6f2da4e96e0e4cbee
Ben <ben@occrp.org>
2017-12-01 14:30:29 +0100

    Add download button.

--- src/components/DocumentScreen/DocumentInfo.jsx
+++ src/components/DocumentScreen/DocumentInfo.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { AnchorButton } from '@blueprintjs/core';
 
 import DualPane from 'src/components/common/DualPane';
 
@@ -6,6 +7,10 @@ class DocumentInfo extends Component {
   render() {
     const { document } = this.props;
     const hasTitle = !!document.title;
+    // If it has a content_hash it is not a folder and <uri>/file should work.
+    const fileUri = document.content_hash !== undefined
+      ? `${document.uri}/file`
+      : undefined
     return (
       <DualPane.InfoPane>
         <h1>{hasTitle ? document.title : document.file_name}</h1>
@@ -14,6 +19,15 @@ class DocumentInfo extends Component {
           <li>{document.schema}</li>
           <li>{document.created_at}</li>
         </ul>
+        {fileUri &&
+          <AnchorButton
+            href={fileUri}
+            download={document.file_name}
+            intent="primary"
+          >
+            Download
+          </AnchorButton>
+        }
       </DualPane.InfoPane>
     );
   }
