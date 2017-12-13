b2d72e9648ed05ab2a1d63d48c3479f5b70f089f
Ben <ben@occrp.org>
2017-12-13 18:46:12 +0100

    Get session actions&reducers on par.
    Not sure how/if they work; just doing blind refactoring.

--- ui/src/reducers/session.js
+++ ui/src/reducers/session.js
@@ -1,28 +1,22 @@
+import { createReducer } from 'redux-act';
 import jwt_decode from 'jwt-decode';
 
+import { loginWithToken, logout } from 'src/actions/sessionActions';
+
 const initialState = {
-  loggedIn: false
+  loggedIn: false,
 };
 
-const login = (token) => {
+const login = token => {
   const data = jwt_decode(token);
   return {
     ...data,
     token,
-    loggedIn: true
+    loggedIn: true,
   };
 };
 
-const session = (state = initialState, action) => {
-  const { type, payload } = action;
-  switch (type) {
-    case 'LOGIN':
-      return login(payload.token);
-    case 'LOGOUT':
-      return {loggedIn: false};
-    default:
-      return state;
-  }
-};
-
-export default session;
+export default createReducer({
+  [loginWithToken]: (state, { token }) => login(token),
+  [logout]: state => ({ loggedIn: false }),
+}, initialState);
