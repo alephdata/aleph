2bb1664f7f59b0e9a624e106dec618c714be914d
Ben <ben@occrp.org>
2017-12-18 18:49:07 +0100

    Simplify SearchResultListItem code.

--- ui/src/components/SearchScreen/SearchResultListItem.jsx
+++ ui/src/components/SearchScreen/SearchResultListItem.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import Schema from 'src/components/common/Schema';
 import Country from 'src/components/common/Country';
 import getPath from 'src/util/getPath';
 
-const ListItem = ({ name, title, schema, collection, links, countries }) => (
+const SearchResultListItem = ({
+  result: { name, title, schema, collection, links, countries }
+}) => (
   <tr className={`result result--${schema}`}>
     <td className="result__name">
       <Link to={getPath(links.ui)}>
@@ -15,24 +16,19 @@ const ListItem = ({ name, title, schema, collection, links, countries }) => (
       </Link>
     </td>
     <td className="result__collection">
-      {collection ?
-        <span title={collection.label}>
-          <span className="pt-icon pt-icon-globe" /> {collection.label}
-        </span> :
-        <span className="pt-skeleton">Loading collection</span>}
+      {collection
+        ? (
+          <span title={collection.label}>
+            <span className="pt-icon pt-icon-globe" /> {collection.label}
+          </span>
+        )
+        : <span className="pt-skeleton">Loading collection</span>
+      }
     </td>
     <td>
-      <Country.List codes={ countries } />
+      <Country.List codes={countries} />
     </td>
   </tr>
 );
 
-const SearchResultListItem = ({ result, countries }) => {
-  return <ListItem {...result} />;
-};
-
-const mapStateToProps = ({ metadata }, { result }) => ({
-  countries: metadata.countries
-});
-
-export default connect(mapStateToProps)(SearchResultListItem);
+export default SearchResultListItem;
