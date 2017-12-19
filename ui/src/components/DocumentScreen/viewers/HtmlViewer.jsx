042acf5844f3448e042cad93a7446e354b276409
Friedrich Lindenberg <friedrich@pudo.org>
2017-12-19 17:17:56 +0100

    display raw html for testings.

--- ui/src/components/DocumentScreen/viewers/HtmlViewer.jsx
+++ ui/src/components/DocumentScreen/viewers/HtmlViewer.jsx
@@ -5,9 +5,10 @@ import './HtmlViewer.css';
 class HtmlViewer extends Component {
   render() {
     const { html } = this.props;
+    // <iframe sandbox srcDoc={html}></iframe>
     return (
       <div className="HtmlViewer">
-        <iframe sandbox srcDoc={html}></iframe>
+        <span dangerouslySetInnerHTML={{__html: html}} />
       </div>
     );
   }
