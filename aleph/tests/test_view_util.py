3a836aff4dbf0b8371846b34a8673476fc37c84c
rhiaro <amy@rhiaro.co.uk>
2017-12-21 18:18:47 +0100

    Sanitize HTML for documents api output

--- aleph/tests/test_view_util.py
+++ aleph/tests/test_view_util.py
@@ -1,4 +1,4 @@
-from aleph.views.util import get_best_next_url
+from aleph.views.util import get_best_next_url, sanitize_html
 from aleph.tests.util import TestCase, UI_URL
 
 
@@ -14,7 +14,14 @@ class ViewUtilTest(TestCase):
         self.assertEqual(UI_URL, get_best_next_url(self.fake.url()))  # noqa
 
     def test_get_best_next_url_unsafe_safe(self):
-        self.assertEqual(UI_URL + 'next', get_best_next_url(self.fake.url(), '/next'))
+        self.assertEqual(
+            UI_URL + 'next', get_best_next_url(self.fake.url(), '/next'))
 
     def test_get_best_next_url_all_unsafe(self):
         self.assertEqual(UI_URL, get_best_next_url(self.fake.url(), self.fake.url()))  # noqa
+
+    def test_sanitize_html(self):
+        html = '<!doctype html><html><head><title>Article</title><style type="text/css">body { }</style><script>alert("We love Angular")</script><link rel="stylesheet" href="http://xss.rocks/xss.css"></head><body><article id="story"><h1>We welcome our new React overlords</h1><img src="&#14;  javascript:alert(\'XSS\');" alt="" /><p>Published on <time onmouseover="alert(\'XSS\')">1 January 2018</time></p><p>Really the only thing better than the <a href="/blockchain">blockchain</a> is ReactJS.</p></article><video> <source onerror = "javascript: alert (XSS)"></video></body></html>'
+        cleaned = '<div><article id="story"><h1>We welcome our new React overlords</h1><p>Published on <time>1 January 2018</time></p><p>Really the only thing better than the <a href="https://example.org/blockchain" rel="nofollow">blockchain</a> is ReactJS.</p></article></div>'
+        processed = sanitize_html(html, 'https://example.org/welcome-react')
+        self.assertEqual(processed, cleaned)
