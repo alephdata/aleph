abd428583d73d7c34025ab28428922473de219d3
rhiaro <amy@rhiaro.co.uk>
2017-12-22 09:59:25 +0100

    Improve sanitize_html test

--- aleph/tests/test_view_util.py
+++ aleph/tests/test_view_util.py
@@ -1,3 +1,5 @@
+from lxml.html import document_fromstring
+
 from aleph.views.util import get_best_next_url, sanitize_html
 from aleph.tests.util import TestCase, UI_URL
 
@@ -21,7 +23,17 @@ class ViewUtilTest(TestCase):
         self.assertEqual(UI_URL, get_best_next_url(self.fake.url(), self.fake.url()))  # noqa
 
     def test_sanitize_html(self):
-        html = '<!doctype html><html><head><title>Article</title><style type="text/css">body { }</style><script>alert("We love Angular")</script><link rel="stylesheet" href="http://xss.rocks/xss.css"></head><body><article id="story"><h1>We welcome our new React overlords</h1><img src="&#14;  javascript:alert(\'XSS\');" alt="" /><p>Published on <time onmouseover="alert(\'XSS\')">1 January 2018</time></p><p>Really the only thing better than the <a href="/blockchain">blockchain</a> is ReactJS.</p></article><video> <source onerror = "javascript: alert (XSS)"></video></body></html>'
-        cleaned = '<div><article id="story"><h1>We welcome our new React overlords</h1><p>Published on <time>1 January 2018</time></p><p>Really the only thing better than the <a href="https://example.org/blockchain" target="_blank" rel="nofollow">blockchain</a> is ReactJS.</p></article></div>'
-        processed = sanitize_html(html, 'https://example.org/welcome-react')
-        self.assertEqual(processed, cleaned)
+        html_str = '<!doctype html><html><head><title>Article</title><style type="text/css">body { }</style><script>alert("We love Angular")</script><link rel="stylesheet" href="http://xss.rocks/xss.css"></head><body><article id="story"><h1>We welcome our new React overlords</h1><img src="&#14;  javascript:alert(\'XSS\');" alt="" /><p>Published on <time onmouseover="alert(\'XSS\')">1 January 2018</time></p><p>Really the only thing better than the <a href="/blockchain">blockchain</a> is ReactJS.</p></article><video> <source onerror = "javascript: alert (XSS)"></video></body></html>'
+        processed = sanitize_html(
+            html_str, 'https://example.org/welcome-react')
+        html = document_fromstring(processed)
+        assert html.find('.//img') is None, html
+        assert html.find('.//video') is None, html
+        assert html.find('.//style') is None, html
+        assert html.find('.//script') is None, html
+        assert len(html.findall('.//article')) == 1, html
+        assert html.find('.//time').get('onmouseover') == None, html
+        assert html.find(
+            './/a').get('href') == 'https://example.org/blockchain', html
+        assert html.find('.//a').get('target') == '_blank', html
+        assert html.find('.//a').get('rel') == 'nofollow', html
