ee51c26845196309b15696fdb6d521099d2a5d7e
Friedrich Lindenberg <friedrich@pudo.org>
2017-12-23 22:36:49 +0100

    Fix up login, scroll to top

--- ui/src/components/common/Screen.jsx
+++ ui/src/components/common/Screen.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withRouter } from 'react-router';
 
 import './Screen.css';
 
-const Screen = ({ children = [] }) => (
-  <div className="Screen">{ children }</div>
-);
+class Screen extends Component {
 
-export default Screen;
+  componentDidMount() {
+    window.scrollTo(0, 0)
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.location !== prevProps.location) {
+      window.scrollTo(0, 0)
+    }
+  }
+
+  render() {
+    const { children } = this.props;
+    return (
+      <div className="Screen">
+        { children }
+      </div>
+    )
+  }
+}
+
+export default withRouter(Screen);
