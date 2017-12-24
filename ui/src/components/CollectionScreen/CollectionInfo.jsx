c5edc1b4ae92d467a219cfbcb403bdd98d3eff34
Friedrich Lindenberg <friedrich@pudo.org>
2017-12-24 14:25:01 +0100

    Collections & breadcrumbs cleanup

--- ui/src/components/CollectionScreen/CollectionInfo.jsx
+++ ui/src/components/CollectionScreen/CollectionInfo.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { FormattedNumber, FormattedDate } from 'react-intl';
+import { FormattedNumber, FormattedMessage } from 'react-intl';
 
 import DualPane from 'src/components/common/DualPane';
 import Schema from 'src/components/common/Schema';
+import Date from 'src/components/common/Date';
 
 
 class CollectionInfo extends Component {
@@ -16,13 +17,22 @@ class CollectionInfo extends Component {
           {collection.label}
         </h1>
         <p>{collection.summary}</p>
-        Contains:
+        <p>
+          Last update:
+          <Date value={collection.updated_at} />
+        </p>
+        
+        <h3>
+          <FormattedMessage id="collection.contents" defaultMessage="Contents"/>
+        </h3>
         <table>
           <tbody>
             {Object.entries(collection.schemata).map(([key, value]) => (
               <tr key={key}>
-                <th className="tiny"><Schema.Icon schema={key} /></th>
-                <th><Schema.Name schema={key} plural /></th>
+                <th>
+                  <Schema.Icon schema={key} />
+                  <Schema.Name schema={key} plural />
+                </th>
                 <td className="numeric">
                   <FormattedNumber value={value} />
                 </td>
@@ -30,13 +40,6 @@ class CollectionInfo extends Component {
             ))}
           </tbody>
         </table>
-        <ul>
-          
-        </ul>
-        <p>
-          Last update:
-          <FormattedDate value={collection.updated_at} />
-        </p>
       </DualPane.InfoPane>
     );
   }
