58d8284bc02adf4faf57083eae8e1e35a1111662
Friedrich Lindenberg <friedrich@pudo.org>
2018-01-10 17:15:33 +0100

    Iterate on CollectionCard, fixes #268.

--- ui/src/components/CollectionScreen/CollectionCard.jsx
+++ ui/src/components/CollectionScreen/CollectionCard.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { FormattedMessage, FormattedNumber } from 'react-intl';
+import Truncate from 'react-truncate';
 
 import Date from 'src/components/common/Date';
+import Category from 'src/components/common/Category';
 import Collection from 'src/components/common/Collection';
 
 import './CollectionCard.css';
@@ -17,27 +19,36 @@ class CollectionCard extends Component {
         <h4><Collection.Link collection={collection} /></h4>
         <div className="facts">
           <div className="fact">
-            <FormattedNumber value={collection.count} />{' '}
-            <FormattedMessage id="collection.total.count" defaultMessage="Entries"/>
-          </div>
-          <div className="fact">
-            <i className="fa fa-fw fa-refresh" />
-            <Date value={collection.updated_at} />
-          </div>
-          {/*
-          <div className="fact">
             <Category collection={collection} />
           </div>
-          */}
+          <div className="fact">
+            <FormattedMessage id="collection.total.count"
+                              defaultMessage="{count} Entries"
+                              values={{
+                                count: <FormattedNumber value={collection.count} />
+                              }}/>
+          </div>
         </div>
         {collection.summary && 
-          (<p>{ collection.summary }</p>)
+          <p>
+            <Truncate lines={2}>
+              { collection.summary }
+            </Truncate>
+          </p>
         }
         {!collection.summary && 
           (<p className="missing">
             <FormattedMessage id="collection.summary.missing" defaultMessage="This collecton has no description."/>
           </p>)
         }
+        <div className="bottom-fact">
+          <i className="fa fa-fw fa-refresh" />
+          <FormattedMessage id="collection.last_updated"
+                            defaultMessage="Last updated: {date}"
+                            values={{
+                              date: <Date value={collection.updated_at} />
+                            }}/>
+        </div>
       </div>
     );
   }
--- ui/src/components/EntityScreen/Entity.jsx
+++ ui/src/components/EntityScreen/Entity.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import React, { Component } from 'react';
+import truncateText from 'truncate';
 
 import Schema from 'src/components/common/Schema';
 import getPath from 'src/util/getPath';
@@ -7,15 +8,20 @@ import getPath from 'src/util/getPath';
 
 class Label extends Component {
   render() {
-    const { icon = false } = this.props;
+    const { icon = false, truncate } = this.props;
     let { title, name, file_name, schema } = this.props.entity;
+    let text = title || name || file_name;
+
+    if (truncate) {
+      text = truncateText(text, truncate);
+    }
     
     return (
       <span className="entity-label" title={ title || name }>
         {icon && (
           <Schema.Icon schema={schema} />
         )}
-        { title || name || file_name }
+        { text }
       </span>
     );
   }
@@ -23,10 +29,10 @@ class Label extends Component {
 
 class EntityLink extends Component {
   render() {
-    const { entity, className, icon, short } = this.props;
+    const { entity, className, icon, truncate } = this.props;
     return (
       <Link to={getPath(entity.links.ui)} className={className}>
-        <Label entity={entity} icon={icon} short={short} />
+        <Label entity={entity} icon={icon} truncate={truncate} />
       </Link>
     );
   }
