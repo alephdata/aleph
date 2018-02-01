1a3ef2e924b7adcbad4a3c957839a0a656f266e0
Friedrich Lindenberg <friedrich@pudo.org>
2018-02-01 15:03:55 +0100

    Clean up document tag thing and normalize it’s values.

--- aleph/model/document_tag.py
+++ aleph/model/document_tag.py
@@ -1,6 +1,7 @@
 import logging
 import exactitude
 from normality import stringify, slugify
+from followthemoney.types import TYPES
 
 from aleph.core import db
 from aleph.model.common import IdModel
@@ -17,15 +18,29 @@ class DocumentTag(db.Model, IdModel):
     TYPE_ORGANIZATION = 'organization'
     TYPE_LOCATION = 'location'
 
+    TYPES = {
+        TYPE_PERSON: exactitude.names,
+        TYPE_ORGANIZATION: exactitude.names,
+        TYPE_EMAIL: exactitude.emails,
+        TYPE_PHONE: exactitude.phones,
+        TYPE_LOCATION: exactitude.addresses,
+    }
+
     id = db.Column(db.BigInteger, primary_key=True)
     origin = db.Column(db.Unicode(255), nullable=False, index=True)
     type = db.Column(db.Unicode(16), nullable=False)
     weight = db.Column(db.Integer, default=1)
-    key = db.Column(db.Unicode(1024), nullable=False, index=True)
     text = db.Column(db.Unicode(1024), nullable=True)
 
     document_id = db.Column(db.Integer(), db.ForeignKey('document.id'), index=True)  # noqa
     document = db.relationship("Document", backref=db.backref('tags', cascade='all, delete-orphan'))  # noqa
+
+    @property
+    def field(self):
+        type_ = self.TYPES[self.type]
+        for (candidate, invert) in TYPES.values():
+            if candidate == type_:
+                return invert
 
     @classmethod
     def delete_by(cls, document_id=None, origin=None, type=None):
@@ -41,7 +56,7 @@ class DocumentTag(db.Model, IdModel):
         db.session.flush()
 
     def __repr__(self):
-        return '<DocumentTag(%r,%r)>' % (self.document_id, self.key)
+        return '<DocumentTag(%r,%r)>' % (self.document_id, self.text)
 
 
 class DocumentTagCollector(object):
@@ -50,14 +65,6 @@ class DocumentTagCollector(object):
     This is useful when many tags about the same documented are emitted by a
     particular source."""
 
-    CLEANERS = {
-        DocumentTag.TYPE_PERSON: exactitude.names,
-        DocumentTag.TYPE_ORGANIZATION: exactitude.names,
-        DocumentTag.TYPE_EMAIL: exactitude.emails,
-        DocumentTag.TYPE_PHONE: exactitude.phones,
-        DocumentTag.TYPE_LOCATION: exactitude.addresses,
-    }
-
     def __init__(self, document, origin):
         self.document = document
         self.origin = origin
@@ -65,35 +72,27 @@ class DocumentTagCollector(object):
 
     def emit(self, text, type, key=None, weight=1):
         "Create a tag, this can be called multiple times with the same text."
-        cleaner = self.CLEANERS[type]
+        cleaner = DocumentTag.TYPES[type]
         text = stringify(text)
         text = cleaner.clean(text, countries=self.document.countries)
         if text is None:
             return
 
-        key = stringify(key)
-        if key is None:
-            key = slugify(text, sep='-')
-
-        if key is None:
-            return
-
-        if (key, type) not in self.keyed:
-            self.keyed[(key, type)] = dict(text=text, weight=weight)
+        if (text, type) not in self.keyed:
+            self.keyed[(text, type)] = dict(text=text, weight=weight)
         else:
-            self.keyed[(key, type)]['weight'] += weight
+            self.keyed[(text, type)]['weight'] += weight
 
     def save(self):
         """Flush all existing tags from this origin and store new ones."""
         DocumentTag.delete_by(document_id=self.document.id,
                               origin=self.origin)
-        for (key, type), tag in self.keyed.items():
+        for (text, type), tag in self.keyed.items():
             obj = DocumentTag()
             obj.document_id = self.document.id
             obj.origin = self.origin
             obj.type = type
-            obj.key = key
-            obj.text = tag['text']
+            obj.text = text
             obj.weight = tag['weight']
             db.session.add(obj)
         db.session.flush()
