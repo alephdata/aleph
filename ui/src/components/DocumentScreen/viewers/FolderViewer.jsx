fc4d409ab77dc39b875f332e0b2f94ddebb8ec2f
Iain Collins <me@iaincollins.com>
2018-02-16 16:54:33 +0100

    Refactoring folder view to match new wireframes
    * Document/Folder search box no longer disabled.
    * No longer displays two search boxes in folder view.
    * Fixes cosmetic issue with outline being clipped.
    
    Known limitations:
    
    * Currently the new search box always does a folder search and does not detect the document type.
    * It mirrors the behaviour of the old search box, doing a full regular search in the current scope, and does not just load in a context-specific folder search in the same view.

--- ui/src/components/DocumentScreen/viewers/FolderViewer.jsx
+++ ui/src/components/DocumentScreen/viewers/FolderViewer.jsx
@@ -1,43 +1,12 @@
 import React, { Component } from 'react';
-import queryString from 'query-string';
 import { withRouter } from 'react-router';
-import { injectIntl } from 'react-intl';
-
 import SearchContext from 'src/components/SearchScreen/SearchContext';
-import getPath from 'src/util/getPath';
 
 import './FolderViewer.css';
 
 class FolderViewer extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {queryText: ''};
-
-    this.onChangeQueryText = this.onChangeQueryText.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-  }
-
-  onChangeQueryText({target}) {
-    this.setState({queryText: target.value})
-  }
-
-  onSubmit(event) {
-    const { history, document } = this.props;
-    const path = getPath(document.links.ui) + '/related';
-    history.push({
-      pathname: path,
-      search: queryString.stringify({
-        q: this.state.queryText,
-        'filter:ancestors': document.id
-      })
-    });
-    event.preventDefault();
-  }
-
   render() {
-    const { intl, document } = this.props;
-    const { queryText } = this.state;
+    const { document } = this.props;
 
     if (!document || !document.id || !document.links) {
       return null;
@@ -52,16 +21,6 @@ class FolderViewer extends Component {
     };
     return (
       <div id="children" className="FolderViewer">
-        <section className="folder-filter">
-          <form onSubmit={this.onSubmit}>
-            <div className="pt-input-group">
-              <span className="pt-icon pt-icon-search"></span>
-              <input className="pt-input" type="search"
-                  placeholder={intl.formatMessage({id: "document.folder.search", defaultMessage: "Search this folder" })}
-                  onChange={this.onChangeQueryText} value={queryText} />
-            </div>
-          </form>
-        </section>
         <SearchContext collection={document.collection}
                        context={context}
                        aspects={aspects} />
@@ -70,6 +29,5 @@ class FolderViewer extends Component {
   }
 }
 
-FolderViewer = injectIntl(FolderViewer)
 FolderViewer = withRouter(FolderViewer)
 export default FolderViewer;
