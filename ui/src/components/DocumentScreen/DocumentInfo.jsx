d64b10deaf2c0416a97032bdd96debd724f24a6a
Iain Collins <me@iaincollins.com>
2018-02-20 14:43:15 +0100

    Adding tabs to document info sidebar

--- ui/src/components/DocumentScreen/DocumentInfo.jsx
+++ ui/src/components/DocumentScreen/DocumentInfo.jsx
@@ -1,39 +1,75 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import {FormattedMessage} from 'react-intl';
+import React from 'react';
+import { connect } from 'react-redux';
+import { injectIntl, FormattedMessage } from 'react-intl';
+import { Tab, Tabs, Icon } from "@blueprintjs/core";
 
 import Entity from 'src/components/EntityScreen/Entity';
 import EntityInfoTags from 'src/components/EntityScreen/EntityInfoTags';
 import DualPane from 'src/components/common/DualPane';
 import DocumentMetadata from 'src/components/DocumentScreen/DocumentMetadata';
-import CollectionCard from 'src/components/CollectionScreen/CollectionCard';
+import DocumentCollection from 'src/components/DocumentScreen/DocumentCollection';
 
 import './DocumentInfo.css';
 
-class DocumentInfo extends Component {
-    render() {
-        const {document} = this.props;
+class DocumentInfo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeTabId: 'overview'
+    };
+    this.handleTabChange = this.handleTabChange.bind(this);
+  }
+  
+  handleTabChange(activeTabId: TabId) {
+    this.setState({ activeTabId });
+  }
 
-        return (
-          <DualPane.InfoPane className="DocumentInfo">
-            <h1>
-              <Entity.Label entity={document} addClass={true}/>
-            </h1>
-            <DocumentMetadata document={document}/>
-            <h2>
-              <FormattedMessage id="collection.section" defaultMessage="Origin"/>
-            </h2>
-            <div>
-              <CollectionCard collection={document.collection}/>
-            </div>
-            <EntityInfoTags entity={document} />
-          </DualPane.InfoPane>
-        );
-    }
+  render() {
+    const {document} = this.props;
+    return (
+      <DualPane.InfoPane className="DocumentInfo">
+        <div className="PaneHeading">
+          <h1 style={{margin: 0, border: 0}}>
+            <Entity.Label entity={document} addClass={true}/>
+          </h1>
+        </div>
+        <div className="PaneContent">
+          <Tabs id="TabsExample"  large="true" onChange={this.handleTabChange} selectedTabId={this.state.activeTabId}>
+              <Tab id="overview"
+                title={
+                  <React.Fragment>
+                    <Icon icon="info-sign"/> <FormattedMessage id="document.info.overview" defaultMessage="Overview"/>
+                  </React.Fragment>
+                }
+                panel={<DocumentMetadata document={document}/>} 
+              />
+              <Tab id="source" 
+                title={
+                  <React.Fragment>
+                    <Icon icon="graph"/> <FormattedMessage id="document.info.source" defaultMessage="Source"/>
+                  </React.Fragment>
+                }
+                panel={<DocumentCollection collection={document.collection}/>}
+              />
+              <Tab id="tags"
+                title={
+                  <React.Fragment>
+                    <Icon icon="tag"/> <FormattedMessage id="document.info.tags" defaultMessage="Related Tags"/>
+                  </React.Fragment>
+                }
+                panel={<EntityInfoTags entity={document} />}
+              />
+              <Tabs.Expander />
+          </Tabs>
+        </div>
+      </DualPane.InfoPane>
+    );
+  }
 }
 
 const mapStateToProps = state => ({
   session: state.session,
 });
 
+DocumentInfo = injectIntl(DocumentInfo)
 export default connect(mapStateToProps)(DocumentInfo);
