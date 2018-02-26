4623f96677c22647e17a6935ce141609e78cb5c3
Ben <ben@occrp.org>
2018-02-26 22:28:40 +0100

    Merge branch 'loader-components'

--- ui/src/components/CollectionScreen/index.jsx
+++ ui/src/components/CollectionScreen/index.jsx
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import { Helmet } from 'react-helmet';
 import { NonIdealState } from '@blueprintjs/core';
 import { defineMessages, injectIntl } from 'react-intl';
 
-import { fetchCollection } from 'src/actions';
 import Screen from 'src/components/common/Screen';
 import ScreenLoading from 'src/components/common/ScreenLoading';
 import Breadcrumbs from 'src/components/common/Breadcrumbs';
 import DualPane from 'src/components/common/DualPane';
 import CollectionContent from './CollectionContent';
 import CollectionInfo from './CollectionInfo';
+import Collection from 'src/components/common/Collection';
 
 const messages = defineMessages({
   not_found: {
@@ -20,28 +19,15 @@ const messages = defineMessages({
 });
 
 class CollectionScreen extends Component {
-  componentDidMount() {
-    const { collectionId } = this.props;
-    this.props.fetchCollection({ id: collectionId });
-  }
-
-  componentDidUpdate(prevProps) {
-    const { collectionId } = this.props;
-    if (collectionId !== prevProps.collectionId) {
-      this.props.fetchCollection({ id: collectionId });
-    }
-  }
-
   render() {
-    const { collection, location, intl } = this.props;
-    if (collection === undefined || collection.isFetching) {
-      return <ScreenLoading />;
-    }
+    const { collection, intl } = this.props;
+
     if (collection.error) {
       return (
         <NonIdealState visual="error" title={intl.formatMessage(messages.not_found)} />
       );
     }
+
     return (
       <Screen>
         <Helmet>
@@ -49,19 +35,22 @@ class CollectionScreen extends Component {
         </Helmet>
         <Breadcrumbs collection={collection} />
         <DualPane>
-          <CollectionInfo collection={collection} location={location} />
+          <CollectionInfo collection={collection} />
           <CollectionContent collection={collection} />
         </DualPane>
       </Screen>
-      
-    )
+    );
   }
 }
 
-const mapStateToProps = (state, ownProps) => {
-  const { collectionId } = ownProps.match.params;
-  const collection = state.collections[collectionId];
-  return { collectionId, collection };
-};
+CollectionScreen = injectIntl(CollectionScreen);
 
-export default connect(mapStateToProps, { fetchCollection })(injectIntl(CollectionScreen));
+// Wrap the CollectionScreen into Collection.Load to handle data fetching.
+export default ({ match, ...otherProps }) => (
+  <Collection.Load
+    id={match.params.collectionId}
+    renderWhenLoading={<ScreenLoading />}
+  >{collection => (
+    <CollectionScreen collection={collection} {...otherProps} />
+  )}</Collection.Load>
+);
