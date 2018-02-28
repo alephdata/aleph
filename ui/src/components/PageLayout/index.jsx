73cd4eacf350c9a01fc0aeb7b63b49183418b911
Iain Collins <me@iaincollins.com>
2018-02-28 17:57:33 +0100

    Moved Navbar and Footer from PageLayout to Screen
    * This leaves us with a cleaner PageLayout file with simpler logic.
    
    * A SearchContext can now be passed to the Screens that need it by wrapping <Screen> and passing searchContext (e.g. <Screen searchContext={searchContext}>).
    
    * The code for fetching metadata still lives in PageLayout (attempts to further refactor it into Screen just caused woes with React Router interactions).
    
    It might be better to call PageLayout something like Router.

--- ui/src/components/PageLayout/index.jsx
+++ ui/src/components/PageLayout/index.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { Spinner } from '@blueprintjs/core';
-import { Helmet } from 'react-helmet';
 
 import { fetchMetadata } from 'src/actions';
 import LoginScreen from 'src/components/auth/LoginScreen';
@@ -38,63 +37,33 @@ class PageLayout extends Component {
     const isLoaded = metadata && metadata.app && session;
     if (!isLoaded) {
       return (
-        <div className="PageLayout-spinner">
-          <div className="spinner">
-            <Spinner className="pt-large"/>
-          </div>
+        <div className="PageLayoutLoading">
+          <div className="spinner"><Spinner className="pt-large"/></div>
         </div>
       )
     }
 
     return (
-      <div className="PageLayout-root">
-        <Helmet titleTemplate={`%s - ${metadata.app.title}`}>
-          <title>{metadata.app.title}</title>
-          <link rel="shortcut icon" href={metadata.app.favicon} />
-        </Helmet>
-        <SearchContext>{searchContext => (
-          <React.Fragment>
-            <PageNavbar metadata={metadata} session={session} searchContext={searchContext}/>
-            <main className="PageLayout-main">
-              <Switch>
-                <Route path="/login" exact component={LoginScreen}/>
-                <Route path="/logout" exact component={LogoutScreen}/>
-                <Route path="/signup" exact component={SignupScreen}/>
-                <Route path="/settings" exact component={ProfileScreen}/>
-                <Route path="/activate/:code" exact component={ActivateScreen}/>
-                <Route path="/entities/:entityId" exact component={EntityScreen}/>
-                <Route path="/entities/:entityId/related" exact component={EntityRelatedScreen}/>
-                <Route path="/documents/:documentId" exact component={DocumentScreen}/>
-                <Route path="/documents/:documentId/related" exact component={DocumentRelatedScreen}/>
-                <Route path="/text/:documentId" exact component={DocumentRedirectScreen}/>
-                <Route path="/tabular/:documentId/:sheet" exact component={DocumentRedirectScreen}/>
-                <Route path="/collections/:collectionId" exact component={CollectionScreen}/>
-                <Route path="/collections/:collectionId/edit" exact component={CollectionEditScreen}/>
-                <Route path="/collections/:collectionId/xref/:otherId" exact component={CollectionXrefScreen}/>
-                <Route path="/search" exact render={()=><SearchScreen searchContext={searchContext}/>}/>
-                <Route path="/" exact component={HomeScreen}/>
-                <Route component={ErrorScreen}/>
-              </Switch>
-            </main>
-          </React.Fragment>
-        )}</SearchContext>
-        <footer className="PageLayout-footer">
-          <p>
-            <strong>ℵ</strong> aleph Mk II
-            <span className="pt-text-muted"> • </span>
-            <span>
-              <a href="https://github.com/alephdata/aleph"><i className="fa fa-fw fa-github" /></a>
-              {' '}
-              <a href="https://github.com/alephdata/aleph">Source Code</a>
-            </span>
-            <span className="pt-text-muted"> • </span>
-            <span>
-              <a href="https://github.com/alephdata/aleph/wiki/User-manual"><i className="fa fa-fw fa-book" /></a>
-              {' '}
-              <a href="https://github.com/alephdata/aleph/wiki/User-manual">Documentation</a>
-            </span>
-          </p>
-        </footer>
+      <div className="PageLayout">
+        <Switch>
+          <Route path="/login" exact component={LoginScreen}/>
+          <Route path="/logout" exact component={LogoutScreen}/>
+          <Route path="/signup" exact component={SignupScreen}/>
+          <Route path="/settings" exact component={ProfileScreen}/>
+          <Route path="/activate/:code" exact component={ActivateScreen}/>
+          <Route path="/entities/:entityId" exact component={EntityScreen}/>
+          <Route path="/entities/:entityId/related" exact component={EntityRelatedScreen}/>
+          <Route path="/documents/:documentId" exact component={DocumentScreen}/>
+          <Route path="/documents/:documentId/related" exact component={DocumentRelatedScreen}/>
+          <Route path="/text/:documentId" exact component={DocumentRedirectScreen}/>
+          <Route path="/tabular/:documentId/:sheet" exact component={DocumentRedirectScreen}/>
+          <Route path="/collections/:collectionId" exact component={CollectionScreen}/>
+          <Route path="/collections/:collectionId/edit" exact component={CollectionEditScreen}/>
+          <Route path="/collections/:collectionId/xref/:otherId" exact component={CollectionXrefScreen}/>
+          <Route path="/search" exact component={SearchScreen}/>
+          <Route path="/" exact component={HomeScreen}/>
+          <Route component={ErrorScreen}/>
+        </Switch>
       </div>
     );
   }
--- ui/src/components/common/Screen.jsx
+++ ui/src/components/common/Screen.jsx
@@ -1,28 +1,65 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { withRouter } from 'react-router';
+import { connect } from 'react-redux';
+import { Helmet } from 'react-helmet';
+
+import PageNavbar from 'src/components/PageLayout/PageNavbarSearchForm';
 
 import './Screen.css';
 
-class Screen extends Component {
+class Screen extends React.Component {
 
   componentDidMount() {
     window.scrollTo(0, 0)
   }
 
   componentDidUpdate(prevProps) {
-    if (this.props.location.pathname !== prevProps.location.pathname) {
+    if (this.props.location && (this.props.location.pathname !== prevProps.location.pathname)) {
       window.scrollTo(0, 0)
     }
   }
 
   render() {
-    const { children } = this.props;
+    
     return (
       <div className="Screen">
-        { children }
+        <Helmet titleTemplate={`%s - ${this.props.title || this.props.metadata.app.title}`}>
+          <title>{this.props.title || this.props.metadata.app.title}</title>
+          <link rel="shortcut icon" href={this.props.metadata.app.favicon} />
+        </Helmet>
+      
+        <PageNavbar metadata={this.props.metadata} session={this.props.session} searchContext={this.props.searchContext}/>
+        
+        { this.props.children }
+        
+        <footer className="PageLayout-footer">
+          <p>
+            <strong>ℵ</strong> aleph Mk II
+            <span className="pt-text-muted"> • </span>
+            <span>
+              <a href="https://github.com/alephdata/aleph"><i className="fa fa-fw fa-github" /></a>
+              {' '}
+              <a href="https://github.com/alephdata/aleph">Source Code</a>
+            </span>
+            <span className="pt-text-muted"> • </span>
+            <span>
+              <a href="https://github.com/alephdata/aleph/wiki/User-manual"><i className="fa fa-fw fa-book" /></a>
+              {' '}
+              <a href="https://github.com/alephdata/aleph/wiki/User-manual">Documentation</a>
+            </span>
+          </p>
+        </footer>              
       </div>
     )
   }
 }
 
-export default withRouter(Screen);
+const mapStateToProps = (state) => {
+  return state;
+};
+
+Screen = connect(
+  mapStateToProps
+)(Screen);
+
+export default Screen;
