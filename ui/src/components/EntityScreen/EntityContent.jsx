88122d6a196065e19690b3480640169160d78bf4
Iain Collins <me@iaincollins.com>
2018-03-12 12:06:12 +0100

    Moved Entity info from EntityContent into sidebar
    It is now visible in the Preview sidebar.

--- ui/src/components/EntityScreen/EntityContent.jsx
+++ ui/src/components/EntityScreen/EntityContent.jsx
@@ -1,12 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { FormattedMessage } from 'react-intl';
-import _ from 'lodash';
 
-import Property from './Property';
 import Entity from './Entity';
 import DualPane from 'src/components/common/DualPane';
-import Date from 'src/components/common/Date';
 import Schema from 'src/components/common/Schema';
 import EntityReferences from './EntityReferences';
 import { fetchEntityReferences } from '../../actions/index';
@@ -22,56 +18,21 @@ class EntityContent extends Component {
   }
 
   render() {
-    const { entity, schema } = this.props;
-
-    const properties = _.values(schema.properties).filter((prop) => {
-      if (prop.caption) {
-        return false;
-      }
-      return prop.featured || !!entity.properties[prop.name];
-    });
+    const { entity } = this.props;
 
     return (
       <DualPane.ContentPane style={{padding: 0}}>
         <div className="content-pane-outer">
           <div className="content-pane-inner EntityContent">
-      
             <div className="EntitySummary">
               <span className="pt-text-muted">
                 <Schema.Label schema={entity.schema} icon={true} />
               </span>
-
               <h1>
                 <Entity.Label entity={entity} addClass={true}/>
               </h1>
             </div>
-      
-            <div style={{padding: '0 20px 20px 20px'}}>
-              <ul className="info-sheet" style={{marginTop: 0}}>
-                <li>
-                </li>
-                { properties.map((prop) => (
-                  <li key={prop.name}>
-                    <span className="key">
-                      <Property.Name model={prop} />
-                    </span>
-                    <span className="value">
-                      <Property.Values model={prop} values={entity.properties[prop.name]} />
-                    </span>
-                  </li>
-                ))}
-                <li>
-                  <span className="key">
-                    <FormattedMessage id="entity.last_updated" defaultMessage="Last updated"/>
-                  </span>
-                  <span className="value">
-                    <Date value={entity.updated_at} />
-                  </span>
-                </li>
-              </ul>
-        
-              <EntityReferences entity={entity} />
-            </div>
+            <EntityReferences entity={entity} />
           </div>
         </div>
       </DualPane.ContentPane>
