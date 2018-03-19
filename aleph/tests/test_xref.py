d5cf6bf81f60b313693a1fff94868d2f883e4146
Friedrich Lindenberg <friedrich@pudo.org>
2018-03-19 19:17:32 +0100

    Try to fix unit tests

--- aleph/tests/test_xref.py
+++ aleph/tests/test_xref.py
@@ -12,9 +12,9 @@ class XrefTestCase(TestCase):
     def setUp(self):
         super(XrefTestCase, self).setUp()
         self.user = self.create_user()
-        self.coll_a = self.create_collection(creator=self.user)
-        self.coll_b = self.create_collection(creator=self.user)
-        self.coll_c = self.create_collection(creator=self.user)
+        self.coll_a = self.create_collection(creator=self.user, managed=True)
+        self.coll_b = self.create_collection(creator=self.user, managed=True)
+        self.coll_c = self.create_collection(creator=self.user, managed=True)
         db.session.commit()
 
     def test_xref(self):
