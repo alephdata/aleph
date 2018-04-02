3d4d683753f1769dcee8d6e91b859e0d4744e833
Emina <eminamuratovic14@gmail.com>
2018-04-02 10:36:15 +0200

    minor changes

--- ui/src/components/ErrorMessages/ErrorScreen.jsx
+++ ui/src/components/ErrorMessages/ErrorScreen.jsx
@@ -28,40 +28,6 @@ class EmptyList extends Component {
   }
 }
 
-class LinkDescription extends Component {
-  render() {
-    const {title, intl, isPage} = this.props;
-    let visual = this.props.visual !== undefined ? this.props.visual : 'error';
-    let description = this.props.description !== undefined ? this.props.description
-      : {id: 'error_desc', defaultMessage: ' '};
-    let main = isPage ? <Screen>
-      <DualPane>
-        <DualPane.ContentPane>
-          <NonIdealState
-            visual={visual}
-            title={intl.formatMessage(title)}
-            description={<a href='/login'>{intl.formatMessage(description)}</a>}
-          />
-        </DualPane.ContentPane>
-      </DualPane>
-    </Screen> : <div className='outer-div'>
-      <div className='inner-div'>
-        <NonIdealState
-          visual={visual}
-          title={intl.formatMessage(title)}
-          description={<a href='/login'>{intl.formatMessage(description)}</a>}
-        />
-      </div>
-    </div>;
-
-    return (
-      <React.Fragment>
-        {main}
-      </React.Fragment>
-    )
-  }
-}
-
 class PageNotFound extends Component {
   render() {
     const {intl, title} = this.props;
@@ -87,7 +53,6 @@ class PageNotFound extends Component {
 class ErrorScreen extends Component {
   static PageNotFound = injectIntl(PageNotFound);
   static EmptyList = injectIntl(EmptyList);
-  static LinkDescription = injectIntl(LinkDescription);
 }
 
 export default ErrorScreen;
--- ui/src/components/Preview/Preview.jsx
+++ ui/src/components/Preview/Preview.jsx
@@ -235,7 +235,7 @@ class Preview extends React.Component {
         top: previewTop,
         bottom: previewBottom
       }}>
-        <ErrorScreen.LinkDescription isPage={false} title={messages.not_authorized} description={messages.not_authorized_decr}/>
+        <ErrorScreen.EmptyList title={messages.not_authorized} description={messages.not_authorized_decr}/>
       </div>
     } else if(collection !== null && collection.error){
       return <div id="Preview" className={className} style={{
