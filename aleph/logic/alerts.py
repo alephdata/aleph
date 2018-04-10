0211f5c7a3ef4b7037f119d9b183ba751dc13dbd
Friedrich Lindenberg <friedrich@pudo.org>
2018-04-10 08:09:24 +0200

    Make alerts match on entities.

--- aleph/logic/alerts.py
+++ aleph/logic/alerts.py
@@ -1,12 +1,14 @@
 import logging
 from pprint import pprint  # noqa
+from banal import ensure_list
+from elasticsearch.helpers import scan
 
 from aleph.authz import Authz
-from aleph.core import db, celery
+from aleph.core import db, es, celery
 from aleph.model import Role, Alert, Events
+from aleph.index.core import entities_index
 from aleph.index.entities import get_entity
-from aleph.index.util import unpack_result
-from aleph.search import AlertDocumentsQuery, SearchQueryParser
+from aleph.index.util import unpack_result, authz_query
 from aleph.logic.notifications import publish
 
 log = logging.getLogger(__name__)
@@ -22,27 +24,68 @@ def check_alerts():
 
 
 def check_alert(authz, alert):
-    entity = get_entity(alert.entity_id) if alert.entity_id else None
-    query = {'q': alert.query_text}
-    state = SearchQueryParser(query, authz)
-    query = AlertDocumentsQuery(state,
-                                entity=entity,
-                                since=alert.notified_at)
-    results = query.search().get('hits')
-    if results.get('total') == 0:
-        return
-
-    for result in results.get('hits', []):
-        document = unpack_result(result)
+    query = alert_query(alert, authz)
+    for result in scan(es, query=query, index=entities_index()):
+        entity = unpack_result(result)
         params = {
             'alert': alert,
             'role': authz.role,
-            'entity': document
+            'entity': entity
         }
         publish(Events.MATCH_ALERT,
-                actor_id=document.get('uploader_id'),
+                actor_id=entity.get('uploader_id'),
                 params=params)
 
     alert.update()
-    log.info('Found %d new results for: %s', results['total'], alert.label)
+    # log.info('Found %d new results for: %s', results['total'], alert.label)
     db.session.commit()
+
+
+def alert_query(alert, authz):
+    """Construct a search query to find new matching entities and documents
+    for a particular alert. Update handling is done via a timestamp of the
+    latest known result."""
+    entity = get_entity(alert.entity_id)
+    clauses = []
+    if entity is None and not alert.query_text:
+        # TODO: do we want to delete the alert here?
+        clauses.append({'match_none': {}})
+
+    if alert.query_text:
+        # Many users have bookmarked complex queries, otherwise we'd use a
+        # precise match query.
+        clauses.append({
+            'simple_query_string': {    
+                'query': alert.query_text,
+                'fields': ['text'],
+                'default_operator': 'AND',
+                'minimum_should_match': '90%'
+            }
+        })
+
+    if entity is not None:
+        TAGS = ['names', 'fingerprints', 'emails', 'phones']
+        for field in TAGS:
+            for value in ensure_list(entity.get(field)):
+                clauses.append({'term': {field: value}})
+                clauses.append({
+                    "multi_match": {
+                        "query": value,
+                        "fields": ['text']
+                    }
+                })
+
+    return {
+        'query': {
+            'bool': {
+                'should': clauses,
+                'filter': [{
+                    'range': {
+                        'created_at': {'gt': alert.notified_at}
+                    }
+                }],
+                'must': [authz_query(authz)],
+                'minimum_should_match': 1
+            }
+        }
+    }
