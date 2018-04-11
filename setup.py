a457140503ea357e43157eac726c6c5bd81d0104
Friedrich Lindenberg <friedrich@pudo.org>
2018-04-11 21:23:47 +0200

    Bump version: 2.0.7 → 2.0.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aleph',
-    version='2.0.7',
+    version='2.0.8',
     description="Document sifting web frontend",
     long_description="",
     classifiers=[
