022b6bf73ca77e5b272f483d625adbf6895fe348
Friedrich Lindenberg <friedrich@pudo.org>
2018-04-12 16:14:05 +0200

    Redirect entities to document path if it’s a document.

--- ui/src/components/Document/DocumentRedirectScreen.jsx
+++ ui/src/components/Document/DocumentRedirectScreen.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Redirect } from 'react-router'
+import { Redirect } from 'react-router';
 
 class DocumentRedirectScreen extends Component {
   render() {
--- ui/src/screens/EntityScreen/EntityScreen.jsx
+++ ui/src/screens/EntityScreen/EntityScreen.jsx
@@ -1,18 +1,26 @@
 import React, {Component} from 'react';
 import {injectIntl} from 'react-intl';
+import { Redirect } from 'react-router';
 
 import { Screen, ScreenLoading, Breadcrumbs, DualPane, Entity } from 'src/components/common';
 import { EntityContent, EntityInfo } from 'src/components/Entity/';
 import ErrorScreen from "../../components/ErrorMessages/ErrorScreen";
+import getPath from 'src/util/getPath';
+
 
 class EntityScreen extends Component {
   render() {
       const {entity} = this.props;
 
       if (entity.error) {
-          return (
-              <ErrorScreen.NoTranslation title={entity.error}/>
-          )
+        return (
+          <ErrorScreen.NoTranslation title={entity.error}/>
+        )
+      }
+
+      // If the entity is a document, forward to the document page.
+      if (entity && entity.schemata && entity.schemata.indexOf('Document') !== -1) {
+        return <Redirect to={getPath(entity.links.ui)} />
       }
 
       const breadcrumbs = (<Breadcrumbs collection={entity.collection}>
