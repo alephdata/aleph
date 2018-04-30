89afcdaed186e4ed96251b38431af234405d3633
Friedrich Lindenberg <friedrich@pudo.org>
2018-04-30 16:16:02 +0200

    Extract alert toggle into component, fixes #367.

--- ui/src/components/Navbar/Navbar.jsx
+++ ui/src/components/Navbar/Navbar.jsx
@@ -4,13 +4,11 @@ import { withRouter } from 'react-router';
 import { Link } from 'react-router-dom';
 import { defineMessages, injectIntl, FormattedMessage } from 'react-intl';
 import queryString from 'query-string';
-import { ControlGroup, InputGroup, Intent, Switch, Position, Tooltip } from "@blueprintjs/core";
-import classNames from 'classnames';
+import { ControlGroup, InputGroup } from "@blueprintjs/core";
 
+import SearchAlert from 'src/components/SearchAlert/SearchAlert';
 import AuthButtons from 'src/components/AuthButtons/AuthButtons';
 import LanguageMenu from 'src/components/LanguageMenu/LanguageMenu';
-import {addAlert, deleteAlert, fetchAlerts} from 'src/actions';
-import {showSuccessToast, showInfoToast} from "src/app/toast";
 
 import './Navbar.css';
 
@@ -18,22 +16,6 @@ const messages = defineMessages({
   search_placeholder: {
     id: 'navbar.search_placeholder',
     defaultMessage: 'Search companies, people and documents.',
-  },
-  alert_added: {
-    id: 'navbar.alert_added',
-    defaultMessage: 'You will receive alerts for new results'
-  },
-  alert_removed: {
-    id: 'navbar.alert_removed',
-    defaultMessage: 'You will no longer receive alerts for this search'
-  },
-  alert: {
-    id: 'navbar.alert',
-    defaultMessage: 'Alert'
-  },
-  alert_add: {
-    id: 'navbar.alert_add',
-    defaultMessage: 'Add alert'
   }
 });
 
@@ -44,18 +26,12 @@ class Navbar extends React.Component {
 
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
-    this.onAddAlert = this.onAddAlert.bind(this);
-    this.onRemoveAlert = this.onRemoveAlert.bind(this);
-    this.alertExists = this.alertExists.bind(this);
   }
 
   componentDidMount() {
     const { query } = this.props;
     if (query !== undefined) {
-      this.setState({
-        searchValue: query.getString('q'),
-        alertChangeProgress: false
-      })
+      this.setState({ searchValue: query.getString('q') })
     }
   }
 
@@ -65,14 +41,12 @@ class Navbar extends React.Component {
     }
       
     if (nextProps.query && nextProps.query.state.q !== this.state.searchValue) {
-      this.setState({
-        searchValue: nextProps.query.getString('q')
-      })
+      this.setState({ searchValue: nextProps.query.getString('q') })
     }
   }
   
   onChange({ target }) {
-    this.setState({searchValue: target.value})
+    this.setState({ searchValue: target.value })
   }
 
   onSubmit(event) {
@@ -90,85 +64,10 @@ class Navbar extends React.Component {
       })
     }
   }
-
-  alertExists() {
-    const { alerts } = this.props;
-    const {searchValue} = this.state;
-    // @FIXME The try/catch here is temporary to confirm this hotfix works
-    // and workaround the issue temporarily in case it doesn't.
-    try {
-      if (!alerts || !alerts.results || !searchValue)
-        return false;
-
-      return alerts.results.some((a) => {
-        return a.query_text && a.query_text.trim() === searchValue.trim();
-      });
-    } catch (e) {
-      console.log(e);
-      return false;
-    }
-  }
-
-  async onAddAlert(event) {
-    const {intl} = this.props;
-    const {searchValue} = this.state;
-    
-    this.setState({ alertChangeProgress: true });
-    
-    if (this.alertExists())
-      return this.setState({ alertChangeProgress: false });
-
-    event.preventDefault();
-    await this.props.addAlert({query_text: searchValue});
-    await this.props.fetchAlerts();
-    showSuccessToast(intl.formatMessage(messages.alert_added));
-    this.setState({ alertChangeProgress: false });
-  }
-  
-  async onRemoveAlert(event) {
-    const {intl, alerts, deleteAlert, fetchAlerts} = this.props;
-    const {searchValue} = this.state;
-
-    event.preventDefault();
-
-    if (!alerts || !alerts.results)
-      return false;
-
-    this.setState({ alertChangeProgress: true });
-      
-    let alertDeleted = false;
-    alerts.results.forEach((a) => {
-      if (a.query_text.trim() === searchValue.trim()) {
-        deleteAlert(a.id);
-        alertDeleted = true;
-      }
-    });
-
-    if (alertDeleted === true) {
-      await fetchAlerts();
-      showInfoToast(intl.formatMessage(messages.alert_removed));
-    }
-    this.setState({ alertChangeProgress: false });
-  }
   
   render() {
-    const {metadata, session, intl, isHomepage} = this.props;
-    const {searchValue, alertChangeProgress} = this.state;
-    
-    const alertExists = this.alertExists();
-    
-    // Only show alert toggle when there is a search value and user logged in
-    // @TODO Create option to inform users they can use alerts if signed in
-    let showAlertToggle = false;
-    if (searchValue && session && session.loggedIn === true) {
-      showAlertToggle = true;
-    }
-    
-    // Don't let users interact with the button when alerts are being updated
-    let alertButtonOnClick = () => { return; };
-    if (!alertChangeProgress) {
-      alertButtonOnClick = alertExists ? this.onRemoveAlert : this.onAddAlert;
-    }
+    const { metadata, session, intl, isHomepage } = this.props;
+    const { searchValue } = this.state;
     
     return (
       <div id="Navbar" className="Navbar">
@@ -191,22 +90,9 @@ class Navbar extends React.Component {
                     className="pt-large"
                     onChange={this.onChange} value={searchValue}
                     placeholder={intl.formatMessage(messages.search_placeholder)}
-                    /*rightElement={<button className="pt-button pt-minimalx pt-icon-arrow-right"></button>}*/
+                    rightElement={<SearchAlert queryText={searchValue} />}
                   />
                 </ControlGroup>
-                {showAlertToggle && (
-                  <div className="alert-toggle">
-                    <Tooltip position={Position.RIGHT} intent={Intent.PRIMARY} content={
-                      <FormattedMessage id="nav.alerts_tooltip" defaultMessage="Receive alerts for new results"/>
-                    }>
-                      <div className={classNames('pt-button pt-minimal', alertExists ? 'pt-intent-primary' : '')}
-                           onClick={alertButtonOnClick}>
-                        <span className={classNames('pt-icon-notifications', alertExists ? 'selected' : 'pt-text-muted')}/>
-                        <Switch className='alerts-tooltip' checked={alertExists} disabled={alertChangeProgress}/>
-                      </div>
-                    </Tooltip>
-                  </div>
-                )}
               </form>
             )} 
           </div>
@@ -225,12 +111,10 @@ class Navbar extends React.Component {
 }
 
 const mapStateToProps = (state, ownProps) => {
-  return {
-    alerts: state.alerts,
-    session: state.session
-  }
+  return { session: state.session };
 };
 
+Navbar = connect(mapStateToProps, {})(Navbar);
 Navbar = injectIntl(Navbar);
 Navbar = withRouter(Navbar);
-export default connect(mapStateToProps, {addAlert, deleteAlert, fetchAlerts})(Navbar);
+export default Navbar;
---
+++ ui/src/components/SearchAlert/SearchAlert.jsx
@@ -0,0 +1,108 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
+import { defineMessages, injectIntl } from 'react-intl';
+import { Tooltip } from "@blueprintjs/core";
+import c from 'classnames';
+
+import { addAlert, deleteAlert, fetchAlerts } from 'src/actions';
+
+
+const messages = defineMessages({
+  alert_add: {
+    id: 'navbar.alert_add',
+    defaultMessage: 'You will receive alerts for new results.'
+  },
+  alert_remove: {
+    id: 'navbar.alert_remove',
+    defaultMessage: 'You are receiving alerts about this search.'
+  }
+});
+
+
+class SearchAlert extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {updating: false};
+    this.onToggleAlert = this.onToggleAlert.bind(this);
+  }
+
+  shouldComponentUpdate(nextProps) {
+    return !this.props.alerts ||
+      this.props.alerts.total !== nextProps.alerts.total ||
+      this.props.queryText !== nextProps.queryText;
+  }
+
+  componentDidMount() {
+    const { alerts } = this.props;
+    if (!alerts || !alerts.results) {
+      this.props.fetchAlerts();
+    }
+  }
+
+  alertExists() {
+    const { queryText, alerts } = this.props;
+    if (!alerts || !alerts.results || !queryText) {
+      return false;
+    }
+    return !!alerts.results.some((a) => {
+      return a.query_text && a.query_text.trim() === queryText.trim();
+    });
+  }
+
+  async onToggleAlert(event) {
+    event.preventDefault();
+    const { alerts, queryText } = this.props;
+    const alertExists = this.alertExists();
+
+    if (!alerts || !alerts.results || this.state.updating) {
+      return false;
+    }
+
+    this.setState({updating: true});
+    if (alertExists) {
+      for (let alert of alerts.results) {
+        if (alert.query_text.trim() === queryText.trim()) {
+          await this.props.deleteAlert(alert.id);
+          await this.props.fetchAlerts();
+        }
+      }
+    } else {
+      await this.props.addAlert({query_text: queryText.trim()});
+      await this.props.fetchAlerts();
+    }
+    this.setState({updating: false});
+  }
+  
+  render() {
+    const { queryText, session, intl } = this.props;
+    if (!session.loggedIn || !queryText || !queryText.trim().length) {
+      return null;
+    }
+    const alertExists = this.alertExists();
+    const className = c('pt-button',
+                        'pt-minimal',
+                        'pt-icon-notifications',
+                        {'pt-intent-success': alertExists},
+                        {'pt-disabled': this.state.updating});
+    const tooltip = alertExists ? intl.formatMessage(messages.alert_remove)
+                                : intl.formatMessage(messages.alert_add);
+    return (
+      <Tooltip content={tooltip}>
+        <button className={className} type="button" onClick={this.onToggleAlert} />
+      </Tooltip>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    alerts: state.alerts,
+    session: state.session,
+  }
+};
+
+SearchAlert = connect(mapStateToProps, {addAlert, deleteAlert, fetchAlerts})(SearchAlert)
+SearchAlert = injectIntl(SearchAlert);
+SearchAlert = withRouter(SearchAlert);
+export default SearchAlert;
