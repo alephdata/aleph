0bf4721eb18961b3a4eb3fefde7a5aa73a856b7f
Emina <eminamuratovic14@gmail.com>
2018-05-11 14:18:51 +0200

    working on file upload screen, made case screen and added file upload react package

--- ui/src/components/Case/CaseDocumentsContent.jsx
+++ ui/src/components/Case/CaseDocumentsContent.jsx
@@ -1,17 +1,59 @@
 import React, { Component } from 'react';
 
 import CaseScreen from 'src/screens/CaseScreen/CaseScreen';
+import { ScreenLoading, Collection } from 'src/components/common';
+import { selectCollectionsResult } from "../../selectors";
+import Query from "../../app/Query";
+import { queryCollections } from "../../actions";
+import { connect } from "react-redux";
+import FileDrop from 'react-file-drop';
+
+import './CaseDocumentsContent.css';
 
 class CaseDocumentsContent extends Component {
+
+  handleDrop(files, event) {
+    console.log('drop', files, event)
+  }
+
   render() {
     const { collection } = this.props;
+    console.log(collection)
 
     return (
-      <CaseScreen>
-        <div>bla bla</div>
+      <CaseScreen collection={collection} result={this.props.result}>
+        <div className="react-file-drop-demo">
+          <FileDrop onDrop={this.handleDrop}>
+            Drop some files here!
+          </FileDrop>
+        </div>
       </CaseScreen>
     );
   }
 }
 
-export default CaseDocumentsContent;
+const mapStateToProps = (state, ownProps) => {
+  const {location} = ownProps;
+  const context = {
+    facet: [ 'category', 'countries' ],
+    'filter:kind': 'casefile'
+  };
+  const query = Query.fromLocation('collections', location, context, 'collections')
+    .sortBy('count', true)
+    .limit(30);
+
+  return {
+    query: query,
+    result: selectCollectionsResult(state, query)
+  };
+};
+
+CaseDocumentsContent = connect(mapStateToProps, {queryCollections})(CaseDocumentsContent);
+export default ({ match, ...otherProps }) => (
+  <Collection.Load
+    id={match.params.collectionId}
+    renderWhenLoading={<ScreenLoading />}
+  >{collection => (
+    <CaseDocumentsContent collection={collection} {...otherProps} />
+  )}</Collection.Load>
+);
--- ui/src/screens/CaseScreen/CaseScreen.jsx
+++ ui/src/screens/CaseScreen/CaseScreen.jsx
@@ -2,11 +2,10 @@ import React, { Component } from 'react';
 import { injectIntl } from 'react-intl';
 import { connect } from 'react-redux';
 
-import { Screen, ScreenLoading, Breadcrumbs, DualPane, Collection, ErrorScreen } from 'src/components/common';
-import { CaseInfo, CaseContent, CaseDocumentsContent } from 'src/components/Case';
+import { Screen, Breadcrumbs, DualPane, ErrorScreen } from 'src/components/common';
+import { CaseInfo, CaseContent } from 'src/components/Case';
 import { queryCollections } from "src/actions";
-import { selectCollectionsResult } from "src/selectors";
-import Query from "src/app/Query";
+import { withRouter } from "react-router";
 
 class CaseScreen extends Component {
   constructor(props) {
@@ -18,18 +17,12 @@ class CaseScreen extends Component {
   }
 
   async componentDidMount() {
-    await this.fetchData();
-  }
-
-  async fetchData() {
-    let {query} = this.props;
-    this.props.queryCollections({query});
     this.setState({result: this.props.result})
   }
 
   render() {
     const {collection, result} = this.props;
-    console.log(this.props);
+    //console.log(this.props);
 
     if (collection.isError) {
       return <ErrorScreen error={collection.error}/>;
@@ -50,28 +43,11 @@ class CaseScreen extends Component {
 }
 
 const mapStateToProps = (state, ownProps) => {
-  const {location} = ownProps;
-  const context = {
-    facet: [ 'category', 'countries' ],
-    'filter:kind': 'casefile'
-  };
-  const query = Query.fromLocation('collections', location, context, 'collections')
-    .sortBy('count', true)
-    .limit(30);
-
   return {
-    query: query,
-    result: selectCollectionsResult(state, query)
   };
 };
 
 CaseScreen = injectIntl(CaseScreen);
+CaseScreen = withRouter(CaseScreen);
 CaseScreen = connect(mapStateToProps, {queryCollections})(CaseScreen);
-export default ({ match, ...otherProps }) => (
-  <Collection.Load
-    id={match.params.collectionId}
-    renderWhenLoading={<ScreenLoading />}
-  >{collection => (
-    <CaseScreen collection={collection} {...otherProps} />
-  )}</Collection.Load>
-);
+export default (CaseScreen);
