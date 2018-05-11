1f26a8c399d644fb0463ff8727f4e67555102761
Emina <eminamuratovic14@gmail.com>
2018-05-11 14:18:51 +0200

    working on file upload screen, made case screen and added file upload react package

--- ui/src/screens/CaseScreen/CaseScreen.jsx
+++ ui/src/screens/CaseScreen/CaseScreen.jsx
@@ -2,11 +2,18 @@ import React, { Component } from 'react';
 import { injectIntl } from 'react-intl';
 import { connect } from 'react-redux';
 
+<<<<<<< HEAD
 import { Screen, ScreenLoading, Breadcrumbs, DualPane, Collection, ErrorScreen } from 'src/components/common';
 import { CaseInfo, CaseContent, CaseDocumentsContent } from 'src/components/Case';
 import { queryCollections } from "../../actions";
 import { selectCollectionsResult } from "../../selectors";
 import Query from "src/app/Query";
+=======
+import { Screen, Breadcrumbs, DualPane, ErrorScreen } from 'src/components/common';
+import { CaseInfo, CaseContent } from 'src/components/Case';
+import { queryCollections } from "src/actions";
+import { withRouter } from "react-router";
+>>>>>>> working on file upload screen, made case screen and added file upload react package
 
 class CaseScreen extends Component {
   constructor(props) {
@@ -18,18 +25,12 @@ class CaseScreen extends Component {
   }
 
   async componentDidMount() {
-    await this.fetchData();
-  }
-
-  async fetchData() {
-    let {query} = this.props;
-    this.props.queryCollections({query});
     this.setState({result: this.props.result})
   }
 
   render() {
     const {collection, result} = this.props;
-    console.log(this.props);
+    //console.log(this.props);
 
     if (collection.isError) {
       return <ErrorScreen error={collection.error}/>;
@@ -58,20 +59,11 @@ const mapStateToProps = (state, ownProps) => {
   const query = Query.fromLocation('collections', location, context, 'collections')
     .sortBy('count', true)
     .limit(30);
-
   return {
-    query: query,
-    result: selectCollectionsResult(state, query)
   };
 };
 
 CaseScreen = injectIntl(CaseScreen);
+CaseScreen = withRouter(CaseScreen);
 CaseScreen = connect(mapStateToProps, {queryCollections})(CaseScreen);
-export default ({ match, ...otherProps }) => (
-  <Collection.Load
-    id={match.params.collectionId}
-    renderWhenLoading={<ScreenLoading />}
-  >{collection => (
-    <CaseScreen collection={collection} {...otherProps} />
-  )}</Collection.Load>
-);
+export default (CaseScreen);
