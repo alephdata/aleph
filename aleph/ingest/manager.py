dbdc265d1829970b0c9f7b68a57d23062f54fca6
Friedrich Lindenberg <friedrich@pudo.org>
2018-05-19 09:55:01 +0200

    Refactor document ingest methods.

--- aleph/ingest/manager.py
+++ aleph/ingest/manager.py
@@ -1,9 +1,7 @@
-import os
 import logging
-from tempfile import mkdtemp
 
 from ingestors import Manager
-from ingestors.util import decode_path, remove_directory
+from ingestors.util import decode_path
 
 from aleph.core import db, settings
 from aleph.model import Document, Cache
@@ -34,13 +32,7 @@ class DocumentManager(Manager):
         result.document.delete_records()
 
     def after(self, result):
-        from aleph.logic.documents import process_document
         result.update()
-        db.session.commit()
-        document = result.document
-        log.debug('Ingested [%s:%s]: %s',
-                  document.id, document.schema, document.title)
-        process_document(document)
 
     def get_cache(self, key):
         return Cache.get_cache(key)
@@ -61,47 +53,5 @@ class DocumentManager(Manager):
         doc.mime_type = mime_type or doc.meta.get('mime_type')
 
         from aleph.ingest import ingest_document
-        ingest_document(doc, file_path, role_id=parent.role_id)
-        return DocumentResult(self, doc,
-                              file_path=file_path,
-                              role_id=parent.role_id)
-
-    def ingest_document(self, document, file_path=None,
-                        role_id=None, shallow=False):
-        """Ingest a database-backed document.
-
-        First retrieve its data and then call the actual ingestor.
-        """
-        # Work path will be used by storagelayer to cache a local
-        # copy of data from an S3-based archive, and by ingestors
-        # to perform processing and generate intermediary files.
-        work_path = mkdtemp(prefix="aleph.ingest.")
-        content_hash = document.content_hash
-        if file_path is None and content_hash is not None:
-            file_name = document.safe_file_name
-            file_path = self.archive.load_file(content_hash,
-                                               file_name=file_name,
-                                               temp_path=work_path)
-
-        if file_path is not None and not os.path.exists(file_path):
-            # Probably indicative of file system encoding issues.
-            log.error("Invalid path [%r]: %s", document, file_path)
-            return
-
-        try:
-            result = DocumentResult(self, document,
-                                    file_path=file_path,
-                                    role_id=role_id)
-            self.ingest(file_path, result=result)
-
-            if not shallow and file_path is None:
-                # When a directory is ingested, the data is not stored. Thus,
-                # try to recurse on the database-backed known children.
-                for child in Document.by_parent(document):
-                    from aleph.ingest import ingest_document
-                    ingest_document(child, None, role_id=role_id)
-        finally:
-            db.session.rollback()
-            # Removing the temp_path given to storagelayer makes it redundant
-            # to also call cleanup on the archive.
-            remove_directory(work_path)
+        ingest_document(doc, file_path, role_id=parent.document.uploader_id)
+        return DocumentResult(self, doc, file_path=file_path)
--- aleph/logic/documents.py
+++ aleph/logic/documents.py
@@ -12,7 +12,6 @@ log = logging.getLogger(__name__)
 def update_document(document):
     # These are operations that should be executed after each
     # write to a document or its metadata.
-    process_document_id.delay(document.id)
     index.index_document(document)
 
 
@@ -23,12 +22,6 @@ def delete_document(document, deleted_at=None):
 
     index.delete_document(document.id)
     document.delete(deleted_at=deleted_at)
-
-
-@celery.task()
-def process_document_id(document_id):
-    """Perform post-ingest tasks like analysis and indexing."""
-    analyze_document(Document.by_id(document_id))
 
 
 def process_document(document):
