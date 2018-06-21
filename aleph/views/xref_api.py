c5d02783ccc53c651769e58c8d2c7b97d9a6bbfa
Friedrich Lindenberg <friedrich@pudo.org>
2018-06-21 12:46:52 +0200

    Remove fixed page size.

--- aleph/views/xref_api.py
+++ aleph/views/xref_api.py
@@ -13,7 +13,7 @@ blueprint = Blueprint('xref_api', __name__)
 @blueprint.route('/api/2/collections/<int:id>/xref', methods=['GET'])
 def index(id):
     collection = get_db_collection(id)
-    parser = QueryParser(request.args, request.authz, limit=10)
+    parser = QueryParser(request.args, request.authz)
     q = Match.group_by_collection(collection.id, authz=request.authz)
     result = DatabaseQueryResult(request, q,
                                  parser=parser,
