c7878da47fd2664ce07147cabb5dafdb4acd5c22
Friedrich Lindenberg <friedrich@pudo.org>
2018-07-11 14:53:14 +0200

    Handle app context in text iterator correctly for gRPC threading.

--- aleph/analyze/analyzer.py
+++ aleph/analyze/analyzer.py
@@ -1,7 +1,9 @@
 import logging
+from flask import current_app
 
+from aleph import settings
 from aleph.model import DocumentTagCollector
-
+from alephclient.services.common_pb2 import Text
 
 log = logging.getLogger(__name__)
 
@@ -30,3 +32,21 @@ class EntityAnalyzer(Analyzer):
 
     def extract(self, collector, document):
         pass
+
+
+class TextIterator(object):
+    MIN_LENGTH = 100
+
+    def text_iterator(self, document):
+        app = current_app._get_current_object()
+        return self._text_iterator(app, document)
+
+    def _text_iterator(self, app, document):
+        with app.app_context():
+            languages = list(document.languages)
+            if not len(languages):
+                languages = [settings.DEFAULT_LANGUAGE]
+            for text in document.texts:
+                if text is None or len(text) <= self.MIN_LENGTH:
+                    continue
+                yield Text(text=text, languages=languages)
--- aleph/analyze/extract_entity.py
+++ aleph/analyze/extract_entity.py
@@ -2,18 +2,16 @@ import grpc
 import logging
 
 from aleph import settings
-from aleph.analyze.analyzer import EntityAnalyzer
+from aleph.analyze.analyzer import EntityAnalyzer, TextIterator
 from aleph.model import DocumentTag
 from alephclient.services.entityextract_pb2_grpc import EntityExtractStub
 from alephclient.services.entityextract_pb2 import ExtractedEntity
-from alephclient.services.common_pb2 import Text
 
 log = logging.getLogger(__name__)
 TYPE = ExtractedEntity.Type.Value
 
 
-class EntityExtractor(EntityAnalyzer):
-    MIN_LENGTH = 100
+class EntityExtractor(EntityAnalyzer, TextIterator):
     TYPES = {
         TYPE('PERSON'): DocumentTag.TYPE_PERSON,
         TYPE('ORGANIZATION'): DocumentTag.TYPE_ORGANIZATION,
@@ -23,22 +21,13 @@ class EntityExtractor(EntityAnalyzer):
     def __init__(self):
         self.active = self.SERVICE is not None
 
-    def make_iterator(self, document):
-        languages = list(document.languages)
-        if not len(languages):
-            languages = [settings.DEFAULT_LANGUAGE]
-        for text in document.texts:
-            if len(text) <= self.MIN_LENGTH:
-                continue
-            text = Text(text=text, languages=languages)
-            yield text
-
     def extract(self, collector, document):
         try:
             channel = grpc.insecure_channel(self.SERVICE)
             service = EntityExtractStub(channel)
-            entities = service.Extract(self.make_iterator(document)).entities
-            for entity in entities:
+            texts = self.text_iterator(document)
+            entities = service.Extract(texts)
+            for entity in entities.entities:
                 type_ = self.TYPES.get(entity.type)
                 if type_ is None:
                     continue
