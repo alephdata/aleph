03116745aed747d153dd34dee3a37a22bd655c7d
Ueland <tor.henning@gmail.com>
2018-07-25 10:01:41 +0200

    Working OAuth integration against Azure with certificate verification

--- aleph/oauth.py
+++ aleph/oauth.py
@@ -1,5 +1,10 @@
 import jwt
+from urllib.request import urlopen
+import base64
+import json
 import logging
+from cryptography.x509 import load_pem_x509_certificate
+from cryptography.hazmat.backends import default_backend
 from flask_oauthlib.client import OAuth
 
 from aleph import signals, settings
@@ -33,9 +38,30 @@ def handle_azure_oauth(sender, provider=None, oauth=None):
     if 'login.microsoftonline.com' not in provider.base_url:
         return
 
-    token_data = jwt.decode(oauth.get('id_token'), verify=False) # TODO: verify=True
+    # Get incoming token, extract header for use with certificate verification
+    id_token = oauth.get('id_token')
+    headerbit = id_token.split('.')[0]
+    headerbit = base64.b64decode(headerbit).decode('utf8')
+    headerbit = json.loads(headerbit)
 
+    # Load cert from MS - can be cached for upwards of 24hrs, not done now
+    cert_loc = 'https://login.microsoftonline.com/common/discovery/keys'
+    cert_data= json.loads(urlopen(cert_loc).read())
+    pemstart = "-----BEGIN CERTIFICATE-----\n"
+    pemend = "\n-----END CERTIFICATE-----\n"
+    # Find correct cert based on header
+    for key in cert_data['keys']:
+        if headerbit['kid'] == key['kid'] and headerbit['x5t'] == key['x5t']:
+            mspubkey = key['x5c'][0]
+            break
+    cert_str = pemstart + mspubkey + pemend
+    cert_obj = load_pem_x509_certificate(cert_str.encode('ascii'), default_backend())
+    public_key = cert_obj.public_key()
 
+    # Decode incoming token and verify against the MS cert
+    token_data = jwt.decode(id_token, public_key, verify=True, audience=settings.OAUTH_KEY)
+
+    # All Ok, move on
     user_id = 'azure:%s' % token_data['upn']
     return Role.load_or_create(user_id, Role.USER, token_data['name'],
                                email=token_data['upn'])
