40b1036c90814977c13384bd19f55a1f384c9988
Friedrich Lindenberg <friedrich@pudo.org>
2018-08-12 14:27:28 +0200

    make session id a request property

--- aleph/logic/audit.py
+++ aleph/logic/audit.py
@@ -1,6 +1,4 @@
 import logging
-from banal import hash_data
-
 from flask import request
 
 from aleph.core import db, celery
@@ -9,20 +7,13 @@ from aleph.model import Audit
 log = logging.getLogger(__name__)
 
 
-def _get_session_id(request):
-    session_id = request.headers.get('X-Aleph-Session')
-    if not session_id:
-        session_id = hash_data([request.remote_addr,
-                                request.accept_languages,
-                                request.user_agent])
-    return session_id
-
-
 def record_audit(activity, keys=None, **data):
     keys = keys or data.keys()
-    session_id = _get_session_id(request)
-    role_id = request.authz.id
-    record_audit_task.delay(activity, session_id, role_id, data, keys)
+    record_audit_task.delay(activity,
+                            request.session_id,
+                            request.authz.id,
+                            data,
+                            keys)
 
 
 @celery.task(priority=2)
