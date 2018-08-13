b2d7072c40abf5b3279f2b609f11b4560370003c
Friedrich Lindenberg <friedrich@pudo.org>
2018-08-13 08:05:33 +0200

    and one more

--- aleph/queues.py
+++ aleph/queues.py
@@ -7,6 +7,7 @@ from aleph.logic.documents import ingest, process_documents  # noqa
 from aleph.logic.scheduled import hourly, daily  # noqa
 from aleph.logic.xref import xref_collection # noqa
 from aleph.logic.alerts import check_alerts  # noqa
+from aleph.logic.audit import record_audit_task  # noqa
 
 flask_app = create_app()
 flask_app.app_context().push()
