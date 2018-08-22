8cfba3fe26268864f4a643d25d409829f48ba0b8
Friedrich Lindenberg <friedrich@pudo.org>
2018-08-22 11:11:10 +0200

    revert lock-based ocr thing.

--- services/recognize-text/textrecognizer/recognize.py
+++ services/recognize-text/textrecognizer/recognize.py
@@ -1,4 +1,6 @@
+import time
 import logging
+import threading
 from PIL import Image
 from io import BytesIO
 from languagecodes import list_to_alpha3 as alpha3
@@ -11,11 +13,13 @@ class OCR(object):
     MAX_MODELS = 5
     MIN_WIDTH = 10
     MIN_HEIGHT = 10
+    DEFAULT_MODE = PSM.AUTO_OSD
+    # DEFAULT_MODE = PSM.AUTO
 
     def __init__(self):
         # Tesseract language types:
         _, self.supported = get_languages()
-        self.reset_engine('eng')
+        self.t = threading.local()
 
     def language_list(self, languages):
         models = [c for c in alpha3(languages) if c in self.supported]
@@ -25,17 +29,21 @@ class OCR(object):
         models.append('eng')
         return '+'.join(sorted(set(models)))
 
-    def reset_engine(self, languages):
-        if hasattr(self, 'api'):
-            self.api.Clear()
-            self.api.End()
-        self.api = PyTessBaseAPI(lang=languages, oem=OEM.LSTM_ONLY)
+    def configure_engine(self, languages, mode):
+        log.info("Configuring OCR engine (%s)", languages)
+        if not hasattr(self.t, 'api'):
+            self.t.api = PyTessBaseAPI(lang=languages, oem=OEM.LSTM_ONLY)
+        if languages != self.t.api.GetInitLanguagesAsString():
+            self.t.api.Init(lang=languages, oem=OEM.LSTM_ONLY)
+        if mode != self.t.api.GetPageSegMode():
+            self.t.api.SetPageSegMode(mode)
+        return self.t.api
 
-    def extract_text(self, data, languages=None, mode=PSM.AUTO_OSD):
+    def extract_text(self, data, languages=None, mode=DEFAULT_MODE):
         """Extract text from a binary string of data."""
+        start_time = time.time()
         languages = self.language_list(languages)
-        if languages != self.api.GetInitLanguagesAsString():
-            self.reset_engine(languages)
+        api = self.configure_engine(languages, mode)
 
         try:
             image = Image.open(BytesIO(data))
@@ -45,17 +53,16 @@ class OCR(object):
             if image.height <= self.MIN_HEIGHT:
                 return
 
-            if mode != self.api.GetPageSegMode():
-                self.api.SetPageSegMode(mode)
-
-            self.api.SetImage(image)
-            text = self.api.GetUTF8Text()
-            confidence = self.api.MeanTextConf()
-            log.info("%s chars (w: %s, h: %s, langs: %s, confidence: %s)",
+            api.SetImage(image)
+            text = api.GetUTF8Text()
+            confidence = api.MeanTextConf()
+            end_time = time.time()
+            duration = end_time - start_time
+            log.info("%s chars (w: %s, h: %s, langs: %s, c: %s), took: %.5f",
                      len(text), image.width, image.height, languages,
-                     confidence)
+                     confidence, duration)
             return text
         except Exception as ex:
             log.exception("Failed to OCR: %s", languages)
         finally:
-            self.api.Clear()
+            api.Clear()
