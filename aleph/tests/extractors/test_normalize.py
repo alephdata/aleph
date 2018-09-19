3cf0a6d4fbb88b939774fc5b1de667c566b099e5
Friedrich Lindenberg <friedrich@pudo.org>
2018-09-19 15:48:48 +0200

    try and fix ner tests

--- aleph/tests/extractors/test_normalize.py
+++ aleph/tests/extractors/test_normalize.py
@@ -1,19 +1,15 @@
 from aleph.tests.util import TestCase
 
-from aleph.logic.extractors.result import Result
+from aleph.logic.extractors.result import NamedResult
 
 
 class TestNormalize(TestCase):
 
     def test_clean_name(self):
-        assert Result.clean_name('  ') is None
-        assert Result.clean_name(None) is None
-        assert Result.clean_name('xx') is None  # too short
-        assert Result.clean_name('Mr. Clean and Proper') == 'Clean and Proper'
-        assert Result.clean_name('The') is None  # single token
-        assert Result.clean_name('The Thing Bling') == 'Thing Bling'
-        assert Result.clean_name('of The Thing Bling') == 'Thing Bling'
-
-    def test_label_key(self):
-        assert Result.label_key('') is None
-        assert Result.label_key('BANANA') == 'banana'
+        assert NamedResult.clean_name('  ') is None
+        assert NamedResult.clean_name(None) is None
+        assert NamedResult.clean_name('xx') is None  # too short
+        assert NamedResult.clean_name('Mr. Clean and Proper') == 'Clean and Proper'
+        assert NamedResult.clean_name('The') is None  # single token
+        assert NamedResult.clean_name('The Thing Bling') == 'Thing Bling'
+        assert NamedResult.clean_name('of The Thing Bling') == 'Thing Bling'
