8609c75be1ce68348a66f24788ab2e3b768724db
Friedrich Lindenberg <friedrich@pudo.org>
2018-10-12 11:45:56 +0300

    Fix doc redirect

--- ui/src/screens/CollectionDocumentsScreen/CollectionDocumentsScreen.jsx
+++ ui/src/screens/CollectionDocumentsScreen/CollectionDocumentsScreen.jsx
@@ -5,7 +5,7 @@ import { Redirect } from 'react-router';
 class CollectionDocumentsScreen extends Component {
   render() {
     const { collectionId } = this.props.match.params;
-    return <Redirect to={`/collections/${collectionId}#mode=browse`} />;
+    return <Redirect to={`/collections/${collectionId}#mode=Document`} />;
   }
 }
 
