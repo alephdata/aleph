afe589487d611f61222c4420cc4e39391896a701
Friedrich Lindenberg <friedrich@pudo.org>
2018-10-12 09:44:09 +0300

    Clean up imports

--- ui/src/components/Collection/CollectionEntitiesMode.jsx
+++ ui/src/components/Collection/CollectionEntitiesMode.jsx
@@ -8,13 +8,13 @@ import EntitySearch from 'src/components/EntitySearch/EntitySearch';
 
 class CollectionEntitiesMode extends React.Component {
   render() {
-    const { collection, query } = this.props;
+    const { query } = this.props;
     return <EntitySearch query={query} hideCollection={true} />;
   }
 }
 
 const mapStateToProps = (state, ownProps) => {
-  const { collection, location, activeMode } = ownProps;
+  const { location, activeMode } = ownProps;
   const context = {
     'filter:schema': activeMode
   };
