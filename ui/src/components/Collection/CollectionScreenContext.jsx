b92e5fc6406f1441ba252084d098837799f709a3
Friedrich Lindenberg <friedrich@pudo.org>
2018-10-12 09:58:46 +0300

    Hide search if inactive

--- ui/src/components/Collection/CollectionScreenContext.jsx
+++ ui/src/components/Collection/CollectionScreenContext.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {defineMessages, FormattedMessage, injectIntl} from 'react-intl';
+import { defineMessages, injectIntl } from 'react-intl';
 import queryString from 'query-string';
 import { withRouter } from 'react-router';
 import { connect } from 'react-redux';
