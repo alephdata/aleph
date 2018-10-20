54f94c15930137431c0e177a76f1362bbf26bce5
Friedrich Lindenberg <friedrich@pudo.org>
2018-10-20 21:53:36 +0200

    Fix var name in streaming API

--- aleph/views/stream_api.py
+++ aleph/views/stream_api.py
@@ -24,8 +24,8 @@ def entities(collection_id=None):
     includes = ensure_list(request.args.getlist('include'))
     includes = [f for f in includes if f not in excludes]
     if collection_id is not None:
-        get_db_collection(id, request.authz.READ)
-        record_audit(Audit.ACT_COLLECTION, id=id)
+        get_db_collection(collection_id, request.authz.READ)
+        record_audit(Audit.ACT_COLLECTION, id=collection_id)
     entities = iter_entities(authz=request.authz,
                              collection_id=collection_id,
                              schemata=schemata,
