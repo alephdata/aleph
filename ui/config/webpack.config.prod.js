d6934574284eb5368642a29d068c488c8eddcc96
Davit Khurshudyan <davo11122@gmail.com>
2018-11-05 16:45:40 +0400

    Code cleanup

--- ui/config/webpack.config.prod.js
+++ ui/config/webpack.config.prod.js
@@ -21,7 +21,6 @@ const getClientEnvironment = require('./env');
 const ModuleNotFoundPlugin = require('react-dev-utils/ModuleNotFoundPlugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin-alt');
 const typescriptFormatter = require('react-dev-utils/typescriptFormatter');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 // Webpack uses `publicPath` to determine where the app is being served from.
 // It requires a trailing slash, or the file assets will get an incorrect path.
@@ -534,7 +533,6 @@ module.exports = {
         silent: true,
         formatter: typescriptFormatter,
       }),
-    new BundleAnalyzerPlugin(),
   ].filter(Boolean),
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
