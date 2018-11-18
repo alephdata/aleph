6d4d5b7ab6d12c2646d5418f3b65414a9e6cd415
Friedrich Lindenberg <friedrich@pudo.org>
2018-11-18 19:55:55 +0100

    Generate ftm index mappings, fixes #496.

--- aleph/index/admin.py
+++ aleph/index/admin.py
@@ -1,5 +1,8 @@
 import logging
 from copy import deepcopy
+from pprint import pprint, pformat  # noqa
+from followthemoney import model
+from followthemoney.types import registry
 
 from aleph.core import es
 from aleph.index.core import record_index, entity_index
@@ -7,13 +10,14 @@ from aleph.index.core import collections_index, all_indexes
 from aleph.index.mapping import RECORD_MAPPING
 from aleph.index.mapping import ENTITY_MAPPING
 from aleph.index.mapping import COLLECTION_MAPPING
-from aleph.index.mapping import INDEX_SETTINGS
+from aleph.index.mapping import INDEX_SETTINGS, PARTIAL_DATE
 
 log = logging.getLogger(__name__)
 
 
 def upgrade_search():
     """Add any missing properties to the index mappings."""
+    compile_entity_mapping()
     INDEXES = [
         (collections_index(), COLLECTION_MAPPING),
         (entity_index(), ENTITY_MAPPING),
@@ -34,6 +38,23 @@ def upgrade_search():
             # es.indices.put_mapping(index=index, doc_type='doc', body=mapping)
             es.indices.open(index=index, ignore=[400, 404])
             es.indices.clear_cache(index=index, ignore=[400, 404])
+
+
+def compile_entity_mapping():
+    TYPES = {
+        registry.text: {"type": "text", "analyzer": "icu_latin"},
+        registry.date: {"type": "date", "format": PARTIAL_DATE},
+    }
+    mapping = {}
+    for prop in model.properties:
+        config = TYPES.get(prop.type)
+        if config is None:
+            continue
+        if mapping.get(prop.name, config) != config:
+            raise RuntimeError("Ambiguous type for FtM property: %s" % prop)
+        mapping[prop.name] = config
+    ENTITY_MAPPING['properties']['properties']['properties'] = mapping
+    log.info("%s", pformat(ENTITY_MAPPING))
 
 
 def delete_index():
