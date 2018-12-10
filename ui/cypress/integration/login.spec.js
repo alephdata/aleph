4b18e5500fc36c4cab1643699dbadefaaaf809fd
Davit Khurshudyan <davo11122@gmail.com>
2018-12-10 14:09:44 +0200

    [TEST] Login flow

--- ui/cypress/integration/login.spec.js
+++ ui/cypress/integration/login.spec.js
@@ -13,23 +13,31 @@ describe('The Login Page', function () {
   it('sets auth cookie when logging in via form submission', function () {
     // destructuring assignment of the this.currentUser object
     // const { username, password } = this.currentUser
-    cy.server()
+    cy.server();
+    cy.route('/api/2/metadata', 'fixture:metadata').as('getMetadata');
+    cy.route('/api/2/statistics', 'fixture:statistics').as('getStatistics');
+    cy.route({
+      url:'/api/2/sessions/login',
+      method:'POST',
+      status:200,
+      headers:{
+        "x-aleph-session": "576f65a8-bab2-4e59-ba3f-a866482b2470"
+      },
+      response:{
+        token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NCwiZXhwIjoxNTQ0MjgxMjAxLCJyb2xlcyI6WzEsMiw0XSwiaXNfYWRtaW4iOnRydWUsInJvbGUiOnsiaXNfbXV0ZWQiOm51bGwsImFwaV9rZXkiOiJjZjIwMGIzNTBhNDU0OGJjYjNhMTAwNjZmMTEzNjJkYSIsImxhYmVsIjoiYW1lPUFsaWNlIDx1KioqQGV4YW1wbGUuY29tPiIsImVtYWlsIjoidXNlckBleGFtcGxlLmNvbSIsImlkIjoiNCIsInR5cGUiOiJ1c2VyIiwiaGFzX3Bhc3N3b3JkIjp0cnVlLCJuYW1lIjoiYW1lPUFsaWNlIiwid3JpdGVhYmxlIjp0cnVlLCJsaW5rcyI6eyJzZWxmIjoiaHR0cDovL2xvY2FsaG9zdDo4MDgwL2FwaS8yL3JvbGVzLzQifX19.bZ_5la8W89vznqyqi6gu_slz9_LaJlKCgQpjsOqx6-Q"
+        }
+    }).as('loginRequest');
+    cy.visit('/');
 
-    cy.route('/api/v2/metadata', )
-    cy.visit('/')
-
-    cy.get('input[name=username]').type('dasd')
+    cy.wait(['@getMetadata', '@getStatistics']);
+    cy.get('.AuthButtons>button').click();
+    cy.get('input[name=email]:visible').type('users@example.com');
 
     // {enter} causes the form to submit
-    cy.get('input[name=password]').type(`asds{enter}`)
+    cy.get('input[name=password]').type(`123abc{enter}`)
+    cy.wait(['@loginRequest', '@getStatistics','@getMetadata'])
 
-    // we should be redirected to /dashboard
-    cy.url().should('include', '/dashboard')
-
-    // our auth cookie should be present
-    cy.getCookie('your-session-cookie').should('exist')
-
-    // UI should reflect this user being logged in
-    cy.get('h1').should('contain', 'jane.lane')
+    cy.get('.AuthButtons>button').should('not.to.exist');
+    
   })
 })
