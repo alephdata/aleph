a822329f580ee55e39fbcf04b6ada781bc220924
Friedrich Lindenberg <friedrich@pudo.org>
2018-12-29 17:22:28 +0100

    Simplify NER service

--- services/extract-entities/entityextractor/service.py
+++ services/extract-entities/entityextractor/service.py
@@ -3,7 +3,6 @@ import grpc
 import time
 import spacy
 import logging
-# from polyglot.text import Text
 from concurrent import futures
 from alephclient.services.entityextract_pb2_grpc import (
     add_EntityExtractServicer_to_server, EntityExtractServicer
@@ -11,15 +10,6 @@ from alephclient.services.entityextract_pb2_grpc import (
 from alephclient.services.entityextract_pb2 import ExtractedEntity
 
 log = logging.getLogger('service')
-
-# POLYGLOT_PATH = os.environ.get('POLYGLOT_DATA_PATH')
-# POLYGLOT_NER_PATH = os.path.join(POLYGLOT_PATH, 'polyglot_data/ner2')
-# POLYGLOT_LANGUAGES = os.listdir(POLYGLOT_NER_PATH)
-# POLYGLOT_TYPES = {
-#     'I-PER': ExtractedEntity.PERSON,
-#     'I-ORG': ExtractedEntity.ORGANIZATION,
-#     'I-LOC': ExtractedEntity.LOCATION
-# }
 
 # https://spacy.io/api/annotation#named-entities
 SPACY_TYPES = {
@@ -29,60 +19,27 @@ SPACY_TYPES = {
     'LOC': ExtractedEntity.LOCATION,
     'GPE': ExtractedEntity.LOCATION
 }
+nlp = spacy.load('xx')
 
 
 class EntityServicer(EntityExtractServicer):
 
-    def __init__(self):
-        log.info("Loading spaCy model xx...")
-        self.spacy = spacy.load('xx')
-
-    # def extract_polyglot(self, text):
-    #     try:
-    #         parsed = Text(text)
-    #         lang = parsed.language
-    #         if lang.confidence > 90:
-    #             yield self.make_entity(lang.code,
-    #                                    ExtractedEntity.LANGUAGE,
-    #                                    0, len(text))
-    #         if lang.code not in POLYGLOT_LANGUAGES:
-    #             return
-    #         for ent in parsed.entities:
-    #             label = ' '.join(ent)
-    #             type_ = POLYGLOT_TYPES.get(ent.tag)
-    #             if type_ is not None and len(label):
-    #                 entity = ExtractedEntity()
-    #                 entity.text = label
-    #                 entity.type = type_
-    #                 entity.start = ent.start
-    #                 entity.end = ent.end
-    #                 yield entity
-    #     except Exception as ex:
-    #         log.exception("Polyglot failed")
-
-    def extract_spacy(self, text):
-        try:
-            doc = self.spacy(text)
-            for ent in doc.ents:
-                type_ = SPACY_TYPES.get(ent.label_)
-                label = ent.text.strip()
-                if type_ is not None and len(label):
-                    entity = ExtractedEntity()
-                    entity.text = label
-                    entity.type = type_
-                    entity.start = ent.start
-                    entity.end = ent.end
-                    yield entity
-        except Exception:
-            log.exception("spaCy failed")
-
     def Extract(self, request, context):
-        # yield from self.extract_polyglot(request.text)
-        yield from self.extract_spacy(request.text)
+        doc = nlp(request.text)
+        for ent in doc.ents:
+            type_ = SPACY_TYPES.get(ent.label_)
+            label = ent.text.strip()
+            if type_ is not None and len(label):
+                entity = ExtractedEntity()
+                entity.text = label
+                entity.type = type_
+                entity.start = ent.start
+                entity.end = ent.end
+                yield entity
 
 
 def serve(port):
-    executor = futures.ThreadPoolExecutor(max_workers=3)
+    executor = futures.ThreadPoolExecutor(max_workers=10)
     server = grpc.server(executor)
     add_EntityExtractServicer_to_server(EntityServicer(), server)
     server.add_insecure_port(port)
@@ -97,5 +54,4 @@ def serve(port):
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
-    logging.getLogger('polyglot').setLevel(logging.WARNING)
     serve('[::]:50000')
--- services/extract-entities/setup.py
+++ services/extract-entities/setup.py
@@ -4,22 +4,12 @@ from setuptools import setup, find_packages
 setup(
     name='entityextractor',
     version='0.0.1',
-    author='Organized Crime and Corruption Reporting Project',
-    author_email='data@occrp.org',
-    url='https://occrp.org',
     license='MIT',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-    namespace_packages=[],
     include_package_data=True,
-    package_data={},
     install_requires=[
-        'PyICU==2.0.6',
-        # 'pycld2==0.31',
-        # 'polyglot==16.7.4',
-        # 'Morfessor==2.0.4',
-        # 'spacy', - explicit in Dockerfile
         'grpcio==1.11.0',
-        'alephclient==0.7.2'
+        'alephclient==0.9.1'
     ],
     zip_safe=False
 )
