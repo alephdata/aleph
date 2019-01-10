736b636c98373b3b7e5f5c40033ef0de6edc1b52
Friedrich Lindenberg <friedrich@pudo.org>
2019-01-10 13:40:17 +0100

    Well that was stupid.

--- aleph/services.py
+++ aleph/services.py
@@ -19,7 +19,7 @@ class ServiceClientMixin(object):
         """Lazily connect to the RPC service."""
         if not self.has_channel():
             return
-        if not hasattr(self, 'channel') or self.channel is None:
+        if not hasattr(self, '_channel') or self._channel is None:
             options = (
                 # ('grpc.keepalive_time_ms', settings.GRPC_CONN_AGE),
                 ('grpc.keepalive_timeout_ms', settings.GRPC_CONN_AGE),
@@ -27,9 +27,9 @@ class ServiceClientMixin(object):
                 ('grpc.max_connection_idle_ms', settings.GRPC_CONN_AGE),
                 ('grpc.lb_policy_name', settings.GRPC_LB_POLICY)
             )
-            self.channel = grpc.insecure_channel(self.SERVICE, options)
-        return self.channel
+            self._channel = grpc.insecure_channel(self.SERVICE, options)
+        return self._channel
 
     def reset_channel(self):
-        self.channel.close()
-        self.channel = None
+        self._channel.close()
+        self._channel = None
