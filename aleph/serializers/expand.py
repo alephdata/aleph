22b4ebd8ae4b3de23d389252b7cd63d83f8a1658
Friedrich Lindenberg <friedrich@pudo.org>
2019-02-01 17:39:12 +0100

    Load entities without using redis-backed getter

--- aleph/serializers/expand.py
+++ aleph/serializers/expand.py
@@ -1,10 +1,11 @@
 import logging
+from pprint import pprint  # noqa
 from banal import ensure_list, first, is_mapping
 from marshmallow import Schema, post_dump
 
 from aleph.model import Role, Alert, Entity, Collection
 from aleph.logic.collections import get_collection
-from aleph.logic.entities import get_entity
+from aleph.index.entities import entities_by_ids
 from aleph.logic.roles import get_role
 from aleph.logic.alerts import get_alert
 
@@ -13,7 +14,6 @@ log = logging.getLogger(__name__)
 LOADERS = {
     Role: get_role,
     Collection: get_collection,
-    Entity: get_entity,
     Alert: get_alert
 }
 
@@ -33,6 +33,15 @@ class ExpandableSchema(Schema):
     @post_dump(pass_many=True)
     def expand(self, objs, many=False):
         cache = {}
+        for obj in ensure_list(objs):
+            for (field, type_, target, schema, multi) in self.EXPAND:
+                for key in self._get_values(obj, field):
+                    if type_ == Entity:
+                        cache[(type_, key)] = None
+
+        for entity in entities_by_ids([k for (t, k) in cache.keys()]):
+            cache[(Entity, entity.get('id'))] = entity
+
         for obj in ensure_list(objs):
             for (field, type_, target, schema, multi) in self.EXPAND:
                 value = []
