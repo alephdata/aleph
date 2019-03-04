aa287f3ccc83a9da5b36a2ade251b56c3b0d4ec6
Davit Khurshudyan <davo11122@gmail.com>
2019-03-04 18:26:56 +0400

    count component fixed widths

--- ui/src/components/common/Count.jsx
+++ ui/src/components/common/Count.jsx
@@ -1,17 +1,20 @@
 import React, { PureComponent } from 'react';
+import c from 'classnames';
 import numeral from 'numeral';
+import { FormattedNumber } from 'react-intl';
 
+import './Count.scss';
 
 class Count extends PureComponent {
   render() {
-    const { count } = this.props;
+    const { count, noTag = false, full = false } = this.props;
 
     if (count === undefined || count === 0) {
       return null;
     }
     return (
-      <span className="bp3-tag bp3-round bp3-intent-primary">
-        {numeral(count).format('0a')}
+      <span className={c({ 'bp3-tag': !noTag }, 'bp3-round', 'bp3-intent-primary', 'count')}>
+        {full ? <FormattedNumber value={count} /> : numeral(count).format('0a')}
       </span>
     );
   }
