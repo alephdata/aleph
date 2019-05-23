d77a4cf05d3b2171bfd2cc703def3163466462f6
Davit Khurshudyan <davo11122@gmail.com>
2019-05-23 19:25:37 +0400

    Make collection languages configurable in the UI
    resolves #665

--- ui/src/components/common/Language.jsx
+++ ui/src/components/common/Language.jsx
@@ -4,6 +4,8 @@ import { connect } from 'react-redux';
 
 import wordList from 'src/util/wordList';
 import { selectMetadata } from 'src/selectors';
+import { Classes, MenuItem, Position } from '@blueprintjs/core';
+import { MultiSelect as BlueprintMultiSelect } from '@blueprintjs/select';
 
 
 class Name extends PureComponent {
@@ -28,12 +30,107 @@ class List extends Component {
   }
 }
 
+class MultiSelect extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+
+    this.itemRenderer = this.itemRenderer.bind(this);
+    this.itemFilter = this.itemFilter.bind(this);
+    this.onItemSelect = this.onItemSelect.bind(this);
+    this.onRemoveTag = this.onRemoveTag.bind(this);
+    this.tagRenderer = this.tagRenderer.bind(this);
+  }
+
+  static getDerivedStateFromProps(nextProps) {
+    return { codes: nextProps.codes || [] };
+  }
+
+  onRemoveTag(event, index) {
+    const { codes } = this.state;
+    codes.splice(index, 1);
+    this.setState({ codes });
+    this.props.onChange(codes);
+  }
+
+  onItemSelect(item, event) {
+    event.stopPropagation();
+    const { codes } = this.state;
+    codes.push(item);
+    this.setState({ codes });
+    this.props.onChange(codes);
+  }
+
+  tagRenderer(item) {
+    return <Name code={item} languages={this.props.languages} />;
+  }
+
+  itemFilter(query, item) {
+    if (!query.length || this.state.codes.indexOf(item) !== -1) {
+      return false;
+    }
+    const label = this.props.languages[item].toLowerCase();
+    return label.includes(query.toLowerCase());
+  }
+
+  itemRenderer(item, { modifiers, handleClick }) {
+    if (modifiers.matchesPredicate) {
+      return (
+        <MenuItem
+          key={item}
+          className={modifiers.active ? Classes.ACTIVE : ''}
+          onClick={handleClick}
+          text={this.props.languages[item]}
+        />
+      );
+    }
+    return undefined;
+  }
+
+  render() {
+    const { codes } = this.state;
+    const { languages } = this.props;
+    const items = Object.keys(languages);
+
+    return (
+      <BlueprintMultiSelect
+        initialContent={
+          <MenuItem disabled text="asd" />
+        }
+        itemPredicate={this.itemFilter}
+        noResults={
+          <MenuItem disabled text="sd" />
+        }
+        items={items}
+        itemRenderer={this.itemRenderer}
+        tagRenderer={this.tagRenderer}
+        onItemSelect={this.onItemSelect}
+        intent={false}
+        tagInputProps={{
+          onRemove: this.onRemoveTag,
+        }}
+        popoverProps={{
+          position: Position.BOTTOM_LEFT,
+          className: 'CountryMultiSelect',
+          usePortal: false,
+        }}
+        resetOnSelect
+        openOnKeyDown
+        selectedItems={codes}
+      />
+    );
+  }
+}
+
+
 const mapStateToProps = state => ({
   languages: selectMetadata(state).languages,
 });
 
 class Language extends Component {
   static Name = connect(mapStateToProps)(Name);
+
+  static MultiSelect = connect(mapStateToProps)(MultiSelect)
 
   static List = connect(mapStateToProps)(List);
 }
