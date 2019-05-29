199656dabacdb4aee33792c0d93745230fe6c4c1
Friedrich Lindenberg <friedrich@pudo.org>
2019-05-29 15:44:33 +0200

    Pin version

--- ui/src/components/common/Entity.jsx
+++ ui/src/components/common/Entity.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { FormattedMessage } from 'react-intl';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
