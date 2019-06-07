843536011a892b01e10a80f9189be21262f1d7b5
Friedrich Lindenberg <friedrich@pudo.org>
2019-06-07 12:13:49 +0200

    Collections status API

--- aleph/views/collections_api.py
+++ aleph/views/collections_api.py
@@ -8,7 +8,8 @@ from aleph.core import db, settings
 from aleph.authz import Authz
 from aleph.model import Role, Collection
 from aleph.search import CollectionsQuery
-from aleph.queues import queue_task, OP_BULKLOAD, OP_PROCESS
+from aleph.queues import queue_task, get_status, cancel_queue
+from aleph.queues import OP_BULKLOAD, OP_PROCESS
 from aleph.logic.collections import create_collection, refresh_collection
 from aleph.logic.collections import delete_collection, update_collection
 from aleph.logic.processing import bulk_write
@@ -17,7 +18,7 @@ from aleph.views.context import enable_cache
 from aleph.views.forms import CollectionCreateSchema, CollectionUpdateSchema
 from aleph.views.serializers import CollectionSerializer
 from aleph.views.util import get_db_collection, get_index_collection
-from aleph.views.util import require, parse_request
+from aleph.views.util import require, parse_request, jsonify
 from aleph.views.util import render_xml, get_flag
 
 blueprint = Blueprint('collections_api', __name__)
@@ -75,7 +76,7 @@ def process(collection_id):
     collection = get_db_collection(collection_id, request.authz.WRITE)
     # re-process the documents
     queue_task(collection, OP_PROCESS)
-    return ('', 204)
+    return ('', 202)
 
 
 @blueprint.route('/api/2/collections/<int:collection_id>/mapping', methods=['POST', 'PUT'])  # noqa
@@ -91,15 +92,16 @@ def mapping(collection_id):
         except InvalidMapping as invalid:
             raise BadRequest(invalid)
     queue_task(collection, OP_BULKLOAD, payload=data)
-    return ('', 204)
+    return ('', 202)
 
 
 @blueprint.route('/api/2/collections/<int:collection_id>/_bulk', methods=['POST'])  # noqa
+@blueprint.route('/api/2/collections/<int:collection_id>/bulk', methods=['POST'])  # noqa
 def bulk(collection_id):
     collection = get_db_collection(collection_id, request.authz.WRITE)
     require(request.authz.can_bulk_import())
 
-    # This will disable certain security measures in order to allow bulk
+    # This will disable checksum security measures in order to allow bulk
     # loading of document data.
     unsafe = get_flag('unsafe', default=False)
     unsafe = unsafe and request.authz.is_admin
@@ -108,6 +110,19 @@ def bulk(collection_id):
     bulk_write(collection, entities, unsafe=unsafe)
     refresh_collection(id)
     return ('', 204)
+
+
+@blueprint.route('/api/2/collections/<int:collection_id>/status', methods=['GET'])  # noqa
+def status(collection_id):
+    collection = get_db_collection(collection_id, request.authz.READ)
+    return jsonify(get_status(collection))
+
+
+@blueprint.route('/api/2/collections/<int:collection_id>/status', methods=['DELETE'])  # noqa
+def cancel(collection_id):
+    collection = get_db_collection(collection_id, request.authz.WRITE)
+    cancel_queue(collection)
+    return jsonify(get_status(collection))
 
 
 @blueprint.route('/api/2/collections/<int:collection_id>', methods=['DELETE'])
