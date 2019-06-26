5acbe382029fdc8f9562984fe594d52432306a85
Friedrich Lindenberg <friedrich@pudo.org>
2019-06-26 18:10:56 +0200

    assumed wrong base version

--- services/recognize-text/setup.py
+++ services/recognize-text/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='textrecognizer',
-    version='3.1.2',
+    version='3.1.3',
     author='Organized Crime and Corruption Reporting Project',
     author_email='pudo@occrp.org',
     url='https://occrp.org',
