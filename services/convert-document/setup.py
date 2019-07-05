77a4bd34022e467b26a4cbc7bde0c3e09abbf6c4
Friedrich Lindenberg <friedrich@pudo.org>
2019-07-05 16:56:25 +0200

    Bump version: 3.2.2 → 3.2.3

--- services/convert-document/setup.py
+++ services/convert-document/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='convert',
-    version='3.2.2',
+    version='3.2.3',
     packages=find_packages(exclude=[]),
     install_requires=[
         'aiohttp',
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aleph',
-    version='3.2.2',
+    version='3.2.3',
     description="Document sifting web frontend",
     long_description="",
     classifiers=[
