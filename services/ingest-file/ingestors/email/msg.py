3da1109d32c2475830167b720290b37b02d27769
Friedrich Lindenberg <friedrich@pudo.org>
2019-07-21 11:21:07 +0200

    Refactor text decoding

--- services/ingest-file/ingestors/email/msg.py
+++ services/ingest-file/ingestors/email/msg.py
@@ -7,12 +7,13 @@ from followthemoney import model
 
 from ingestors.ingestor import Ingestor
 from ingestors.support.email import EmailSupport
+from ingestors.support.encoding import EncodingSupport
 from ingestors.exc import ProcessingException
 
 log = logging.getLogger(__name__)
 
 
-class RFC822Ingestor(Ingestor, EmailSupport):
+class RFC822Ingestor(Ingestor, EmailSupport, EncodingSupport):
     MIME_TYPES = [
         'multipart/mixed',
         'message/rfc822'
@@ -31,9 +32,7 @@ class RFC822Ingestor(Ingestor, EmailSupport):
         mime_type = normalize_mimetype(part.get_content_type())
         payload = part.get_payload(decode=True)
         charset = part.get_content_charset()
-        if charset is not None:
-            # TODO: do we want to do chardet after decoding fails?
-            payload = payload.decode(charset, 'replace')
+        payload = self.decode_string(payload, charset)
 
         if 'text/html' in mime_type:
             self.extract_html_content(entity, payload, extract_metadata=False)
--- services/ingest-file/ingestors/support/encoding.py
+++ services/ingest-file/ingestors/support/encoding.py
@@ -1,5 +1,7 @@
+import codecs
 import logging
 import chardet
+from normality import stringify, guess_encoding
 from normality.encoding import guess_file_encoding, normalize_result
 
 from ingestors.exc import ProcessingException
@@ -11,6 +13,39 @@ class EncodingSupport(object):
     """Decode the contents of the given file as plain text by guessing its
     encoding."""
     DEFAULT_ENCODING = 'utf-8'
+
+    def _is_encoding_codec(self, encoding):
+        """Check if a given string is a valid encoding name."""
+        try:
+            codecs.lookup(encoding)
+            return True
+        except LookupError:
+            return False
+
+    def normalize_encoding(self, encoding):
+        """Make sure that the given string is a valid encoding name."""
+        encoding = stringify(encoding)
+        if encoding is None:
+            return self.DEFAULT_ENCODING
+        if self._is_encoding_codec(encoding):
+            return encoding
+        encoding = encoding.replace('-', '')
+        if self._is_encoding_codec(encoding):
+            return encoding
+        return self.DEFAULT_ENCODING
+
+    def decode_string(self, text, encoding=DEFAULT_ENCODING):
+        if not isinstance(text, bytes):
+            return stringify(text)
+        encoding = self.normalize_encoding(encoding)
+        try:
+            return text.decode(encoding, 'strict')
+        except Exception:
+            try:
+                detected = guess_encoding(text)
+                return text.decode(detected, 'strict')
+            except Exception:
+                return text.decode(encoding, 'replace')
 
     def detect_stream_encoding(self, fh, default=DEFAULT_ENCODING):
         return guess_file_encoding(fh, default=default)
