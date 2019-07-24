02326e3bddaa07feb60c04a1957e85c4454e711b
Friedrich Lindenberg <friedrich@pudo.org>
2019-07-24 19:08:28 +0200

    Adopt msglite

--- services/ingest-file/ingestors/email/outlookmsg.py
+++ services/ingest-file/ingestors/email/outlookmsg.py
@@ -1,24 +1,18 @@
-from __future__ import unicode_literals
-
 import logging
+from msglite import Message
 from olefile import isOleFile
 from email.parser import Parser
 from normality import stringify
 from followthemoney import model
+from email.utils import parsedate_to_datetime
 
 from ingestors.ingestor import Ingestor
 from ingestors.support.email import EmailSupport, EmailIdentity
 from ingestors.support.ole import OLESupport
-from extract_msg import Message
+from ingestors.exc import ProcessingException
+
 
 log = logging.getLogger(__name__)
-
-
-class FieldMessage(Message):
-
-    def getField(self, name):
-        return self._getStringStream('__substg1.0_%s' % name)
-
 
 class OutlookMsgIngestor(Ingestor, EmailSupport, OLESupport):
     MIME_TYPES = [
@@ -34,7 +28,11 @@ class OutlookMsgIngestor(Ingestor, EmailSupport, OLESupport):
 
     def ingest(self, file_path, entity):
         entity.schema = model.get('Email')
-        msg = FieldMessage(file_path)
+        try:
+            msg = Message(file_path)
+        except Exception as exc:
+            raise ProcessingException("Cannot open message file.") from msg
+        
         self.extract_olefileio_metadata(msg, entity)
 
         try:
@@ -42,23 +40,29 @@ class OutlookMsgIngestor(Ingestor, EmailSupport, OLESupport):
         except Exception:
             log.exception("Cannot parse Outlook-stored headers")
 
+        entity.add('encoding', msg.encoding)
         entity.add('bodyText', msg.body)
         entity.add('bodyHtml', msg.htmlBody)
         entity.add('messageId', msg.message_id)
         # entity.add('inReplyTo', msg.reply_to)
         entity.add('subject', msg.subject)
-        entity.add('threadTopic', msg.getField('0070'))
-        entity.add('date', msg.parsedDate)
+        entity.add('threadTopic', msg.getStringField('0070'))
+
+        try:
+            date = parsedate_to_datetime(msg.date).isoformat()
+            entity.add('date', date)
+        except Exception as exc:
+            log.warning("Could not parse date: %s", msg.date)
 
         # sender name and email
         sender = self.get_identities(msg.sender)
         self.apply_identities(entity, sender, 'emitters', 'sender')
 
         # received by
-        sender = self.get_identity(msg.getField('0040'), msg.getField('0076'))
+        sender = self.get_identity(msg.getStringField('0040'), msg.getStringField('0076'))
         self.apply_identities(entity, sender, 'emitters')
 
-        froms = self.get_identities(msg.getField('1046'))
+        froms = self.get_identities(msg.getStringField('1046'))
         self.apply_identities(entity, froms, 'emitters', 'from')
 
         tos = self.get_identities(msg.to)
@@ -67,16 +71,19 @@ class OutlookMsgIngestor(Ingestor, EmailSupport, OLESupport):
         ccs = self.get_identities(msg.cc)
         self.apply_identities(entity, ccs, 'recipients', 'cc')
 
-        bccs = self.get_identities(msg.getField('0E02'))
+        bccs = self.get_identities(msg.getStringField('0E02'))
         self.apply_identities(entity, bccs, 'recipients', 'bcc')
 
         self.resolve_message_ids(entity)
         for attachment in msg.attachments:
+            if attachment.type != 'data':
+                continue
             name = stringify(attachment.longFilename)
             name = name or stringify(attachment.shortFilename)
             self.ingest_attachment(entity, name,
                                    attachment.type,
                                    attachment.data)
+            
 
     @classmethod
     def match(cls, file_path, entity):
