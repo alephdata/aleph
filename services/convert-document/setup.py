12f05370971b96034854559885f5cf36fe36dfb3
Friedrich Lindenberg <friedrich@pudo.org>
2019-08-29 12:24:43 +0200

    Bump version: 3.2.8 → 3.2.9

--- services/convert-document/setup.py
+++ services/convert-document/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='convert',
-    version='3.2.8',
+    version='3.2.9',
     packages=find_packages(exclude=[]),
     install_requires=[],
 )
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aleph',
-    version='3.2.8',
+    version='3.2.9',
     description="Document sifting web frontend",
     long_description="",
     classifiers=[
