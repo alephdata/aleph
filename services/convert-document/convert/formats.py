cdbd7250eca31c01099381bd57a9947c8739b143
Friedrich Lindenberg <friedrich@pudo.org>
2019-09-12 16:56:35 +0200

    Replace LibreOffice converter with unoconv, fixes #688.

--- services/convert-document/convert/formats.py
+++ services/convert-document/convert/formats.py
@@ -1,54 +1,37 @@
 from lxml import etree
-from collections import defaultdict, OrderedDict
 from pantomime import normalize_mimetype, normalize_extension
 
 NS = {'oor': 'http://openoffice.org/2001/registry'}
 NAME = '{%s}name' % NS['oor']
+FILES = [
+    '/usr/lib/libreoffice/share/registry/writer.xcd',
+    '/usr/lib/libreoffice/share/registry/impress.xcd',
+    '/usr/lib/libreoffice/share/registry/draw.xcd',
+]
 
 
-class Formats(object):
-    FILES = [
-        '/usr/lib/libreoffice/share/registry/writer.xcd',
-        '/usr/lib/libreoffice/share/registry/impress.xcd',
-        '/usr/lib/libreoffice/share/registry/draw.xcd',
-        # '/usr/lib/libreoffice/share/registry/calc.xcd',
-    ]
+def load_mime_extensions():
+    media_types = {}
+    for xcd_file in FILES:
+        doc = etree.parse(xcd_file)
+        path = './*[@oor:package="org.openoffice.TypeDetection"]/node/node'
+        for tnode in doc.xpath(path, namespaces=NS):
+            node = {}
+            for prop in tnode.findall('./prop'):
+                name = prop.get(NAME)
+                for value in prop.findall('./value'):
+                    node[name] = value.text
 
-    def __init__(self):
-        self.media_types = defaultdict(list)
-        self.extensions = defaultdict(list)
-        for xcd_file in self.FILES:
-            doc = etree.parse(xcd_file)
-            path = './*[@oor:package="org.openoffice.TypeDetection"]/node/node'
-            for tnode in doc.xpath(path, namespaces=NS):
-                node = {}
-                for prop in tnode.findall('./prop'):
-                    name = prop.get(NAME)
-                    for value in prop.findall('./value'):
-                        node[name] = value.text
+            media_type = normalize_mimetype(node.get('MediaType'),
+                                            default=None)
+            if media_type is None:
+                continue
 
-                name = node.get('PreferredFilter', tnode.get(NAME))
-                media_type = normalize_mimetype(node.get('MediaType'),
-                                                default=None)
-                if media_type is not None:
-                    self.media_types[media_type].append(name)
+            extensions = node.get('Extensions')
+            if extensions is None:
+                continue
 
-                for ext in self.parse_extensions(node.get('Extensions')):
-                    self.extensions[ext].append(name)
-
-    def parse_extensions(self, extensions):
-        if extensions is not None:
-            for ext in extensions.split(' '):
-                if ext == '*':
-                    continue
-                ext = normalize_extension(ext)
-                if ext is not None:
-                    yield ext
-
-    def get_filters(self, extension, media_type):
-        filters = OrderedDict()
-        for filter_name in self.media_types.get(media_type, []):
-            filters[filter_name] = None
-        for filter_name in self.extensions.get(extension, []):
-            filters[filter_name] = None
-        return filters.keys()
+            extension = normalize_extension(extensions.split(' ')[0])
+            if extension is not None:
+                media_types[media_type] = extension
+    return media_types
