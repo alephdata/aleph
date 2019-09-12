cbbea570c1d5d6e8eb1728bb44ea54f08dc994cd
Tarashish Mishra <sunu0000@gmail.com>
2019-09-12 20:00:54 +0530

    Move dashboard component into SystemStatusScreen

--- ui/src/screens/SystemStatusScreen/SystemStatusScreen.jsx
+++ ui/src/screens/SystemStatusScreen/SystemStatusScreen.jsx
@@ -3,12 +3,17 @@ import { defineMessages, FormattedMessage, injectIntl } from 'react-intl';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
+import { Button, Tooltip } from '@blueprintjs/core';
+
 import Query from 'src/app/Query';
-import { DualPane, Breadcrumbs } from 'src/components/common';
+import { DualPane, Breadcrumbs, Collection, SectionLoading, ErrorSection } from 'src/components/common';
 import Toolbar from 'src/components/Toolbar/Toolbar';
-import Dashboard from 'src/components/Dashboard/Dashboard';
 import Screen from 'src/components/Screen/Screen';
 import ErrorScreen from 'src/components/Screen/ErrorScreen';
+import { triggerCollectionCancel, queryDashboard } from 'src/actions';
+import { selectDashboardResult } from 'src/selectors';
+import { Link } from 'react-router-dom';
+import getCollectionLink from 'src/util/getCollectionLink';
 
 
 import './SystemStatusScreen.scss';
@@ -19,12 +24,42 @@ const messages = defineMessages({
     id: 'dashboard.title',
     defaultMessage: 'System Status',
   },
+  no_active_collection: {
+    id: 'dashboard.no_active_collection',
+    defaultMessage: 'There are no active collections',
+  },
+  cancel_button: {
+    id: 'collection.status.cancel_button',
+    defaultMessage: 'Cancel the process',
+  },
 });
 
 
 export class SystemStatusScreen extends React.Component {
+  componentDidMount() {
+    this.fetchIfNeeded();
+    this.interval = setInterval(() => this.fetchIfNeeded(), 10000);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (!this.props.query.sameAs(prevProps.query)) {
+      this.fetchIfNeeded();
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  fetchIfNeeded() {
+    const { result, query } = this.props;
+    if (!result.isLoading) {
+      this.props.queryDashboard({ query });
+    }
+  }
+
   render() {
-    const { query, result, intl } = this.props;
+    const { result, intl } = this.props;
 
     if (result.isError) {
       return <ErrorScreen error={result.error} />;
@@ -42,7 +77,55 @@ export class SystemStatusScreen extends React.Component {
                 <FormattedMessage id="notifications.heading" defaultMessage="Active Collections" />
               </h1>
             </Toolbar>
-            <Dashboard query={query} />
+            <React.Fragment>
+              {result.total === 0
+                && (
+                  <ErrorSection
+                    visual="dashboard"
+                    title={intl.formatMessage(messages.no_active_collection)}
+                  />
+                )
+              }
+              {result.total !== 0 && (
+                <table className="Dashboard">
+                  <thead>
+                    <tr>
+                      <th><FormattedMessage id="infoMode.collection" defaultMessage="Collection" /></th>
+                      <th><FormattedMessage id="collection.status.jobs" defaultMessage="Number of Jobs" /></th>
+                      <th><FormattedMessage id="collection.status.finished_tasks" defaultMessage="Finished Tasks" /></th>
+                      <th><FormattedMessage id="collection.status.pending_tasks" defaultMessage="Pending Tasks" /></th>
+                      <th />
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {result.results.map(res => (
+                      (
+                        <tr key={res.id}>
+                          <td className="entity">
+                            <Link to={getCollectionLink(res.collection)}>
+                              <Collection.Label collection={res.collection} />
+                            </Link>
+                          </td>
+                          <td>{res.jobs.length}</td>
+                          <td>{res.finished}</td>
+                          <td>{res.pending}</td>
+                          <td>
+                            <Tooltip content={intl.formatMessage(messages.cancel_button)}>
+                              <Button onClick={() => this.props.triggerCollectionCancel(res.collection.id)} icon="delete">
+                                <FormattedMessage id="collection.cancel.button" defaultMessage="Cancel" />
+                              </Button>
+                            </Tooltip>
+                          </td>
+                        </tr>
+                      )
+                    ))}
+                  </tbody>
+                </table>
+              )}
+              {result.isLoading && (
+                <SectionLoading />
+              )}
+            </React.Fragment>
           </DualPane.ContentPane>
         </DualPane>
       </Screen>
@@ -54,11 +137,11 @@ export class SystemStatusScreen extends React.Component {
 const mapStateToProps = (state, ownProps) => {
   const { location } = ownProps;
   const query = Query.fromLocation('status', location, {}, 'status');
-  const result = {};
+  const result = selectDashboardResult(state, query);
   return { query, result };
 };
 
-const mapDispatchToProps = {};
+const mapDispatchToProps = { triggerCollectionCancel, queryDashboard };
 
 export default compose(
   withRouter,
