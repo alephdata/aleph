bef3efc746798338fd76d12d1e68ae726c452660
Friedrich Lindenberg <friedrich@pudo.org>
2019-09-20 18:38:28 +0200

    Bump version: 3.2.11 → 3.2.12

--- services/convert-document/setup.py
+++ services/convert-document/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='convert',
-    version='3.2.11',
+    version='3.2.12',
     packages=find_packages(exclude=[]),
     install_requires=[],
 )
