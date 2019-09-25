c9d14d288e17403ad2f48845f8986ed4132fa86a
Friedrich Lindenberg <friedrich@pudo.org>
2019-09-25 14:48:26 +0200

    Re-factor more of the status screen

--- ui/src/actions/metadataActions.js
+++ ui/src/actions/metadataActions.js
@@ -21,3 +21,8 @@ export const fetchStatistics = asyncActionCreator(() => async () => {
   const response = await endpoint.get('statistics');
   return { statistics: response.data };
 }, { name: 'FETCH_STATISTICS' });
+
+export const fetchSystemStatus = asyncActionCreator(() => async () => {
+  const response = await endpoint.get('status');
+  return { status: response.data };
+}, { name: 'FETCH_SYSTEM_STATUS' });
