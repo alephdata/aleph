cf7df4702c3d4cdaa615c2e42d84a338dc584ca7
Friedrich Lindenberg <friedrich@pudo.org>
2019-10-03 21:07:43 +0200

    Update all dependencies

--- ui/scripts/start.js
+++ ui/scripts/start.js
@@ -79,7 +79,7 @@ checkBrowsers(paths.appPath, isInteractive)
     const appName = require(paths.appPackageJson).name;
     const urls = prepareUrls(protocol, HOST, port);
     // Create a webpack compiler that is configured with custom messages.
-    const compiler = createCompiler(webpack, config, appName, urls, useYarn);
+    const compiler = createCompiler({ webpack, config, appName, urls, useYarn });
     // Load proxy config
     const proxySetting = require(paths.appPackageJson).proxy;
     const proxyConfig = prepareProxy(proxySetting, paths.appPublic);
