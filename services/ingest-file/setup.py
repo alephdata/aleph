af683505c5289607cd73c225ed585b67cfc27d22
Friedrich Lindenberg <friedrich@pudo.org>
2019-11-02 23:25:27 +0100

    Bump version: 3.3.3 → 3.3.4

--- services/ingest-file/setup.py
+++ services/ingest-file/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ingest',
-    version='3.3.3',
+    version='3.3.4',
     author="Organized Crime and Corruption Reporting Project",
     packages=find_packages(exclude=['tests']),
     package_dir={'ingestors': 'ingestors'},
