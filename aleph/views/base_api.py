4d5dcce09d6c64114673687e64eb71b9eb9cab91
Friedrich Lindenberg <friedrich@pudo.org>
2019-11-04 09:48:10 +0100

    Fix exception handling

--- aleph/views/base_api.py
+++ aleph/views/base_api.py
@@ -173,9 +173,10 @@ def handle_jwt_expired(err):
 @blueprint.app_errorhandler(TransportError)
 def handle_es_error(err):
     message = err.error
-    error = err.info.get('error', {})
-    for root_cause in error.get('root_cause', []):
-        message = root_cause.get('reason', message)
+    if hasattr(err, 'info') and isinstance(err, dict):
+        error = err.info.get('error', {})
+        for root_cause in error.get('root_cause', []):
+            message = root_cause.get('reason', message)
     return jsonify({
         'status': 'error',
         'message': message,
