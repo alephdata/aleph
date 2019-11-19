b11739e2c63b168e75072a2ce14835c68a6ed8bd
Friedrich Lindenberg <friedrich@pudo.org>
2019-11-20 00:08:10 +0100

    Document xref API

--- aleph/views/xref_api.py
+++ aleph/views/xref_api.py
@@ -17,6 +17,34 @@ blueprint = Blueprint('xref_api', __name__)
 def index(collection_id):
     """
     ---
+    get:
+      summary: Fetch cross-reference summary
+      description: >-
+        Fetch cross-reference matches grouped by collection, for entities in
+        the collection with id `collection_id`
+      parameters:
+      - in: path
+        name: collection_id
+        required: true
+        schema:
+          type: integer
+      responses:
+        '200':
+          description: OK
+          content:
+            application/json:
+              schema:
+                type: object
+                allOf:
+                - $ref: '#/components/schemas/QueryResponse'
+                properties:
+                  results:
+                    type: array
+                    items:
+                      $ref: '#/components/schemas/XrefCollection'
+      tags:
+      - Xref
+      - Collection
     """
     collection = get_db_collection(collection_id)
     parser = QueryParser(request.args, request.authz)
@@ -28,6 +56,42 @@ def index(collection_id):
 @blueprint.route('/api/2/collections/<int:collection_id>/xref/<int:other_id>',
                  methods=['GET'])
 def matches(collection_id, other_id):
+    """
+    ---
+    get:
+      summary: Fetch cross-reference matches between collections
+      description: >-
+        Fetch cross-reference matches between 2 collections with ids
+        `collection_id` and `other_id`
+      parameters:
+      - in: path
+        name: collection_id
+        required: true
+        schema:
+          type: integer
+      - in: path
+        name: other_id
+        required: true
+        schema:
+          type: integer
+      responses:
+        '200':
+          description: OK
+          content:
+            application/json:
+              schema:
+                type: object
+                allOf:
+                - $ref: '#/components/schemas/QueryResponse'
+                properties:
+                  results:
+                    type: array
+                    items:
+                      $ref: '#/components/schemas/XrefMatch'
+      tags:
+      - Xref
+      - Collection
+    """
     collection = get_db_collection(collection_id)
     other = get_db_collection(other_id)
     parser = QueryParser(request.args, request.authz)
@@ -38,7 +102,39 @@ def matches(collection_id, other_id):
 
 @blueprint.route('/api/2/collections/<int:collection_id>/xref', methods=['POST'])  # noqa
 def generate(collection_id):
-    data = parse_request(XrefSchema)
+    """
+    ---
+    post:
+      summary: Generate cross-reference matches
+      description: >
+        Generate cross-reference matches for entities in a collection.
+      parameters:
+      - in: path
+        name: collection_id
+        required: true
+        schema:
+          type: integer
+      requestBody:
+        content:
+          application/json:
+            schema:
+              $ref: '#/components/schemas/XrefGenerate'
+      responses:
+        '202':
+          content:
+            application/json:
+              schema:
+                properties:
+                  status:
+                    description: accepted
+                    type: string
+                type: object
+          description: Accepted
+      tags:
+      - Xref
+      - Collection
+    """
+    data = parse_request('XrefGenerate')
     collection = get_db_collection(collection_id, request.authz.WRITE)
     against = ensure_list(data.get("against_collection_ids"))
     payload = {'against_collection_ids': against}
@@ -48,6 +144,28 @@ def generate(collection_id):
 
 @blueprint.route('/api/2/collections/<int:collection_id>/xref/export')
 def export(collection_id):
+    """
+    ---
+    get:
+      summary: Download cross-reference results
+      description: Download results of cross-referencing as an Excel file
+      parameters:
+      - in: path
+        name: collection_id
+        required: true
+        schema:
+          type: integer
+      responses:
+        '200':
+          description: OK
+          content:
+            application/vnd.openxmlformats-officedocument.spreadsheetml.sheet:
+              schema:
+                type: object
+      tags:
+      - Xref
+      - Collection
+    """
     collection = get_db_collection(collection_id, request.authz.READ)
     buffer = export_matches(collection.id, request.authz)
     file_name = '%s - Crossreference.xlsx' % collection.label
