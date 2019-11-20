608d587a330dfc85657868a1af0983b44d2ba6b1
kjacks <kjack227@gmail.com>
2019-11-20 17:54:47 +0100

    adapted collection mapping actions to use new API endpoints without parameters

--- ui/src/actions/collectionActions.js
+++ ui/src/actions/collectionActions.js
@@ -72,32 +72,40 @@ export const triggerCollectionCancel = asyncActionCreator(id => async () => {
   return { id, data: response.data };
 }, { name: 'TRIGGER_COLLECTION_CANCEL' });
 
+const executeTrigger = async (collectionId, mappingId) => (
+  endpoint.put(`collections/${collectionId}/mappings/${mappingId}/trigger`)
+);
+
+const executeFlush = async (collectionId, mappingId) => (
+  endpoint.put(`collections/${collectionId}/mappings/${mappingId}/flush`)
+);
+
+export const flushCollectionMapping = asyncActionCreator((collectionId, mappingId) => async () => {
+  executeFlush(collectionId, mappingId);
+  return { collectionId, mappingId };
+}, { name: 'FLUSH_COLLECTION_MAPPING' });
+
 export const createCollectionMapping = asyncActionCreator((collectionId, mapping) => async () => {
   const response = await endpoint.post(`collections/${collectionId}/mappings`, mapping);
   if (response && response.data && response.data.id) {
-    await endpoint.put(`collections/${collectionId}/mappings/${response.data.id}/trigger`);
+    executeTrigger(collectionId, response.data.id);
   }
   return { collectionId, data: [response.data] };
 }, { name: 'CREATE_COLLECTION_MAPPING' });
 
 export const updateCollectionMapping = (
   asyncActionCreator((collectionId, mappingId, mapping) => async () => {
-    const config = { params: { flush: true, trigger: true } };
-    const response = await endpoint.put(`collections/${collectionId}/mappings/${mappingId}`, mapping, config);
+    const response = await endpoint.put(`collections/${collectionId}/mappings/${mappingId}`, mapping);
+    executeTrigger(collectionId, mappingId);
     return { collectionId, mappingId, data: [response.data] };
   }, { name: 'UPDATE_COLLECTION_MAPPING' })
 );
 
 export const deleteCollectionMapping = asyncActionCreator((collectionId, mappingId) => async () => {
-  const config = { params: { flush: true } };
-  const response = await endpoint.delete(`collections/${collectionId}/mappings/${mappingId}`, config);
-  return { collectionId, mappingId, data: [response.data] };
+  executeFlush(collectionId, mappingId);
+  endpoint.delete(`collections/${collectionId}/mappings/${mappingId}`);
+  return { collectionId, mappingId };
 }, { name: 'DELETE_COLLECTION_MAPPING' });
-
-export const flushCollectionMapping = asyncActionCreator((collectionId, mappingId) => async () => {
-  const response = await endpoint.put(`collections/${collectionId}/mappings/${mappingId}/flush`);
-  return { collectionId, mappingId, data: [response.data] };
-}, { name: 'FLUSH_COLLECTION_MAPPING' });
 
 export const fetchCollectionMappings = asyncActionCreator((collectionId) => async () => {
   const response = await endpoint.get(`collections/${collectionId}/mappings`);
