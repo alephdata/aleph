4e351d87a565f7364e6d29c513b6e1977f1cbcb7
kjacks <kjack227@gmail.com>
2019-12-02 10:19:13 +0100

    modified argumanets of diagram reducers

--- ui/src/actions/diagramActions.js
+++ ui/src/actions/diagramActions.js
@@ -4,27 +4,27 @@ import asyncActionCreator from './asyncActionCreator';
 
 export const fetchCollectionDiagrams = asyncActionCreator((collectionId) => async () => {
   const response = await endpoint.get(`collections/${collectionId}/diagrams`);
-  return { collectionId, data: response.data.results };
+  return { data: response.data.results };
 }, { name: 'FETCH_COLLECTION_DIAGRAMS' });
 
 export const fetchCollectionDiagram = asyncActionCreator((collectionId, diagramId) => async () => {
   const response = await endpoint.get(`collections/${collectionId}/diagrams/${diagramId}`);
-  return { collectionId, data: response.data.results };
+  return { diagramId, data: response.data.results };
 }, { name: 'FETCH_COLLECTION_DIAGRAM' });
 
 export const createCollectionDiagram = asyncActionCreator((collectionId, diagram) => async () => {
   const response = await endpoint.post(`collections/${collectionId}/diagrams`, diagram);
-  return { collectionId, data: [response.data] };
+  return { diagramId, data: [response.data] };
 }, { name: 'CREATE_COLLECTION_DIAGRAM' });
 
 export const updateCollectionDiagram = (
   asyncActionCreator((collectionId, diagramId, diagram) => async () => {
     const response = await endpoint.put(`collections/${collectionId}/diagrams/${diagramId}`, diagram);
-    return { collectionId, diagramId, data: [response.data] };
+    return { diagramId, data: [response.data] };
   }, { name: 'UPDATE_COLLECTION_DIAGRAM' })
 );
 
 export const deleteCollectionDiagram = asyncActionCreator((collectionId, diagramId) => async () => {
   await endpoint.delete(`collections/${collectionId}/diagrams/${diagramId}`);
-  return { collectionId, diagramId };
+  return { diagramId };
 }, { name: 'DELETE_COLLECTION_DIAGRAM' });
---
+++ ui/src/reducers/diagrams.js
@@ -0,0 +1,33 @@
+import { createReducer } from 'redux-act';
+
+import { fetchCollectionDiagrams, fetchCollectionDiagram, updateCollectionDiagram, createCollectionDiagram, deleteCollectionDiagram } from 'src/actions';
+import { objectLoadStart, objectLoadError, objectLoadComplete, objectDelete, resultObjects } from 'src/reducers/util';
+
+const initialState = {};
+
+export default createReducer({
+  // [fetchCollectionDiagrams.START]: (state, { data }) => resultObjects(state, data),
+  //
+  // [fetchCollectionDiagrams.ERROR]: (state, {
+  //   error, args: { diagramId },
+  // }) => objectLoadError(state, diagramId, error),
+
+  [fetchCollectionDiagrams.COMPLETE]: (state, { data }) => resultObjects(state, data),
+
+  [fetchCollectionDiagram.COMPLETE]: (state, {
+    diagramId, data,
+  }) => objectLoadComplete(state, diagramId, data),
+
+  [createCollectionDiagram.COMPLETE]: (state, {
+    diagramId, data,
+  }) => objectLoadComplete(state, diagramId, data),
+
+  [updateCollectionDiagram.COMPLETE]: (state, {
+    diagramId, data,
+  }) => objectLoadComplete(state, diagramId, data),
+
+  [deleteCollectionDiagram.COMPLETE]: (state, {
+    diagramId,
+  }) => objectDelete(state, collectionId),
+
+}, initialState);
