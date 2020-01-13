f57dc5e8470b98ae37a92f804a3b30b7702d7830
Friedrich Lindenberg <friedrich@pudo.org>
2020-01-13 16:07:51 +0100

    Correct #963

--- aleph/model/role.py
+++ aleph/model/role.py
@@ -175,7 +175,7 @@ class Role(db.Model, IdModel, SoftDeleteModel):
         if foreign_id not in settings._roles:
             role_id = cls.all_ids().filter_by(foreign_id=foreign_id).first()
             if role_id is not None:
-                settings._roles[foreign_id] = role_id
+                settings._roles[foreign_id] = role_id[0]
         return settings._roles.get(foreign_id)
 
     @classmethod
