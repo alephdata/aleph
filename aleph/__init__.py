da7c9c20841eb46dc38a91ba26bd23e1581ac023
Simon Wörpel <simon.woerpel@medienrevolte.de>
2020-01-29 13:53:03 +0100

    Logging: Don't override specific loggers, rename servicelayer module

--- aleph/__init__.py
+++ aleph/__init__.py
@@ -3,7 +3,7 @@ import warnings
 from pkg_resources import get_distribution
 from sqlalchemy.exc import SAWarning
 
-from servicelayer.util import configure_logging
+from servicelayer.logs import configure_logging
 
 __version__ = get_distribution('aleph').version
 
@@ -14,28 +14,28 @@ warnings.filterwarnings('ignore', category=YAMLLoadWarning)
 warnings.filterwarnings('ignore', 'Unicode type received non-unicode bind param value.')  # noqa
 warnings.filterwarnings('ignore', category=SAWarning)
 
-# logging
-logging_level = configure_logging()
+# global logging settings
+configure_logging()
 
 # specific loggers
-logging.getLogger('faker').setLevel(logging_level)
-logging.getLogger('flanker').setLevel(logging_level)
-logging.getLogger('flanker.addresslib').setLevel(logging_level)
-logging.getLogger('langid').setLevel(logging_level)
-logging.getLogger('rdflib').setLevel(logging_level)
-logging.getLogger('requests').setLevel(logging_level)
-logging.getLogger('urllib3').setLevel(logging_level)
-logging.getLogger('elasticsearch').setLevel(logging_level)
-logging.getLogger('redis').setLevel(logging_level)
-logging.getLogger('s3transfer').setLevel(logging_level)
-logging.getLogger('amqp').setLevel(logging_level)
-logging.getLogger('polyglot').setLevel(logging_level)
-logging.getLogger('PIL').setLevel(logging_level)
-logging.getLogger('neo4j').setLevel(logging_level)
-logging.getLogger('pdfminer').setLevel(logging_level)
-logging.getLogger('httpstream').setLevel(logging_level)
-logging.getLogger('factory').setLevel(logging_level)
-logging.getLogger('polyglot').setLevel(logging_level)
+logging.getLogger('faker').setLevel(logging.WARNING)
+logging.getLogger('flanker').setLevel(logging.WARNING)
+logging.getLogger('flanker.addresslib').setLevel(logging.ERROR)
+logging.getLogger('langid').setLevel(logging.WARNING)
+logging.getLogger('rdflib').setLevel(logging.WARNING)
+logging.getLogger('requests').setLevel(logging.WARNING)
+logging.getLogger('urllib3').setLevel(logging.ERROR)
+logging.getLogger('elasticsearch').setLevel(logging.WARNING)
+logging.getLogger('redis').setLevel(logging.DEBUG)
+logging.getLogger('s3transfer').setLevel(logging.WARNING)
+logging.getLogger('amqp').setLevel(logging.INFO)
+logging.getLogger('polyglot').setLevel(logging.WARNING)
+logging.getLogger('PIL').setLevel(logging.WARNING)
+logging.getLogger('neo4j').setLevel(logging.WARNING)
+logging.getLogger('pdfminer').setLevel(logging.WARNING)
+logging.getLogger('httpstream').setLevel(logging.WARNING)
+logging.getLogger('factory').setLevel(logging.WARNING)
+logging.getLogger('polyglot').setLevel(logging.WARNING)
 # logging.getLogger('sqlalchemy').setLevel(log_level)
 
 # Log all SQL statements:
