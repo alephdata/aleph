1c891633a96eac43641d6677a05df1f948dbae4e
kjacks <kjack227@gmail.com>
2020-03-13 11:26:07 +0100

    modified skeleton to use react.createElement

--- ui/src/components/common/Skeleton.jsx
+++ ui/src/components/common/Skeleton.jsx
@@ -7,73 +7,28 @@ import './Skeleton.scss';
 class TextSkeleton extends PureComponent {
   render() {
     const { className, length, type } = this.props;
-    const placeholder = "-".repeat(length);
+    const placeholder = '-'.repeat(length);
 
-    switch (type) {
-      case 'h1':
-        return (
-          <h1 className={c(Classes.SKELETON, className)}>{placeholder}</h1>
-        );
-      case 'h4':
-        return (
-          <h4 className={c(Classes.SKELETON, className)}>{placeholder}</h4>
-        );
-      case 'h6':
-        return (
-          <h6 className={c(Classes.SKELETON, className)}>{placeholder}</h6>
-        );
-
-      case 'p':
-        return (
-          <p className={c(Classes.SKELETON, className)}>{placeholder}</p>
-        );
-
-      case 'span':
-        return (
-          <span className={c(Classes.SKELETON, className)}>{placeholder}</span>
-        );
-
-      case 'li':
-        return (
-          <li className={c(Classes.SKELETON, className)}>{placeholder}</li>
-        );
-    }
+    return React.createElement(
+      type,
+      { className: c(Classes.SKELETON, className) },
+      placeholder,
+    );
   }
 }
 
 class LayoutSkeleton extends PureComponent {
   render() {
-    const { itemHeight, colCount, rowCount, type } = this.props;
+    const { itemHeight, colCount, type } = this.props;
 
-    const rows = rowCount ? [...Array(rowCount).keys()] : [];
     const columns = colCount ? [...Array(colCount).keys()] : [];
 
     switch (type) {
-      case 'grid':
-        return (
-          <div className="LayoutSkeleton grid">
-            {columns.map((i) => (
-              <div className={Classes.SKELETON} key={i} style={{ height: itemHeight }} />
-            ))}
-          </div>
-        );
-      case 'multi-column':
+      default:
         return (
           <div className="LayoutSkeleton multi-column">
             {columns.map((i) => (
               <div className={Classes.SKELETON} key={i} style={{ height: itemHeight }} />
-            ))}
-          </div>
-        );
-      case 'table':
-        return (
-          <div className="LayoutSkeleton table">
-            {rows.map((x, i) => (
-              <div className="table-row" key={i}>
-                {columns.map((x, j) => (
-                  <div className={c('table-item', Classes.SKELETON)} key={j} style={{ height: itemHeight }} />
-                ))}
-              </div>
             ))}
           </div>
         );
