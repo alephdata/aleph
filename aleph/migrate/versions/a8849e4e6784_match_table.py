b682339c19702e01de228d0ff982ca086ef9906f
Friedrich Lindenberg <friedrich@pudo.org>
2020-03-15 14:17:59 +0100

    Drop the old xref table

---
+++ aleph/migrate/versions/a8849e4e6784_match_table.py
@@ -0,0 +1,22 @@
+"""Remove the match table.
+
+Revision ID: a8849e4e6784
+Revises: 2979a1322381
+Create Date: 2020-03-14 20:16:35.882396
+
+"""
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = 'a8849e4e6784'
+down_revision = '2979a1322381'
+
+
+def upgrade():
+    op.drop_index('ix_match_collection_id', table_name='match')
+    op.drop_index('ix_match_match_collection_id', table_name='match')
+    op.drop_table('match')
+
+
+def downgrade():
+    pass
