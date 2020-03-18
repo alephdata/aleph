30e735689304294b228a77304194b62323af443c
Friedrich Lindenberg <friedrich@pudo.org>
2020-03-19 00:07:37 +0100

    Introduce a few helpers

--- services/ingest-file/ingestors/documents/cellebrite.py
+++ services/ingest-file/ingestors/documents/cellebrite.py
@@ -19,25 +19,30 @@ class CellebriteIngestor(Ingestor, EncodingSupport, CellebriteSupport):
         root = doc.getroot()
 
         project_id = root.get('id')
-        print(project_id)
+        entity.add('messageId', project_id)
+        owner = None
 
-        self.device_owner = self.manager.make_entity('LegalEntity')
-        self.device_id = root.xpath('./ns:metadata/ns:item[@name="DeviceInfoUniqueID"][1]/text()', namespaces=ns)[0]  # noqa
-        imei = root.xpath('./ns:metadata/ns:item[@name="IMEI"][1]/text()', namespaces=ns)[0]  # noqa
-        self.device_owner.make_id(self.device_id, imei)
-        emails = root.xpath('./ns:metadata/ns:item[@name="DeviblceInfoAppleID"]/text()', namespaces=ns)  # noqa
-        self.device_owner.add('email', emails)
-        name = root.xpath('./ns:metadata/ns:item[@name="DeviceInfoOwnerName"]/text()', namespaces=ns)  # noqa
-        self.device_owner.add('name', name)
-        phone_numbers = root.xpath('./ns:metadata/ns:item[@name="MSISDN"]/text()', namespaces=ns)  # noqa
-        self.device_owner.add('phone', phone_numbers)
-        self.manager.emit_entity(self.device_owner)
+        for meta in root.xpath('./ns:metadata', namespaces=ns):
+            owner = self.manager.make_entity('LegalEntity')
+            owner.add('proof', entity)
+            identities = set()
+            identities.update(meta.xpath('./ns:item[@name="DeviceInfoUniqueID"]/text()', namespaces=ns))  # noqa
+            identities.update(meta.xpath('./ns:item[@name="IMEI"]/text()', namespaces=ns))  # noqa
+            if len(identities) and not owner.id:
+                owner.make_id(project_id, *sorted(identities))
+            owner.add('email', meta.xpath('./ns:item[@name="DeviceInfoAppleID"]/text()', namespaces=ns))  # noqa
+            owner.add('name', meta.xpath('./ns:item[@name="DeviceInfoOwnerName"]/text()', namespaces=ns))  # noqa
+            owner.add('phone', meta.xpath('./ns:item[@name="MSISDN"]/text()', namespaces=ns))  # noqa
 
-        self.parse_calls(root)
-        self.parse_messages(root)
-        self.parse_notes(root)
-        self.parse_sms(root)
-        self.parse_contacts(root)
+        if owner.id is not None:
+            self.manager.emit_entity(owner)
+
+        for decoded in root.xpath('/ns:project/ns:decodedData', namespaces=ns):
+            self.parse_calls(entity, project_id, decoded, owner)
+            self.parse_messages(entity, project_id, decoded, owner)
+            self.parse_notes(entity, project_id, decoded)
+            self.parse_sms(entity, project_id, decoded)
+            self.parse_contacts(entity, project_id, decoded)
 
     @classmethod
     def match(cls, file_path, entity):
