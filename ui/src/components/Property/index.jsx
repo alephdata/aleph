84e46549d24a3737ef4c7d97a4ba3fe7956c9a48
kjacks <kjack227@gmail.com>
2020-04-30 16:13:38 +0200

    extended Property to pull from vislib

--- ui/src/components/Property/index.jsx
+++ ui/src/components/Property/index.jsx
@@ -1,40 +1,23 @@
 import React, { Component, PureComponent } from 'react';
-import { FormattedMessage } from 'react-intl';
+import { connect } from 'react-redux';
+import { Property as VLProperty } from '@alephdata/vislib';
 
-import { Value, Values } from 'src/components/Property/Value';
+import { selectLocale } from 'src/selectors';
 import { ValueLink, ValueLinks } from 'src/components/Property/Link';
 
 import './Property.scss';
 
-class Name extends PureComponent {
-  render() {
-    return this.props.prop.label;
-  }
-}
+const mapStateToProps = state => ({
+  locale: selectLocale(state),
+});
 
-class Reverse extends PureComponent {
-  render() {
-    const { prop } = this.props;
-    if (!prop.hasReverse) {
-      return <FormattedMessage id="property.inverse" defaultMessage="'{label}' of …" values={prop} />;
-    }
-    const reverseProp = prop.getReverse();
-    return reverseProp.label;
-  }
-}
+class Property extends VLProperty {
 
-class Property extends Component {
-    static Name = Name;
+  static Reverse = connect(mapStateToProps)(super.Reverse);
 
-    static Reverse = Reverse;
+  static Link = ValueLink;
 
-    static Value = Value;
-
-    static Values = Values;
-
-    static Link = ValueLink;
-
-    static Links = ValueLinks;
+  static Links = ValueLinks;
 }
 
 export default Property;
