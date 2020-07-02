c231be1cb5cc2d96ce945d92c5ea1281de5f2e5f
Tarashish Mishra <sunu0000@gmail.com>
2020-07-02 12:47:42 +0530

    Use app context

--- aleph/worker.py
+++ aleph/worker.py
@@ -3,6 +3,7 @@ from servicelayer.jobs import Dataset
 from servicelayer.worker import Worker
 
 from aleph.core import kv, db
+from aleph.wsgi import app
 from aleph.model import Collection
 from aleph.queues import get_rate_limit
 from aleph.queues import OP_INDEX, OP_REINDEX, OP_REINGEST, OP_XREF
@@ -31,43 +32,45 @@ class AlephWorker(Worker):
         self.daily = get_rate_limit('daily', unit=3600, interval=24, limit=1)
 
     def periodic(self):
-        db.session.remove()
-        if self.hourly.check():
-            self.hourly.update()
-            log.info("Running hourly tasks...")
-            self.cleanup_jobs()
-            compute_collections()
-            check_alerts()
+        with app.app_context():
+            db.session.remove()
+            if self.hourly.check():
+                self.hourly.update()
+                log.info("Running hourly tasks...")
+                self.cleanup_jobs()
+                compute_collections()
+                check_alerts()
 
-        if self.daily.check():
-            self.daily.update()
-            log.info("Running daily tasks...")
-            generate_digest()
-            update_roles()
+            if self.daily.check():
+                self.daily.update()
+                log.info("Running daily tasks...")
+                generate_digest()
+                update_roles()
 
     def handle(self, task):
-        stage = task.stage
-        payload = task.payload
-        collection = Collection.by_foreign_id(task.job.dataset.name)
-        if collection is None:
-            log.error("Collection not found: %s", task.job.dataset)
-            return
-        sync = task.context.get('sync', False)
-        if stage.stage == OP_INDEX:
-            index_many(stage, collection, sync=sync, **payload)
-        if stage.stage == OP_LOAD_MAPPING:
-            load_mapping(stage, collection, **payload)
-        if stage.stage == OP_FLUSH_MAPPING:
-            flush_mapping(stage, collection, sync=sync, **payload)
-        if stage.stage == OP_REINGEST:
-            reingest_collection(collection, job_id=stage.job.id, **payload)
-        if stage.stage == OP_REINDEX:
-            reindex_collection(collection, sync=sync, **payload)
-        if stage.stage == OP_XREF:
-            xref_collection(stage, collection)
-        if stage.stage == OP_XREF_ITEM:
-            xref_item(stage, collection, **payload)
-        log.info("Task [%s]: %s (done)", task.job.dataset, stage.stage)
+        with app.app_context():
+            stage = task.stage
+            payload = task.payload
+            collection = Collection.by_foreign_id(task.job.dataset.name)
+            if collection is None:
+                log.error("Collection not found: %s", task.job.dataset)
+                return
+            sync = task.context.get('sync', False)
+            if stage.stage == OP_INDEX:
+                index_many(stage, collection, sync=sync, **payload)
+            if stage.stage == OP_LOAD_MAPPING:
+                load_mapping(stage, collection, **payload)
+            if stage.stage == OP_FLUSH_MAPPING:
+                flush_mapping(stage, collection, sync=sync, **payload)
+            if stage.stage == OP_REINGEST:
+                reingest_collection(collection, job_id=stage.job.id, **payload)
+            if stage.stage == OP_REINDEX:
+                reindex_collection(collection, sync=sync, **payload)
+            if stage.stage == OP_XREF:
+                xref_collection(stage, collection)
+            if stage.stage == OP_XREF_ITEM:
+                xref_item(stage, collection, **payload)
+            log.info("Task [%s]: %s (done)", task.job.dataset, stage.stage)
 
     def cleanup_job(self, job):
         if job.is_done():
@@ -82,7 +85,8 @@ class AlephWorker(Worker):
                 self.cleanup_job(job)
 
     def after_task(self, task):
-        self.cleanup_job(task.job)
+        with app.app_context():
+            self.cleanup_job(task.job)
 
 
 def get_worker():
