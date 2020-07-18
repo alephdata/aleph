3893577758040b4e7880a60c3247a4b97173e48a
Friedrich Lindenberg <friedrich@pudo.org>
2020-07-18 09:59:17 +0200

    mini-fixes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aleph",
-    version="version='3.8.7'",
+    version="3.8.7",
     description="Document sifting web frontend",
     long_description="",
     classifiers=[
