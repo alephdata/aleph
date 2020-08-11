44b083b1b699f6a80eb9f4e2d3a694bb136168ee
Friedrich Lindenberg <friedrich@pudo.org>
2020-08-11 19:13:28 +0200

    Refactor a bit

--- aleph/logic/mref.py
+++ aleph/logic/mref.py
@@ -9,6 +9,7 @@ from aleph.model import Entity
 from aleph.index.entities import iter_proxies
 from aleph.logic.aggregator import get_aggregator
 from aleph.logic.collections import reindex_collection
+from aleph.index.collections import delete_entities
 from aleph.logic.xref import _query_item
 from aleph.index import xref as index
 
@@ -17,14 +18,13 @@ log = logging.getLogger(__name__)
 ORIGIN = "xref"
 
 
-def combine_schemata(*schemata):
-    schema = Entity.LEGAL_ENTITY
+def apply_schemata(proxy, schemata):
     for other in schemata:
         try:
-            schema = model.common_schema(schema, other)
+            other = model.get(other)
+            proxy.schema = model.common_schema(proxy.schema, other)
         except InvalidData:
-            schema = Entity.LEGAL_ENTITY
-    return model.get(schema)
+            proxy.schema = model.get(Entity.LEGAL_ENTITY)
 
 
 def _iter_mentions(collection):
@@ -40,7 +40,7 @@ def _iter_mentions(collection):
                 yield proxy
             proxy = model.make_entity(Entity.LEGAL_ENTITY)
             proxy.id = mention.first("resolved")
-        proxy.schema = combine_schemata(proxy.schema, mention.get("detectedSchema"))
+        apply_schemata(proxy, mention.get("detectedSchema"))
         proxy.add("name", mention.get("name"))
         proxy.add("country", mention.get("contextCountry"))
     if proxy.id is not None:
@@ -51,8 +51,6 @@ def _generate_matches(collection):
     aggregator = get_aggregator(collection, origin=ORIGIN)
     writer = aggregator.bulk()
     for proxy in _iter_mentions(collection):
-        log.debug("Mention de-ref: %r", proxy)
-
         schemata = set()
         countries = set()
         for score, _, collection_id, match in _query_item(proxy):
@@ -65,15 +63,18 @@ def _generate_matches(collection):
             countries = countries.intersection(proxy.get("country"))
             proxy.set("country", countries)
             # Try to be more specific about schema:
-            proxy.schema = combine_schemata(proxy.schema, *schemata)
+            apply_schemata(proxy, schemata)
             # Pick a principal name:
             proxy = name_entity(proxy)
+            proxy.context["mutable"] = True
+            log.debug("Reifying [%s]: %s", proxy.schema.name, proxy)
             writer.put(proxy, fragment="mention")
-            pprint(proxy.to_dict())
+            # pprint(proxy.to_dict())
     writer.flush()
     aggregator.close()
 
 
 def collection_mentions(collection):
     index.index_matches(collection, _generate_matches(collection))
+    delete_entities(collection.id, origin=ORIGIN, sync=True)
     reindex_collection(collection, sync=False)
