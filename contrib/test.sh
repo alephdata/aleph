2e735df3d36fc3ac0bd2a8b0d5b9b890abe5c4d2
Alban Diquet <nabla.c0d3@gmail.com>
2020-08-11 18:19:20 -0700

    Add mypy to test script

--- contrib/test.sh
+++ contrib/test.sh
@@ -3,4 +3,5 @@
 psql -c "DROP DATABASE IF EXISTS aleph_test;" $ALEPH_DATABASE_URI
 psql -c "CREATE DATABASE aleph_test;" $ALEPH_DATABASE_URI
 
+mypy --ignore-missing-imports --no-implicit-optional ./aleph
 nosetests --with-coverage --cover-package=aleph --cover-erase
