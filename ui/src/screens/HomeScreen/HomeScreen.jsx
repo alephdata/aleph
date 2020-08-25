df4bef0de2277cc42f35618204f806c55ac93373
kjacks <kjack227@gmail.com>
2020-08-25 15:33:15 +0200

    Skeleton of new home page structure

--- ui/src/screens/HomeScreen/HomeScreen.jsx
+++ ui/src/screens/HomeScreen/HomeScreen.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import ReactMarkdown from 'react-markdown/with-html';
 import queryString from 'query-string';
 import { Redirect } from 'react-router-dom';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { defineMessages, injectIntl } from 'react-intl';
 
-import { SearchBox } from 'components/common';
+import { AnimatedCount, SearchBox } from 'components/common';
 import { fetchStatistics } from 'actions/index';
 import { selectMetadata, selectSession, selectStatistics } from 'selectors';
 import Screen from 'components/Screen/Screen';
@@ -24,6 +25,18 @@ const messages = defineMessages({
   placeholder: {
     id: 'home.placeholder',
     defaultMessage: 'Try searching: {samples}',
+  },
+  count_entities: {
+    id: 'home.counts.entities',
+    defaultMessage: 'Public entities',
+  },
+  count_datasets: {
+    id: 'home.counts.datasets',
+    defaultMessage: 'Public datasets',
+  },
+  count_countries: {
+    id: 'home.counts.countries',
+    defaultMessage: 'Countries & territories',
   },
 });
 
@@ -55,32 +68,68 @@ export class HomeScreen extends Component {
     }
 
     const appDescription = metadata.app.description;
+    const appHomePage = metadata.pages.find(page => page.home);
+    const isHtml = appHomePage.html;
 
     const samples = wordList(metadata.app.samples, ', ').join('');
+
+    console.log('metadata home', appHomePage);
+    console.log('stats', statistics);
     return (
       <Screen
         isHomepage
         title={intl.formatMessage(messages.title)}
         description={metadata.app.description}
       >
-        <section className="HomeScreen">
-          <div className="HomeScreen__section title-section">
+        <div className="HomeScreen">
+          <section className="HomeScreen__section">
             <div className="HomeScreen__section__content">
-              <div className="HomeScreen__text-container">
-                <h1 className="HomeScreen__title">{metadata.app.title}</h1>
-                {appDescription && (
-                  <p className="HomeScreen__description">{appDescription}</p>
-                )}
-              </div>
+              <h1 className="HomeScreen__title">{metadata.app.title}</h1>
+              {appDescription && (
+                <p className="HomeScreen__description">{appDescription}</p>
+              )}
               <SearchBox
                 onSearch={this.onSubmit}
                 placeholder={intl.formatMessage(messages.placeholder, { samples })}
                 inputProps={{ large: true, autoFocus: true }}
               />
+              <div className="HomeScreen__thirds">
+                <AnimatedCount
+                  count={statistics.things}
+                  isPending={statistics.isPending}
+                  label={intl.formatMessage(messages.count_entities)}
+                />
+                <AnimatedCount
+                  count={statistics.collections}
+                  isPending={statistics.isPending}
+                  label={intl.formatMessage(messages.count_datasets)}
+                />
+                <AnimatedCount
+                  count={statistics.countries?.size}
+                  isPending={statistics.isPending}
+                  label={intl.formatMessage(messages.count_countries)}
+                />
+              </div>
+            </div>
+          </section>
+          {appHomePage.content && (
+            <ReactMarkdown
+              escapeHtml={!isHtml}
+            >
+              {appHomePage.content}
+            </ReactMarkdown>
+          )}
+          <section className="HomeScreen__section">
+            <div className="HomeScreen__section__content">
               <StatisticsGroup statistics={statistics} />
             </div>
-          </div>
-        </section>
+          </section>
+          <section className="HomeScreen__section">
+            <div className="HomeScreen__section__content">
+            {/* Aleph technical details: version, link to docs, github, etc. */}
+            </div>
+          </section>
+        </div>
       </Screen>
     );
   }
