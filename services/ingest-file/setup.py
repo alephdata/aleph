fd342c5aae820c329ddfbe74d86bcd5caa605afe
Friedrich Lindenberg <friedrich@pudo.org>
2020-09-01 18:31:47 +0200

    Bump version: 3.8.9 → 3.9.0

--- services/ingest-file/setup.py
+++ services/ingest-file/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ingest",
-    version="3.8.9",
+    version="3.9.0",
     author="Organized Crime and Corruption Reporting Project",
     packages=find_packages(exclude=["tests"]),
     package_dir={"ingestors": "ingestors"},
