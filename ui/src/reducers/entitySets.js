9dd833876f8eeff957158d936c8827cb6615c51f
kjacks <kjack227@gmail.com>
2020-11-12 12:47:54 +0100

    Ensured fetchEntitySet goes through proper start - complete lifecycle

--- ui/src/reducers/entitySets.js
+++ ui/src/reducers/entitySets.js
@@ -1,20 +1,22 @@
 import { createReducer } from 'redux-act';
 
 import { queryEntitySets, fetchEntitySet, updateEntitySet, createEntitySetMutate, createEntitySetNoMutate, deleteEntitySet } from 'actions';
-import { objectLoadComplete, objectLoadError, objectDelete, resultObjects } from 'reducers/util';
+import { objectLoadComplete, objectLoadError, objectLoadStart, objectDelete, resultObjects } from 'reducers/util';
 
 const initialState = {};
 
 export default createReducer({
   [queryEntitySets.COMPLETE]: (state, { result }) => resultObjects(state, result),
 
+  [fetchEntitySet.START]: (state, { id }) => objectLoadStart(state, id),
+
   [fetchEntitySet.ERROR]: (state, {
     error, args,
   }) => objectLoadError(state, args, error),
 
   [fetchEntitySet.COMPLETE]: (state, {
-    entitySetId, data,
-  }) => objectLoadComplete(state, entitySetId, data),
+    id, data,
+  }) => objectLoadComplete(state, id, data),
 
   [createEntitySetMutate.COMPLETE]: (state, {
     entitySetId, data,
