49b6bd9ef14da8fdf674194fc32501e20d116926
Friedrich Lindenberg <friedrich@pudo.org>
2020-12-07 10:02:24 +0100

    one more infinite scroll

--- ui/src/components/CollectionIndex/CollectionIndex.jsx
+++ ui/src/components/CollectionIndex/CollectionIndex.jsx
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
-import { Waypoint } from 'react-waypoint';
 
 import { queryCollections } from 'actions';
 import { selectCollectionsResult } from 'selectors';
-import { ErrorSection, SearchBox, SortingBar } from 'components/common';
+import { ErrorSection, SearchBox, SortingBar, QueryInfiniteLoad } from 'components/common';
 import QueryTags from 'components/QueryTags/QueryTags';
 import CollectionIndexItem from './CollectionIndexItem';
 
 import './CollectionIndex.scss';
+
 
 export class CollectionIndex extends Component {
   constructor(props) {
@@ -18,29 +18,12 @@ export class CollectionIndex extends Component {
 
     this.onSearch = this.onSearch.bind(this);
     this.updateQuery = this.updateQuery.bind(this);
-    this.getMoreResults = this.getMoreResults.bind(this);
-  }
-
-  componentDidMount() {
-    this.fetchIfNeeded();
-  }
-
-  componentDidUpdate() {
-    this.fetchIfNeeded();
   }
 
   onSearch(queryText) {
     const { query } = this.props;
-
     const newQuery = query.set('q', queryText);
     this.updateQuery(newQuery);
-  }
-
-  getMoreResults() {
-    const { query, result } = this.props;
-    if (result && !result.isPending && result.next && !result.isError) {
-      this.props.queryCollections({ query, result, next: result.next });
-    }
   }
 
   updateQuery(newQuery) {
@@ -50,13 +33,6 @@ export class CollectionIndex extends Component {
       pathname: location.pathname,
       search: newQuery.toLocation(),
     });
-  }
-
-  fetchIfNeeded() {
-    const { query, result } = this.props;
-    if (result.shouldLoad) {
-      this.props.queryCollections({ query });
-    }
   }
 
   renderErrors() {
@@ -80,26 +56,19 @@ export class CollectionIndex extends Component {
     const skeletonItems = [...Array(10).keys()];
 
     return (
-      <>
-        <ul className="index">
-          {result.results !== undefined && result.results.map(
-            res => <CollectionIndexItem key={res.id} collection={res} />,
-          )}
-          {result.isPending && skeletonItems.map(
-            item => <CollectionIndexItem key={item} isPending />,
-          )}
-        </ul>
-        <Waypoint
-          onEnter={this.getMoreResults}
-          bottomOffset="-300px"
-          scrollableAncestor={window}
-        />
-      </>
+      <ul className="index">
+        {result.results !== undefined && result.results.map(
+          res => <CollectionIndexItem key={res.id} collection={res} />,
+        )}
+        {result.isPending && skeletonItems.map(
+          item => <CollectionIndexItem key={item} isPending />,
+        )}
+      </ul>
     );
   }
 
   render() {
-    const { placeholder, query, showQueryTags } = this.props;
+    const { placeholder, query, result, showQueryTags } = this.props;
 
     return (
       <div className="CollectionIndex">
@@ -121,6 +90,11 @@ export class CollectionIndex extends Component {
         </div>
         {this.renderErrors()}
         {this.renderResults()}
+        <QueryInfiniteLoad
+          query={query}
+          result={result}
+          fetch={this.props.queryCollections}
+        />
       </div>
     );
   }
