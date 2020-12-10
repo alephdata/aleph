812cdac29cba0bffcfd33eeaeda50b53f788bd8d
Friedrich Lindenberg <friedrich@pudo.org>
2020-12-10 19:10:52 +0100

    Directly return entity links.

--- ui/src/util/getEntityLink.js
+++ ui/src/util/getEntityLink.js
@@ -1,4 +1,9 @@
+import getProfileLink from './getProfileLink';
+
 export default function getEntityLink(entity, profile = true) {
+  if (profile && entity?.profileId) {
+    return getProfileLink(entity.profileId, { via: entity.id });
+  }
   const entityId = typeof entity === "string" ? entity : entity?.id;
   const fragment = !profile ? '#profile=false' : '';
   return entityId ? `/entities/${entityId}${fragment}` : null;
