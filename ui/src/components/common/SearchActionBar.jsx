156f577b1ba0c12389d06ad022c30382b5582e6b
kjacks <kjack227@gmail.com>
2020-12-14 12:42:31 +0100

    cleanup

--- ui/src/components/common/SearchActionBar.jsx
+++ ui/src/components/common/SearchActionBar.jsx
@@ -1,31 +1,20 @@
-import React, { Component } from 'react';
-import { defineMessages, injectIntl } from 'react-intl';
-import { compose } from 'redux';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router';
-import { AnchorButton, ButtonGroup, Classes, ControlGroup, Tooltip } from '@blueprintjs/core';
+import React from 'react';
+import { ControlGroup } from '@blueprintjs/core';
 
 import { ResultText } from 'components/common';
 
 import './SearchActionBar.scss';
 
 
-class SearchActionBar extends Component {
-  render() {
-    const { children, intl, result } = this.props;
-
-    return (
-      <ControlGroup className="SearchActionBar" fill>
-        <div className="SearchActionBar__main text-muted">
-          <ResultText result={result} />
-        </div>
-        {children}
-      </ControlGroup>
-    );
-  }
+const SearchActionBar = ({ children, intl, result }) => {
+  return (
+    <ControlGroup className="SearchActionBar" fill>
+      <div className="SearchActionBar__main text-muted">
+        <ResultText result={result} />
+      </div>
+      {children}
+    </ControlGroup>
+  );
 }
 
-export default compose(
-  withRouter,
-  injectIntl,
-)(SearchActionBar);
+export default SearchActionBar;
