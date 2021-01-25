207759b88e744303378af5c862af14e6420fb422
kjacks <kjack227@gmail.com>
2021-01-24 17:12:46 -0800

    removed queryText prop from non-searchable viewers

--- ui/src/components/Document/DocumentViewMode.jsx
+++ ui/src/components/Document/DocumentViewMode.jsx
@@ -67,29 +67,28 @@ export class DocumentViewMode extends React.Component {
     const processingError = document.getProperty('processingError');
 
     if (processingError && processingError.length) {
-      return <DefaultViewer document={document} queryText={queryText} dir={dir} />;
+      return <DefaultViewer document={document} dir={dir} />;
     }
 
     if (document.schema.isA('Email')) {
       if (activeMode === 'browse') {
         return (
-          <FolderViewer document={document} queryText={queryText} dir={dir} />
+          <FolderViewer document={document} dir={dir} />
         );
       }
       return (
-        <EmailViewer document={document} queryText={queryText} activeMode={activeMode} dir={dir} />
+        <EmailViewer document={document} activeMode={activeMode} dir={dir} />
       );
     }
     if (document.schema.isA('Image')) {
       if (activeMode === 'text') {
         return (
-          <TextViewer document={document} queryText={queryText} dir={dir} />
+          <TextViewer document={document} dir={dir} />
         );
       }
       return (
         <ImageViewer
           document={document}
-          queryText={queryText}
           activeMode={activeMode}
           dir={dir}
         />
@@ -109,17 +108,17 @@ export class DocumentViewMode extends React.Component {
 
     if (document.schema.isA('Table')) {
       return (
-        <TableViewer document={document} queryText={queryText} dir={dir} />
+        <TableViewer document={document} dir={dir} />
       );
     }
     if (document.schema.isA('PlainText')) {
       return (
-        <TextViewer document={document} queryText={queryText} dir={dir} />
+        <TextViewer document={document} dir={dir} />
       );
     }
     if (document.schema.isA('HyperText')) {
       return (
-        <HtmlViewer document={document} queryText={queryText} dir={dir} />
+        <HtmlViewer document={document} dir={dir} />
       );
     }
     if (document.schema.isA('Pages')) {
@@ -136,11 +135,11 @@ export class DocumentViewMode extends React.Component {
     }
     if (document.schema.isA('Folder')) {
       return (
-        <FolderViewer document={document} queryText={queryText} dir={dir} />
+        <FolderViewer document={document} dir={dir} />
       );
     }
 
-    return <DefaultViewer document={document} queryText={queryText} dir={dir} />;
+    return <DefaultViewer document={document} dir={dir} />;
   }
 
   render() {
