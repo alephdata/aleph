c4f715a7a1d370075229948dfe0275eab4b457da
kjacks <kjack227@gmail.com>
2021-01-28 15:45:29 -0800

    Added dynamic polyfill for react-intl

--- ui/src/app/Translator.jsx
+++ ui/src/app/Translator.jsx
@@ -1,23 +1,54 @@
 import React from 'react';
 import { connect } from 'react-redux';
-
 import { IntlProvider } from 'react-intl';
-// import en from 'react-intl/locale-data/en';
-// import de from 'react-intl/locale-data/de';
-// import bs from 'react-intl/locale-data/bs';
-// import ru from 'react-intl/locale-data/ru';
-// import es from 'react-intl/locale-data/es';
-// import ar from 'react-intl/locale-data/ar';
+import { shouldPolyfill } from '@formatjs/intl-relativetimeformat/should-polyfill'
+import { Spinner } from '@blueprintjs/core';
 
 import { selectLocale } from 'selectors';
 import translations from 'content/translations.json';
+import { selectLocale } from 'selectors';
 
-// add locale data to react-intl
-// addLocaleData([...en, ...de, ...bs, ...es, ...ru, ...ar]);
 
-class Translator extends React.PureComponent {
+class Translator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.requiresPollyfill = shouldPolyfill();
+    this.state = { isPending: this.requiresPollyfill };
+  }
+
+  componentDidMount() {
+    if (this.requiresPollyfill) {
+      this.fetchPolyfill();
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.requiresPolyfill && prevProps.locale !== this.props.locale) {
+      this.fetchLocale()
+    }
+  }
+
+  fetchPolyfill = async () => {
+    await import('@formatjs/intl-locale/polyfill')
+    await import('@formatjs/intl-pluralrules/polyfill');
+    await import('@formatjs/intl-relativetimeformat/polyfill');
+    this.fetchLocale();
+  }
+
+  fetchLocale = async () => {
+    const { locale } = this.props;
+    await import(`@formatjs/intl-pluralrules/locale-data/${locale}`)
+    await import(`@formatjs/intl-relativetimeformat/locale-data/${locale}`)
+    this.setState({ isPending: false });
+  }
+
   render() {
     const { children, locale } = this.props;
+    const { isPending } = this.state;
+
+    if (isPending) {
+      return <div className="spinner"><Spinner className="bp3-large" /></div>
+    }
 
     //  override arabic locale to marocan version 
     // We want all dates and numbers in latin instead of default ar eastern digits 
