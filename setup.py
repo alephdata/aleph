47ac45fa72607e1ab16c7c30690013a7d00be116
Friedrich Lindenberg <friedrich@pudo.org>
2021-02-10 17:35:45 +0100

    Bump version: 3.9.10 → 3.9.11

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aleph",
-    version="3.9.10",
+    version="3.9.11",
     description="Document sifting web frontend",
     long_description="",
     classifiers=[
