cb8570afcd063c77ddee94d646aa10402467f1a8
Friedrich Lindenberg <friedrich@pudo.org>
2021-04-01 12:50:08 +0200

    Fix handling of tuple

--- aleph/logic/alerts.py
+++ aleph/logic/alerts.py
@@ -20,7 +20,7 @@ def get_alert(alert_id):
 
 def check_alerts():
     """Go through all alerts."""
-    for alert_id in list(Alert.all_ids()):
+    for (alert_id,) in list(Alert.all_ids()):
         check_alert(alert_id)
 
 
