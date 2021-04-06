9454045becdc8140c72c6af481158ab0217cb259
kjacks <kjack227@gmail.com>
2021-04-06 16:50:42 +0200

    added togglable query tag display cutoff

--- ui/src/components/QueryTags/QueryTags.jsx
+++ ui/src/components/QueryTags/QueryTags.jsx
@@ -4,11 +4,15 @@ import _ from 'lodash';
 import { Button } from '@blueprintjs/core';
 import QueryFilterTag from './QueryFilterTag';
 
+const HIDDEN_TAGS_CUTOFF = 10;
+
 class QueryTags extends Component {
   constructor(props) {
     super(props);
     this.removeFilterValue = this.removeFilterValue.bind(this);
     this.removeAllFilterValues = this.removeAllFilterValues.bind(this);
+
+    this.state = { showHidden: false };
   }
 
   removeFilterValue(filter, value) {
@@ -30,6 +34,8 @@ class QueryTags extends Component {
 
   render() {
     const { query } = this.props;
+    const { showHidden } = this.state;
+
     let activeFilters = query ? query.filters() : [];
 
     if (activeFilters.length === 0) {
@@ -51,7 +57,9 @@ class QueryTags extends Component {
         .map(filter => query.getFilter(filter).map(value => ({ filter, value })))
     );
     const allTags = [...filterTags, ...addlTags];
+    const visibleTags = showHidden ? allTags : allTags.slice(0, HIDDEN_TAGS_CUTOFF);
 
+    const showHiddenToggle = !showHidden && allTags.length > HIDDEN_TAGS_CUTOFF;
     const showClearAll = allTags.length > 1;
 
     // @FIXME This should still selectively display filters for the following:
@@ -60,7 +68,7 @@ class QueryTags extends Component {
     // "?ancestors={id}"
     return (
       <div className="QueryTags">
-        {allTags.map(({ filter, value }) => (
+        {visibleTags.map(({ filter, value }) => (
           <QueryFilterTag
             filter={filter}
             value={value}
@@ -68,6 +76,19 @@ class QueryTags extends Component {
             key={value}
           />
         ))}
+        {showHiddenToggle && (
+          <Button
+            className="filter-clear-tag bp3-tag bp3-large QueryFilterTag"
+            onClick={() => this.setState({ showHidden: true })}
+            outlined
+          >
+            <FormattedMessage
+              id="queryFilters.showHidden"
+              defaultMessage="Show {count} more filters..."
+              values={{ count: allTags.length - visibleTags.length }}
+            />
+          </Button>
+        )}
         {showClearAll && (
           <Button
             className="filter-clear-tag bp3-tag bp3-large QueryFilterTag"
