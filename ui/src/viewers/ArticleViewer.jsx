7b5d9e01e751613655f5c28f2a5aff9c0934b790
Steve Haffenden <steve.haffenden@gmail.com>
2021-05-21 15:08:28 +0200

    #1801 Fomat Article to be similar to email

--- ui/src/viewers/ArticleViewer.jsx
+++ ui/src/viewers/ArticleViewer.jsx
@@ -1,20 +1,69 @@
 import React, { PureComponent } from 'react';
 import { Pre } from '@blueprintjs/core';
 
-import { Skeleton } from 'components/common';
+import { Property, Skeleton } from 'components/common';
+import wordList from 'util/wordList';
 
 import './ArticleViewer.scss';
 
 class ArticleViewer extends PureComponent {
-  render() {
-    const { document, dir, noStyle } = this.props;
+  headerProperty(name, entitiesProp) {
+    const { document } = this.props;
+    const prop = document.schema.getProperty(name);
+    const values = document.getProperty(prop).map((value) => {
+      let result = <Property.Value key={value.id || value} prop={prop} value={value} />;
+      return result;
+    });
+
+    if (values.length === 0) {
+      return null;
+    }
+    
+    return (
+      <tr key={prop.qname}>
+        <th>{prop.label}</th>
+        <td>{wordList(values, ', ')}</td>
+      </tr>
+    );
+  }
+
+  renderHeaders() {
+    const { document } = this.props;
+    if (document.isPending) {
+      return null;
+    }
+
+    return (
+      <div className="article-header">
+      <table className="bp3-html-table">
+        <tbody>
+        {this.headerProperty('title')}
+        {this.headerProperty('author')}
+        {this.headerProperty('publishedAt')}
+        {this.headerProperty('description')}
+        </tbody>
+      </table>
+      </div>
+    );
+  }
+  
+  renderBody() {
+    const { document } = this.props;
     const text = document.isPending
       ? <Skeleton.Text type="pre" length={4000} />
       : <Pre>{document.getFirst('bodyText')}</Pre>;
-    return noStyle ? text : (
+    return (text);
+  }
+
+  render() {
+    const { dir } = this.props;
+    return (
       <div className="outer">
-        <div className="inner TextViewer" dir={dir}>
-          {text}
+        <div className="inner ArticleViewer" dir={dir}>
+          {this.renderHeaders()}
+          <div className="article-body" dir={dir}>
+            {this.renderBody()}
+          </div>
         </div>
       </div>
     );
