dacb533adf8dbb2dec75bdc2333105ca2f033582
Steve Haffenden <steve.haffenden@gmail.com>
2021-06-08 10:26:03 +0200

    Bump version: 3.10.4 → 3.10.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aleph",
-    version="3.10.4",
+    version="3.10.5",
     description="Document sifting web frontend",
     classifiers=[
         "Intended Audience :: Developers",
