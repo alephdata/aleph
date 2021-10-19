5e71becfcd0f9285c4baa419ef0a177cbf8e729a
kjacks <kjack227@gmail.com>
2021-10-19 11:58:19 +0200

    Refactored filterIntervals date util function to more flexibly handle dates

--- ui/src/components/Facet/util.js
+++ ui/src/components/Facet/util.js
@@ -1,47 +1,54 @@
 const DEFAULT_START_INTERVAL = 1950;
-const ES_SUFFIX = '||/y';
 
-const formatDateQParam = (datetime) => {
-  return `${new Date(datetime).getFullYear()}||/y`
+const formatDateQParam = (datetime, granularity) => {
+  const dateObj = new Date(datetime)
+  if (granularity === 'month') {
+    return `${datetime}||/M`
+  }
+  return `${datetime}||/y`
 };
 
 const cleanDateQParam = (value) => {
-  return value.replace(ES_SUFFIX, '');
+  return value
+    .replace('||/y', '')
+    .replace('||/M', '');
 };
 
 const timestampToYear = timestamp => {
   return new Date(timestamp).getFullYear();
 }
 
-const filterDateIntervals = ({ query, intervals, useDefaultBounds }) => {
-  const defaultEndInterval = new Date().getFullYear();
-  const hasGtFilter = query.hasFilter('gte:dates');
-  const hasLtFilter = query.hasFilter('lte:dates');
+const filterDateIntervals = ({ field, query, intervals, useDefaultBounds }) => {
+  const defaultEndInterval = new Date();
+  const hasGtFilter = query.hasFilter(`gte:${field}`);
+  const hasLtFilter = query.hasFilter(`lte:${field}`);
 
-  const gt = hasGtFilter
-    ? cleanDateQParam(query.getFilter('gte:dates')[0])
+  const gtRaw = hasGtFilter
+    ? cleanDateQParam(query.getFilter(`gte:${field}`)[0])
     : (useDefaultBounds && DEFAULT_START_INTERVAL);
 
-  const lt = hasLtFilter
-    ? cleanDateQParam(query.getFilter('lte:dates')[0])
+  const ltRaw = hasLtFilter
+    ? cleanDateQParam(query.getFilter(`lte:${field}`)[0])
     : (useDefaultBounds && defaultEndInterval);
 
-  let gtOutOfRange, ltOutOfRange = false;
+  const gt = gtRaw && new Date(gtRaw)
+  const lt = ltRaw && new Date(ltRaw)
 
+  let gtOutOfRange, ltOutOfRange = false;
   const filteredIntervals = intervals.filter(({ id }) => {
-    const year = timestampToYear(id);
-    if (gt && year < gt) {
+    const date = new Date(id)
+    if (gt && date < gt) {
       gtOutOfRange = true;
       return false;
     }
-    if (lt && year > lt) {
+    if (lt && date > lt) {
       ltOutOfRange = true;
       return false;
     }
     return true;
   })
-
   const hasOutOfRange = useDefaultBounds && ((!hasGtFilter && gtOutOfRange) || (!hasLtFilter && ltOutOfRange));
+
   return { filteredIntervals, hasOutOfRange };
 }
 
