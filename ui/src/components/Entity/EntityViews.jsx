41e276885e12181c413b6f2bf9379ba53a27bd0f
Kirk Jackson <kjack227@gmail.com>
2021-12-17 09:56:46 +0100

    Added warning callout when table entity is part of a workbook (#2069)
    All good. The change here is not as the pull request would have you believe.

--- ui/src/components/Entity/EntityViews.jsx
+++ ui/src/components/Entity/EntityViews.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
-import { Tab, Tabs, Icon } from '@blueprintjs/core';
+import { Callout, Tab, Tabs, Icon } from '@blueprintjs/core';
 import queryString from 'query-string';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import {
-  Count, Property, ResultCount, Schema, SectionLoading, TextLoading,
+  Count, Entity, Property, ResultCount, Schema, SectionLoading, TextLoading,
 } from 'components/common';
 import {
   entityReferenceQuery, entitySimilarQuery, folderDocumentsQuery
@@ -20,6 +20,8 @@ import EntityTagsMode from 'components/Entity/EntityTagsMode';
 import EntitySimilarMode from 'components/Entity/EntitySimilarMode';
 import EntityMappingMode from 'components/Entity/EntityMappingMode';
 import DocumentViewMode from 'components/Document/DocumentViewMode';
+
+import './EntityViews.scss';
 
 
 class EntityViews extends React.Component {
@@ -56,145 +58,162 @@ class EntityViews extends React.Component {
     const hasDocumentViewMode = hasViewer || (!hasBrowseMode && !hasTextMode);
     const hasViewMode = entity.schema.isDocument() && hasDocumentViewMode;
     const processingError = entity.getProperty('processingError');
+    const entityParent = entity.getFirst('parent')
+    const showWorkbookWarning = !isPreview && entity.schema.name === 'Table' && entityParent?.schema?.name === 'Workbook';
 
     return (
-      <Tabs
-        id="EntityInfoTabs"
-        onChange={this.handleTabChange}
-        selectedTabId={activeMode}
-        renderActiveTabPanelOnly
-        className="info-tabs-padding"
-      >
-        {isPreview && (
-          <Tab
-            id="info"
-            title={(
-              <>
-                <Icon icon="info" className="left-icon" />
-                <span className="tab-padding">
-                  <FormattedMessage id="entity.info.info" defaultMessage="Info" />
-                </span>
-              </>
-            )}
-            panel={
-              <EntityProperties entity={entity} />
-            }
-          />
+      <>
+        {showWorkbookWarning && (
+          <Callout className="EntityViews__workbook-warning">
+            <FormattedMessage
+              id="entity.info.workbook_warning"
+              defaultMessage="This sheet is part of workbook {link}"
+              values={{
+                link: (
+                  <Entity.Link entity={entityParent} icon />
+                )
+              }}
+            />
+          </Callout>
         )}
-        {hasViewMode && (
-          <Tab
-            id="view"
-            title={(
-              <>
-                <Icon icon="documentation" className="left-icon" />
-                <FormattedMessage id="entity.info.view" defaultMessage="View" />
-              </>
-            )}
-            panel={<DocumentViewMode document={entity} activeMode={activeMode} />}
-          />
-        )}
-        {hasTextMode && (
-          <Tab
-            id="text"
-            title={(
-              <>
-                <Icon icon="plaintext" className="left-icon" />
-                <FormattedMessage id="entity.info.text" defaultMessage="Text" />
-              </>
-            )}
-            panel={<DocumentViewMode document={entity} activeMode={activeMode} />}
-          />
-        )}
-        {hasBrowseMode && (
-          <Tab
-            id="browse"
-            disabled={children.total < 1}
-            title={(
-              <TextLoading loading={children.isPending}>
-                <Icon icon="folder" className="left-icon" />
-                { entity.schema.isA('Email') && (
-                  <FormattedMessage id="entity.info.attachments" defaultMessage="Attachments" />
-                )}
-                { !entity.schema.isA('Email') && (
-                  <FormattedMessage id="entity.info.documents" defaultMessage="Documents" />
-                )}
-                <ResultCount result={children} />
-              </TextLoading>
-            )}
-            panel={
-              <DocumentViewMode document={entity} activeMode={activeMode} />
-            }
-          />
-        )}
-        {references.results.map(ref => (
-          <Tab
-            id={ref.property.qname}
-            key={ref.property.qname}
-            title={(
-              <>
-                <Schema.Icon schema={ref.schema} className="left-icon" />
-                <Property.Reverse prop={ref.property} />
-                <Count count={ref.count} />
-              </>
-            )}
-            panel={
-              <EntityReferencesMode
-                entity={entity}
-                mode={activeMode}
-                query={referenceQuery}
-                reference={reference}
-                hideCollection={true}
-              />
-            }
-          />
-        ))}
-        {!references.total && references.isPending && (
-          <Tab
-            id='loading'
-            title={<TextLoading loading={true} />}
-          />
-        )}
-        { entity.schema.isDocument() && (!processingError || !processingError.length) && (
-          <Tab
-            id="tags"
-            disabled={tags.total < 1}
-            title={(
-              <TextLoading loading={tags.isPending}>
-                <Icon icon="assessment" className="left-icon" />
-                <FormattedMessage id="entity.info.tags" defaultMessage="Mentions" />
-                <ResultCount result={tags} />
-              </TextLoading>
-            )}
-            panel={<EntityTagsMode entity={entity} />}
-          />
-        )}
-        { entity?.schema?.matchable && !isPreview && (
-          <Tab
-            id="similar"
-            disabled={similar.total === 0}
-            title={(
-              <TextLoading loading={similar.total === undefined}>
-                <Icon icon="similar" className="left-icon" />
-                <FormattedMessage id="entity.info.similar" defaultMessage="Similar" />
-                <ResultCount result={similar} />
-              </TextLoading>
-            )}
-            panel={<EntitySimilarMode entity={entity} />}
-          />
-        )}
-        { (entity?.collection?.writeable && entity.schema.isA('Table')) && (
-          <Tab
-            id="mapping"
-            title={(
-              <>
-                <Icon icon="new-object" className="left-icon" />
-                <FormattedMessage id="entity.mapping.view" defaultMessage="Generate entities" />
-              </>
-            )}
-            panel={<EntityMappingMode document={entity} />}
-          />
-        )}
-      </Tabs>
+        <Tabs
+          id="EntityInfoTabs"
+          onChange={this.handleTabChange}
+          selectedTabId={activeMode}
+          renderActiveTabPanelOnly
+          className="info-tabs-padding"
+        >
+          {isPreview && (
+            <Tab
+              id="info"
+              title={(
+                <>
+                  <Icon icon="info" className="left-icon" />
+                  <span className="tab-padding">
+                    <FormattedMessage id="entity.info.info" defaultMessage="Info" />
+                  </span>
+                </>
+              )}
+              panel={
+                <EntityProperties entity={entity} />
+              }
+            />
+          )}
+          {hasViewMode && (
+            <Tab
+              id="view"
+              title={(
+                <>
+                  <Icon icon="documentation" className="left-icon" />
+                  <FormattedMessage id="entity.info.view" defaultMessage="View" />
+                </>
+              )}
+              panel={<DocumentViewMode document={entity} activeMode={activeMode} />}
+            />
+          )}
+          {hasTextMode && (
+            <Tab
+              id="text"
+              title={(
+                <>
+                  <Icon icon="plaintext" className="left-icon" />
+                  <FormattedMessage id="entity.info.text" defaultMessage="Text" />
+                </>
+              )}
+              panel={<DocumentViewMode document={entity} activeMode={activeMode} />}
+            />
+          )}
+          {hasBrowseMode && (
+            <Tab
+              id="browse"
+              disabled={children.total < 1}
+              title={(
+                <TextLoading loading={children.isPending}>
+                  <Icon icon="folder" className="left-icon" />
+                  { entity.schema.isA('Email') && (
+                    <FormattedMessage id="entity.info.attachments" defaultMessage="Attachments" />
+                  )}
+                  { !entity.schema.isA('Email') && (
+                    <FormattedMessage id="entity.info.documents" defaultMessage="Documents" />
+                  )}
+                  <ResultCount result={children} />
+                </TextLoading>
+              )}
+              panel={
+                <DocumentViewMode document={entity} activeMode={activeMode} />
+              }
+            />
+          )}
+          {references.results.map(ref => (
+            <Tab
+              id={ref.property.qname}
+              key={ref.property.qname}
+              title={(
+                <>
+                  <Schema.Icon schema={ref.schema} className="left-icon" />
+                  <Property.Reverse prop={ref.property} />
+                  <Count count={ref.count} />
+                </>
+              )}
+              panel={
+                <EntityReferencesMode
+                  entity={entity}
+                  mode={activeMode}
+                  query={referenceQuery}
+                  reference={reference}
+                  hideCollection={true}
+                />
+              }
+            />
+          ))}
+          {!references.total && references.isPending && (
+            <Tab
+              id='loading'
+              title={<TextLoading loading={true} />}
+            />
+          )}
+          { entity.schema.isDocument() && (!processingError || !processingError.length) && (
+            <Tab
+              id="tags"
+              disabled={tags.total < 1}
+              title={(
+                <TextLoading loading={tags.isPending}>
+                  <Icon icon="assessment" className="left-icon" />
+                  <FormattedMessage id="entity.info.tags" defaultMessage="Mentions" />
+                  <ResultCount result={tags} />
+                </TextLoading>
+              )}
+              panel={<EntityTagsMode entity={entity} />}
+            />
+          )}
+          { entity?.schema?.matchable && !isPreview && (
+            <Tab
+              id="similar"
+              disabled={similar.total === 0}
+              title={(
+                <TextLoading loading={similar.total === undefined}>
+                  <Icon icon="similar" className="left-icon" />
+                  <FormattedMessage id="entity.info.similar" defaultMessage="Similar" />
+                  <ResultCount result={similar} />
+                </TextLoading>
+              )}
+              panel={<EntitySimilarMode entity={entity} />}
+            />
+          )}
+          { (entity?.collection?.writeable && entity.schema.isA('Table')) && (
+            <Tab
+              id="mapping"
+              title={(
+                <>
+                  <Icon icon="new-object" className="left-icon" />
+                  <FormattedMessage id="entity.mapping.view" defaultMessage="Generate entities" />
+                </>
+              )}
+              panel={<EntityMappingMode document={entity} />}
+            />
+          )}
+        </Tabs>
+      </>
     );
   }
 }
