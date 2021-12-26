8f1af0ae6953e1731b64e707e4796b47469c99b8
Tolga <tolgatasci1@gmail.com>
2021-12-26 17:15:14 +0300

    Update index.js
    docker stop unresponsive fix

--- ui/src/index.js
+++ ui/src/index.js
@@ -2,7 +2,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'app/App';
-
+process.on('SIGINT', function() {
+    //Error docker stop unresponsive fix
+    process.exit(0);
+});
 ReactDOM.render(
   React.createElement(App),
   document.getElementById('root'),
