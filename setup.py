ab6ed09412b70de00b489d33b14704e4a811cf00
Tarashish Mishra <sunu0000@gmail.com>
2022-02-02 11:58:32 +0530

    Bump version: 3.11.1 → 3.12.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aleph",
-    version="3.11.1",
+    version="3.12.0",
     description="Document sifting web frontend",
     classifiers=[
         "Intended Audience :: Developers",
