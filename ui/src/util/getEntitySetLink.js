da66a3037a377a4cedd0ba13474031c77efc19c0
Steve Haffenden <steve.haffenden@gmail.com>
2022-02-07 08:08:29 +0100

    hotfix: Handle malformed notifications. Convert null value to string (#2115)
    * Convert null value to string
    * Remove blank line
    
    Refs https://github.com/alephdata/aleph/issues/2114

--- ui/src/util/getEntitySetLink.js
+++ ui/src/util/getEntitySetLink.js
@@ -1,3 +1,3 @@
 export default function getEntitySetLink(entitySet) {
-  return (entitySet && entitySet.id) ? `/${entitySet.type}s/${entitySet.id}` : null;
+  return (entitySet && entitySet.id) ? `/${entitySet.type}s/${entitySet.id}` : "";
 }
