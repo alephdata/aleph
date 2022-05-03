a12c6bc3693df447a6aac0b4b3a6753bb7ce282f
Matvey <realsmych@gmail.com>
2022-05-03 21:57:22 +0100

    Move is_auto_admin to aleph.util

--- aleph/views/roles_api.py
+++ aleph/views/roles_api.py
@@ -5,11 +5,12 @@ from flask import Blueprint, request
 from itsdangerous import BadSignature
 from werkzeug.exceptions import BadRequest
 
-from aleph.core import db, settings
+from aleph.core import db
 from aleph.authz import Authz
 from aleph.search import QueryParser, DatabaseQueryResult
 from aleph.model import Role
 from aleph.logic.roles import challenge_role, update_role, create_user, get_deep_role
+from aleph.util import is_auto_admin
 from aleph.views.serializers import RoleSerializer
 from aleph.views.util import require, jsonify, parse_request, obj_or_404
 from aleph.views.context import tag_request
@@ -146,7 +147,7 @@ def create():
             status=409,
         )
 
-    role = create_user(email, data.get("name"), data.get("password"), is_admin=settings.is_admin(email))
+    role = create_user(email, data.get("name"), data.get("password"), is_admin=is_auto_admin(email))
     # Let the serializer return more info about this user
     request.authz = Authz.from_role(role)
     tag_request(role_id=role.id)
