f967c96c2df2c243671dfc846b74c5a1ff7497ad
Matvey <realsmych@gmail.com>
2022-05-13 09:38:51 +0100

    Concatenate error messages for the same path

--- aleph/tests/test_view_util.py
+++ aleph/tests/test_view_util.py
@@ -1,7 +1,10 @@
+import json
+
 from lxml.html import document_fromstring
+from werkzeug.exceptions import BadRequest
 
 from aleph.logic.html import sanitize_html
-from aleph.views.util import get_url_path
+from aleph.views.util import get_url_path, validate
 from aleph.tests.util import TestCase
 
 
@@ -32,3 +35,33 @@ class ViewUtilTest(TestCase):
         assert attr == "https://example.org/blockchain", html
         assert html.find(".//a").get("target") == "_blank", html
         assert "nofollow" in html.find(".//a").get("rel"), html
+
+    def test_validate_returns_errors_for_paths(self):
+        # given
+        schema = "RoleCreate" # name min length 4, password min length 6
+        data = json.loads('{"name":"Bob","password":"1234","code":"token"}')
+
+        # then
+        with self.assertRaises(BadRequest):
+            validate(data, schema)
+
+        # and
+        try:
+            validate(data, schema)
+        except BadRequest as err:
+            self.assertEqual(err.response.get_json().get("errors"), {"name": "'Bob' is too short", "password": "'1234' is too short"})
+
+    def test_validate_concatenates_multiple_errors_for_the_same_path(self):
+        # given
+        schema = "RoleCreate" # requires password and code 
+        data = json.loads('{"wrong":"No password nor code"}')
+
+        # then
+        with self.assertRaises(BadRequest):
+            validate(data, schema)
+
+        # and
+        try:
+            validate(data, schema)
+        except BadRequest as err:
+            self.assertEqual(err.response.get_json().get("errors"), {"": "'password' is a required property; 'code' is a required property"})
