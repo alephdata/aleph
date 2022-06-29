43902ec2622670c5d40812924daa34fb2ff8e848
Niharika <niharika@fsfe.org>
2022-06-29 13:21:05 +0200

    making a .js file REUSE compliant

--- ui/src/util/wordList.js
+++ ui/src/util/wordList.js
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2014 2014 Emma Prest, <emma@occrp.org> et al.
+//
+// SPDX-License-Identifier: MIT
+
 /*
  * https://stackoverflow.com/questions/23618744/rendering-comma-separated-list-of-links
  */
