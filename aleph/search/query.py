f1018c420be1a8be7b3fcfee722b37f5daa0f04f
Johan Schuijt <johan.schuijt@ftm.nl>
2022-07-25 16:33:33 +0200

    remove broken debug logs

--- aleph/search/query.py
+++ aleph/search/query.py
@@ -273,15 +273,7 @@ class Query(object):
 
     def search(self):
         """Execute the query as assmbled."""
-        # log.info("Search index: %s", self.get_index())
-        result = es.search(index=self.get_index(), body=self.get_body())
-        # log.info(
-        #     f"Elasticsearch query [{self.to_text()}] took {result.get('took')}ms",
-        #     query=self.to_text(),
-        #     took=result.get("took"),
-        # )
-        # log.info("%s", pformat(self.parser.filters))
-        return result
+        return es.search(index=self.get_index(), body=self.get_body())
 
     @classmethod
     def handle(cls, request, parser=None, **kwargs):
