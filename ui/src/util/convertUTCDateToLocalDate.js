576c8e5e923c50c00134d1acfbbad41e66440f5a
Till Prochaska <till.prochaska@occrp.org>
2022-08-31 20:07:20 +0200

    Fix date helper
    Before, this helper would simply append "UTC" to the string representation of a date in order to override the time zone to UTC.
    
    This does however append multiple time zones to the date:
    
    ```js
    const date = new Date()
    console.log(`${date} UTC`);
    // "Wed Aug 31 2022 20:01:20 GMT+0200 (Central European Summer Time) UTC"
    ```
    
    Firefox and Chrome parse these date strings, even though they are invalid. Safari throws an error.

--- ui/src/util/convertUTCDateToLocalDate.js
+++ ui/src/util/convertUTCDateToLocalDate.js
@@ -1,3 +1,8 @@
+// The Aleph API returns dates as ISO strings without timezone
+// designators. When initializing new `Date` objects, those dates
+// will by default be initialized with the local timezone of the
+// user agent.
 export default function convertUTCDateToLocalDate(date) {
-  return new Date(`${date} UTC`);
+  const dateObj = new Date(date);
+  return new Date(dateObj.getTime() - dateObj.getTimezoneOffset() * 60 * 1000);
 }
