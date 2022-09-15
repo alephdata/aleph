73071ce05327c788ced926011bf0cb717dae5867
Till Prochaska <till.prochaska@occrp.org>
2022-09-15 23:05:27 +0200

    Fix upload status indicator size

--- ui/src/dialogs/DocumentUploadDialog/DocumentUploadTrace.jsx
+++ ui/src/dialogs/DocumentUploadDialog/DocumentUploadTrace.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Spinner, Icon } from '@blueprintjs/core';
+import { Spinner, SpinnerSize, Icon } from '@blueprintjs/core';
 import { UPLOAD_STATUS } from './DocumentUploadStatus';
+
+const STATUS_SIZE = SpinnerSize.SMALL;
 
 export default function DocumentUploadTrace({ trace }) {
   const renderStatus = () => {
@@ -9,7 +11,7 @@ export default function DocumentUploadTrace({ trace }) {
         return (
           <Spinner
             intent="primary"
-            size={Spinner.SIZE_SMALL}
+            size={STATUS_SIZE}
             value={trace.uploaded / trace.total}
           />
         );
@@ -18,16 +20,10 @@ export default function DocumentUploadTrace({ trace }) {
       }
     } else if (trace.status === UPLOAD_STATUS.SUCCESS) {
       return (
-        <Icon
-          intent="success"
-          icon="tick-circle"
-          iconSize={Spinner.SIZE_SMALL}
-        />
+        <Icon intent="success" icon="tick-circle" iconSize={STATUS_SIZE} />
       );
     } else if (trace.status === UPLOAD_STATUS.ERROR) {
-      return (
-        <Icon intent="danger" icon="error" iconSize={Spinner.SIZE_SMALL} />
-      );
+      return <Icon intent="danger" icon="error" iconSize={STATUS_SIZE} />;
     }
   };
 
