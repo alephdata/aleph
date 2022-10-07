9a15363e1c71858cc485d4255981be41c163db8e
Steve Haffenden <steve.haffenden@occrp.org>
2022-10-07 08:24:55 +0200

    Bump version: 3.12.7-rc3 → 3.12.7-rc4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aleph",
-    version="3.12.7-rc3",
+    version="3.12.7-rc4",
     description="Document sifting web frontend",
     classifiers=[
         "Intended Audience :: Developers",
