067d7e653391fe78889c615516a8767b98e250de
Steve Haffenden <steve.haffenden@occrp.org>
2022-10-31 09:21:27 +0100

    Updatings pages and general document organisation

--- docs/aleph/src/config.ts
+++ docs/aleph/src/config.ts
@@ -48,22 +48,34 @@ export type Sidebar = Record<
 export const SIDEBAR: Sidebar = {
 	en: {
 		'Introduction': [{ text: 'Welcome', link: 'en/introduction' },
-		{ text: 'About the project', link: 'en/about' },
-		{ text: 'Get in touch', link: 'en/contact' },
-	],
-		'User Documentation': [
-			{ text: 'Getting started', link: 'en/users/getting-started' },
-			{ text: 'The building blocks of Aleph', link: 'en/users/building-blocks-of-aleph' },
-			{ text: 'Searching your data', link: 'en/users/searching-your-data' },
-			{ text: 'Building out your investigation', link: 'en/users/building-your-investigation' },
-			{ text: 'OpenRefine reconcilliation', link: 'en/users/openrefine-reconcilliation' },
-			{ text: 'Frequently asked questions', link: 'en/users/faq' },
+			{ text: 'About the project', link: 'en/about' },
+			{ text: 'Getting started', link: 'en/guide/getting-started' },
+			{ text: 'The building blocks of Aleph', link: 'en/guide/the-basics' },
+			{ text: 'Frequently asked questions', link: 'en/guide/faq' },
+			{ text: 'Get in touch', link: 'en/contact' },
 		],
+		'Search': [
+			{ text: 'Introduction', link: 'en/guide/search' },
+			{ text: 'Executing a basic search', link: 'en/guide/search/anatomy-of-a-search' },
+			{ text: 'Advanced search techniques', link: 'en/guide/building-your-investigation' },
+			{ text: 'Filtering serach results', link: 'en/guide/openrefine-reconcilliation' },
+			{ text: 'Searching within other contexts', link: 'en/guide/faq' },
+			{ text: 'Searching for a dataset', link: 'en/guide/faq' },
+		],
+		'Investigations': [
+			{ text: 'Introduction', link: 'en/guide/investigation' },
+			{ text: 'Creating an investigation', link: 'en/guide/investigation/creating-an-investigation' },
+			{ text: 'Uploading documents', link: 'en/guide/investigation/uploading-documents' },
+			{ text: 'Network diagrams', link: 'en/guide/investigation/network-diagrams' },
+			{ text: 'Timelines', link: 'en/guide/investigations/timelines' },
+			{ text: 'Creating and editing entities', link: 'en/guide/invetigation/using-the-table-editor' },
+			{ text: 'Generating multiple entities from a spreadsheet or CSV', link: 'en/guide/investigation/generating-multiple-entities-from-a-list' },
+			{ text: 'Cross referencing', link: 'en/guide/investigation/cross-referencing' },
+		],
+
 		'Developers and Operators': [
 			{ text: 'Technical Introduction', link: 'en/page-4' },
 			{ text: 'Installing Aleph', link: 'en/page-4' },
-			{ text: 'Follow the Money', link: 'en/page-4' },
-			{ text: 'Crawling Data with Memorious', link: 'en/page-4' },
 			{ text: 'Alephclient CLI', link: 'en/page-4' },
 			{ text: 'Importing Structured Data', link: 'en/page-4' },
 			{ text: 'Mixed Docuyment/Entity Graphs', link: 'en/page-4' },
@@ -74,5 +86,11 @@ export const SIDEBAR: Sidebar = {
 			{ text: 'Developer tools', link: 'en/page-4' },
 			{ text: 'Data Commons', link: 'en/page-4' },
 		],
+		'Tools and libraries': [
+			{ text: 'Follow the Money', link: 'en/page-4' },
+			{ text: 'Crawling Data with Memorious', link: 'en/page-4' },			
+			{ text: 'Aleph data desktop', link: 'en/guide/faq' },
+			{ text: 'Openrefine Reconcilliation', link: 'en/guide/faq' },
+		]
 	},
 };
