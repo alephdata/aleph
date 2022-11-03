23d3991172665678a6db161bdf3e7ff846bca7c0
Till Prochaska <till.prochaska@occrp.org>
2022-11-03 13:16:26 +0100

    Add button to download/export bookmarks as plaintext file

--- ui/src/components/common/BookmarksDrawer.tsx
+++ ui/src/components/common/BookmarksDrawer.tsx
@@ -1,8 +1,14 @@
 import { FC } from 'react';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
-import { FormattedMessage, injectIntl } from 'react-intl';
-import { Drawer, DrawerSize } from '@blueprintjs/core';
+import downloadFile from 'js-file-download';
+import {
+  FormattedMessage,
+  injectIntl,
+  useIntl,
+  defineMessage,
+} from 'react-intl';
+import { Button, Drawer, DrawerSize } from '@blueprintjs/core';
 
 import { selectBookmarks } from 'selectors';
 import BookmarksList from './BookmarksList';
@@ -19,6 +25,45 @@ type BookmarksDrawerProps = {
   isOpen: boolean;
   toggleDialog: () => void;
   bookmarks: Array<Bookmark>;
+};
+
+type DownloadButtonProps = Button['props'] & {
+  bookmarks: Array<Bookmark>;
+};
+
+const downloadHeading = defineMessage({
+  id: 'bookmarks.download.heading',
+  defaultMessage: 'Your bookmarks',
+});
+
+const DownloadButton: FC<DownloadButtonProps> = ({
+  bookmarks,
+  ...buttonProps
+}) => {
+  const { formatDate, formatTime, formatMessage } = useIntl();
+
+  const download = () => {
+    const items = bookmarks.map(({ caption, bookmarkedAt, id }) =>
+      [
+        caption,
+        `${formatDate(bookmarkedAt)} ${formatTime(bookmarkedAt)}`,
+        new URL(`/entities/${id}`, window.location.href),
+      ].join('\n')
+    );
+
+    const heading = formatMessage(downloadHeading).toUpperCase();
+    const contents = `${heading}\n\n${items.join('\n\n')}`;
+    downloadFile(contents, 'bookmarks.txt');
+  };
+
+  return (
+    <Button onClick={download} {...buttonProps}>
+      <FormattedMessage
+        id="bookmarks.download"
+        defaultMessage="Download your bookmarks"
+      />
+    </Button>
+  );
 };
 
 const BookmarksDrawer: FC<BookmarksDrawerProps> = ({
@@ -53,6 +98,7 @@ const BookmarksDrawer: FC<BookmarksDrawerProps> = ({
             />
           </p>
         )}
+        <DownloadButton icon="archive" bookmarks={bookmarks} />
       </div>
     </Drawer>
   );
