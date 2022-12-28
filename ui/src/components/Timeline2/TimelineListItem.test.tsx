bdb4700907f8192cd100f0923a2f635971315738
Till Prochaska <till.prochaska@occrp.org>
2022-12-28 15:15:58 +0100

    Refactor: Extract keyboard navigation logic into custom hook
    This allows sharing the logic across list and chart renderers.

--- ui/src/components/Timeline2/TimelineListItem.test.tsx
+++ ui/src/components/Timeline2/TimelineListItem.test.tsx
@@ -1,6 +1,5 @@
 import { ReactNode } from 'react';
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import TimelineListItem from './TimelineListItem';
 import { Entity, Model, defaultModel } from '@alephdata/followthemoney';
 
@@ -27,7 +26,15 @@ it('renders start date', () => {
   entity.setProperty('startDate', '2022-01-01');
   entity.setProperty('startDate', '2022-01-02');
 
-  renderRow(<TimelineListItem entity={entity} color="blue" />);
+  renderRow(
+    <TimelineListItem
+      entity={entity}
+      color="blue"
+      onSelect={() => {}}
+      onRemove={() => {}}
+    />
+  );
+
   const startCell = screen.getByText('2022-01-01');
   expect(startCell).toHaveTextContent(/2022-01-01\s*Start date/);
 });
@@ -38,8 +45,15 @@ it('renders start and end date', () => {
   entity.setProperty('endDate', '2022-02-01');
 
   renderRow(
-    <TimelineListItem entity={entity} color="blue" showEndDate={true} />
+    <TimelineListItem
+      entity={entity}
+      color="blue"
+      showEndDate={true}
+      onSelect={() => {}}
+      onRemove={() => {}}
+    />
   );
+
   const startCell = screen.getByText('2022-01-01');
   const endCell = screen.getByText('2022-02-01');
   expect(startCell).toHaveTextContent(/2022-01-01\s*Start date/);
@@ -49,38 +63,29 @@ it('renders start and end date', () => {
 it('renders caption', () => {
   entity.setProperty('name', 'Money laundering workshop');
 
-  renderRow(<TimelineListItem entity={entity} color="blue" />);
+  renderRow(
+    <TimelineListItem
+      entity={entity}
+      color="blue"
+      onSelect={() => {}}
+      onRemove={() => {}}
+    />
+  );
+
   expect(screen.getByText('Money laundering workshop')).toBeInTheDocument();
 });
 
 it('sets custom color property', () => {
-  renderRow(<TimelineListItem entity={entity} color="#ff0000" />);
+  renderRow(
+    <TimelineListItem
+      entity={entity}
+      color="#ff0000"
+      onSelect={() => {}}
+      onRemove={() => {}}
+    />
+  );
+
   const node = document.querySelector('.TimelineListItem') as HTMLElement;
   const color = node.style.getPropertyValue('--timeline-item-color');
   expect(color).toEqual('#ff0000');
-});
-
-it('calls select handler on click', async () => {
-  entity.setProperty('name', 'Event title');
-  const onSelect = jest.fn();
-  renderRow(
-    <TimelineListItem entity={entity} onSelect={onSelect} color="blue" />
-  );
-
-  await userEvent.click(screen.getByText('Event title'));
-  expect(onSelect).toHaveBeenCalledTimes(1);
-});
-
-it('calls select handler by pressing enter/space', async () => {
-  entity.setProperty('name', 'Event title');
-  const onSelect = jest.fn();
-  renderRow(
-    <TimelineListItem entity={entity} onSelect={onSelect} color="blue" />
-  );
-
-  await userEvent.keyboard('{tab}{enter}');
-  expect(onSelect).toHaveBeenCalledTimes(1);
-
-  await userEvent.keyboard(' ');
-  expect(onSelect).toHaveBeenCalledTimes(2);
 });
