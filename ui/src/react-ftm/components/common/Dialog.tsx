900a70bf4f5d9a79438e3d396e429fa1c6508ea0
Till Prochaska <till.prochaska@occrp.org>
2023-01-15 23:04:54 +0100

    Prevent a double click immediately closing a dialog that has just been opened
    Closes #2637

--- ui/src/react-ftm/components/common/Dialog.tsx
+++ ui/src/react-ftm/components/common/Dialog.tsx
@@ -15,12 +15,26 @@ interface IDialogProps {
 
 export class Dialog extends React.Component<IDialogProps> {
   render() {
-    const { children, className, isProcessing, ...rest } = this.props;
+    const { children, className, isProcessing, onClose, ...rest } = this.props;
 
     return (
       <BpDialog
         className={c('Dialog', className)}
         portalClassName="dialog-portal-container"
+        onClose={onClose}
+        canOutsideClickClose={false}
+        backdropProps={{
+          onClick: (event) => {
+            // When a user double click to open a dialog, this may actually lead to the
+            // dialog being closed immediately. The first click opens the dialog, the second
+            // click target the dialog's backdrop, causing it to be closed. However, we can
+            // check whether a click event is the first or the second click using `event.detail`:
+            // https://developer.mozilla.org/en-US/docs/Web/API/UIEvent/detail
+            if (event.detail === 1) {
+              onClose();
+            }
+          },
+        }}
         {...rest}
       >
         <div className={c('Dialog__content', isProcessing)}>
