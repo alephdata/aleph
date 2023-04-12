9aa2c38c8faba420e5d2f7f236399030816acbde
Christian Stefanescu <christian.stefanescu@occrp.org>
2023-04-12 22:09:41 +0200

    Put timestamps on wait-for call

--- e2e/entrypoint.sh
+++ e2e/entrypoint.sh
@@ -2,6 +2,6 @@
 
 TIMEOUT="300"
 
-echo "Waiting for up to $TIMEOUT seconds for $BASE_URL to be rechable"
-./wait-for "$BASE_URL" --timeout="$TIMEOUT" -- echo "$BASE_URL" is up
+echo "$(date) Waiting for up to $TIMEOUT seconds for $BASE_URL to be rechable"
+./wait-for "$BASE_URL" --timeout="$TIMEOUT" -- echo "$(date) $BASE_URL" is up
 exec "$@"
