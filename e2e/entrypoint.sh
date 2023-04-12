ffc5ba38a857451cd4193de95e121146fa4b7c02
Christian Stefanescu <christian.stefanescu@occrp.org>
2023-04-12 19:12:45 +0200

    Wait up to 5 minutes for UI

--- e2e/entrypoint.sh
+++ e2e/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-TIMEOUT="120"
+TIMEOUT="300"
 
-echo "Waiting for up to $TIMEOUT settings for $BASE_URL to be rechable"
+echo "Waiting for up to $TIMEOUT seconds for $BASE_URL to be rechable"
 ./wait-for "$BASE_URL" --timeout="$TIMEOUT" -- echo "$BASE_URL" is up
 exec "$@"
