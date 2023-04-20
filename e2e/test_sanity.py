67a2c1e5cfd33524463d1795abe36b90e87cb700
Christian Stefanescu <christian.stefanescu@occrp.org>
2023-04-20 14:36:30 +0200

    Cleanup for investigations

--- e2e/test_sanity.py
+++ e2e/test_sanity.py
@@ -1,21 +1,28 @@
+import pytest
 from playwright.sync_api import Page
 
-from models import Home, Investigations, DocumentDrawer
+from models import Home, Investigation, Investigations, DocumentDrawer
 
 
-def test_ingest_odt(page: Page) -> None:
+@pytest.fixture
+def investigation(page: Page):
+    """Creates an investigation and deletes it afterwards."""
+    home = Home(page)
+    home.navigate()
+    home.sign_in()
+    investigations: Investigations = home.navigate_to_investigations()
+    investigation: Investigation = investigations.new(name="[e2e] Don't look in here")
+    yield investigation
+    investigation.navigate()
+    investigation.delete()
+
+
+def test_ingest_odt(page: Page, investigation: Investigation) -> None:
     """
     Creates an investigation, uploads an odt file and awaits it to be there.
     Searches for a word inside it, opens the file preview, looks at the extracted
     text and browses to the last page.
     """
-    investigation_name = "[e2e] Don't look in here"
-
-    home = Home(page)
-    home.navigate()
-    home.sign_in()
-    investigations: Investigations = home.navigate_to_investigations()
-    investigation = investigations.new(name=investigation_name)
     investigation.upload_document("e2e/fixtures/random.odt")
 
     drawer: DocumentDrawer = investigation.search_and_expect(
@@ -23,6 +30,3 @@ def test_ingest_odt(page: Page) -> None:
     )
     drawer.navigate_to_text_tab(page_number=14)
     drawer.close()
-
-    investigation.navigate()
-    investigation.delete()
