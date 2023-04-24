380b995d4120a8594d112f544655efac25193eef
Till Prochaska <till.prochaska@occrp.org>
2023-04-24 15:05:07 +0200

    Confirm entity set label before deletion to prevent user form accidentally deleting entity sets

--- ui/src/dialogs/EntitySetDeleteDialog/EntitySetDeleteDialog.jsx
+++ ui/src/dialogs/EntitySetDeleteDialog/EntitySetDeleteDialog.jsx
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
-import { Alert, Intent } from '@blueprintjs/core';
+import {
+  Button,
+  Dialog,
+  DialogBody,
+  Intent,
+  InputGroup,
+  FormGroup,
+} from '@blueprintjs/core';
 import { defineMessages, FormattedMessage, injectIntl } from 'react-intl';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
@@ -10,24 +17,37 @@ import { showSuccessToast } from 'app/toast';
 import getCollectionLink from 'util/getCollectionLink';
 
 const messages = defineMessages({
-  button_confirm: {
-    id: 'entityset.delete.confirm',
-    defaultMessage: 'Delete',
-  },
-  button_cancel: {
-    id: 'entityset.delete.cancel',
-    defaultMessage: 'Cancel',
-  },
   success: {
     id: 'entityset.delete.success',
     defaultMessage: 'Successfully deleted {title}',
   },
 });
 
+const typeLabels = {
+  diagram: (
+    <FormattedMessage
+      id="entityset.delete.diagram"
+      defaultMessage="network diagram"
+    />
+  ),
+  timeline: (
+    <FormattedMessage
+      id="entityset.delete.timeline"
+      defaultMessage="timeline"
+    />
+  ),
+  list: <FormattedMessage id="entityset.delete.list" defaultMessage="list" />,
+  profile: (
+    <FormattedMessage id="entityset.delete.profile" defaultMessage="profile" />
+  ),
+};
+
 class EntitySetDeleteDialog extends Component {
   constructor(props) {
     super(props);
     this.onDelete = this.onDelete.bind(this);
+
+    this.state = { confirmationValue: '' };
   }
 
   onDelete() {
@@ -44,37 +64,97 @@ class EntitySetDeleteDialog extends Component {
   }
 
   render() {
-    const { intl, entitySet } = this.props;
+    const { entitySet } = this.props;
     const { label, type } = entitySet;
 
+    const typeLabel = typeLabels[type];
+
+    const title = (
+      <FormattedMessage
+        id="collection.delete.title.investigation"
+        defaultMessage="Delete {typeLabel}"
+        values={{ typeLabel }}
+      />
+    );
+
+    const buttonLabel = (
+      <FormattedMessage
+        id="collection.delete.confirm"
+        defaultMessage="I understand the consequences. Delete this {typeLabel}."
+        values={{ typeLabel }}
+      />
+    );
+
     return (
-      <Alert
+      <Dialog
         isOpen={this.props.isOpen}
+        title={title}
         icon="trash"
-        intent={Intent.DANGER}
-        cancelButtonText={intl.formatMessage(messages.button_cancel)}
-        confirmButtonText={intl.formatMessage(messages.button_confirm)}
-        onCancel={this.props.toggleDialog}
-        onConfirm={this.onDelete}
+        onClose={this.props.toggleDialog}
       >
-        <p>
-          <strong>
-            <FormattedMessage
-              id="entityset.delete.question"
-              defaultMessage="Are you sure you want to delete {label}?"
-              values={{ label }}
-            />
-          </strong>
-        </p>
-        {type === 'profile' && (
+        <DialogBody>
           <p>
             <FormattedMessage
-              id="profile.delete.warning"
-              defaultMessage="(Deleting this profile will not delete any of the entities or entity decisions contained within it)"
+              id="entityset.delete.question"
+              defaultMessage="Are you sure you want to permanently delete {label}? This cannot be undone."
+              values={{ label: <strong>{label}</strong> }}
             />
           </p>
-        )}
-      </Alert>
+
+          {type === 'profile' && (
+            <p>
+              <FormattedMessage
+                id="profile.delete.warning"
+                defaultMessage="(Deleting this profile will not delete any of the entities or entity decisions contained within it)"
+              />
+            </p>
+          )}
+
+          <p>
+            <FormattedMessage
+              id="entityset.delete.enter_label"
+              defaultMessage="Please enter {label} to confirm:"
+              values={{ label: <strong>{label}</strong> }}
+            />
+          </p>
+
+          <form
+            onSubmit={(event) => {
+              event.preventDefault();
+              this.onDelete();
+            }}
+          >
+            <FormGroup
+              labelFor="collection-delete-confirmation"
+              label={
+                <span className="visually-hidden">
+                  <FormattedMessage
+                    id="collection.delete.collection_label"
+                    defaultMessage="Label"
+                  />
+                </span>
+              }
+            >
+              <InputGroup
+                id="collection-delete-confirmation"
+                placeholder={entitySet.label}
+                required
+                onInput={(event) =>
+                  this.setState({ confirmationValue: event.target.value })
+                }
+              />
+            </FormGroup>
+            <Button
+              type="submit"
+              intent={Intent.DANGER}
+              fill
+              disabled={this.state.confirmationValue !== entitySet.label}
+            >
+              {buttonLabel}
+            </Button>
+          </form>
+        </DialogBody>
+      </Dialog>
     );
   }
 }
