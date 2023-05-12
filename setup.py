dd2bb1e21c7771ec7588015a7c46d14bc6db8683
Christian Stefanescu <christian.stefanescu@occrp.org>
2023-05-12 10:35:49 +0200

    Bump version: 3.14.2-rc3 → 3.14.2-rc4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aleph",
-    version="3.14.2-rc3",
+    version="3.14.2-rc4",
     description="Document sifting web frontend",
     classifiers=[
         "Intended Audience :: Developers",
