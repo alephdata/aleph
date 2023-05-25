c4ff1b68c36b5532cebc7051689580de9bdfae50
Till Prochaska <till.prochaska@occrp.org>
2023-05-25 14:45:05 +0200

    Allow users to reset their API key
    Fix #1027

--- ui/src/actions/index.js
+++ ui/src/actions/index.js
@@ -1,6 +1,12 @@
 import { createAction } from 'redux-act';
 
-export { queryRoles, fetchRole, suggestRoles, updateRole } from './roleActions';
+export {
+  queryRoles,
+  fetchRole,
+  suggestRoles,
+  updateRole,
+  resetApiKey,
+} from './roleActions';
 export { createAlert, deleteAlert, queryAlerts } from './alertActions';
 export { queryNotifications } from './notificationActions';
 export { setConfigValue, dismissHint } from './configActions';
