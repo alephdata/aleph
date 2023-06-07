45a462f350ad098cc3e7926eba595949026a80de
Micah Lee <micah@micahflee.com>
2023-06-07 09:39:24 -0700

    Allow optionally running a single test file at a time

--- contrib/test.sh
+++ contrib/test.sh
@@ -3,4 +3,4 @@
 psql -c "DROP DATABASE IF EXISTS aleph_test;" $ALEPH_DATABASE_URI
 psql -c "CREATE DATABASE aleph_test;" $ALEPH_DATABASE_URI
 
-nosetests --with-coverage --cover-package=aleph --cover-erase
+nosetests --with-coverage --cover-package=aleph --cover-erase $@
