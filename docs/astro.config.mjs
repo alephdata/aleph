67560577c561d0f854794e6e9d7fdb3d03d761f6
Till Prochaska <till.prochaska@occrp.org>
2023-07-24 15:22:10 +0200

    New user guide (#3223)
    * Remove old user guide
    
    * Add new user guide contents
    
    * Optimize image sizes and dimensions
    
    * Update homepage teaser wording
    
    * Fix annotation positions
    
    * Update video tutorial style to increase contrast between video and intro text
    
    * Exclude some terms from auto translatiosn
    
    * Add Portugese translation
    
    * Fix bad links
    
    * Remove changelog from docs
    
    This was a ghost page (i.e. it was not linked anywhere) and we have been publishing a changelog as part of the repo for some time now.
    
    * Remove unused layout
    
    * Temporarily remove link to timelines article as it hasn’t been written yet

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,6 +1,20 @@
+import path from 'node:path';
+import url from 'node:url';
 import { defineConfig } from 'astro/config';
-import theme from 'astro-theme-docs';
+import theme, { injectComponent } from 'astro-theme-docs';
+
+const root = path.dirname(url.fileURLToPath(import.meta.url));
+const base = path.join(root, 'src/components');
 
 export default defineConfig({
-  integrations: [theme()],
+  integrations: [
+    theme({
+      mdxPlugins: [
+        injectComponent('Annotations', path.join(base, 'Annotations.astro'), 'default'),
+        injectComponent('Annotation', path.join(base, 'Annotation.astro'), 'default'),
+        injectComponent('AnnotationRef', path.join(base, 'AnnotationRef.astro'), 'default'),
+        injectComponent('VideoTutorial', path.join(base, 'VideoTutorial.astro'), 'default'),
+      ],
+    }),
+  ],
 });
