a43d89c45c3cd595d8ede321e5f749ac72c4ebf4
Till Prochaska <till.prochaska@occrp.org>
2023-07-26 16:57:42 +0200

    Add redirects for old user guide links (#3229)
    * Upgrade to Astro 2
    
    * Add redirects for old user guide links

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -7,9 +7,32 @@ const root = path.dirname(url.fileURLToPath(import.meta.url));
 const base = path.join(root, 'src/components');
 
 export default defineConfig({
+  experimental: {
+    redirects: true,
+  },
+  redirects: {
+    '/how-aleph-is-used': '/about',
+    '/guide': '/users',
+    '/guide/getting-started': '/users',
+    '/guide/the-basics': '/users/getting-started/key-terms/',
+    '/guide/search': '/users/search/basics/',
+    '/guide/search/anatomy-of-a-search': '/users/search/basics',
+    '/guide/search/advanced-search-methods': '/users/search/advanced',
+    '/guide/search/filtering-your-search-results': '/users/search/basics#filtering-results',
+    '/guide/search/searching-within-other-contexts': '/users/search/datasets',
+    '/guide/search/searching-for-a-dataset': '/users/search/datasets',
+    '/guide/building-out-your-investigation': '/users/investigations/overview',
+    '/guide/building-out-your-investigation/creating-an-investigation': '/users/investigations/create',
+    '/guide/building-out-your-investigation/uploading-documents': '/users/investigations/uploading-documents',
+    '/guide/building-out-your-investigation/network-diagrams': '/users/investigations/network-diagrams',
+    '/guide/building-out-your-investigation/using-the-table-editor': '/users/investigations/entity-editor',
+    '/guide/building-out-your-investigation/generating-multiple-entities-from-a-list': '/users/investigations/cross-referencing#generating-entities-from-yourspreadsheet',
+    '/guide/building-out-your-investigation/cross-referencing': '/users/investigations/cross-referencing',
+    '/guide/reconciliation': '/users/advanced/reconciliation',
+  },
   integrations: [
     theme({
-      mdxPlugins: [
+      remarkPlugins: [
         injectComponent('Annotations', path.join(base, 'Annotations.astro'), 'default'),
         injectComponent('Annotation', path.join(base, 'Annotation.astro'), 'default'),
         injectComponent('AnnotationRef', path.join(base, 'AnnotationRef.astro'), 'default'),
