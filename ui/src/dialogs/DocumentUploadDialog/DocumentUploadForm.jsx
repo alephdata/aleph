5aa767d1d7aa39bfedfec9fb87a20c2db2897941
Christian Stefanescu <christian.stefanescu@occrp.org>
2023-11-07 16:57:00 +0100

    Suggest alephclient for uploading lots of files instead of UI
    Relates to #3403

--- ui/src/dialogs/DocumentUploadDialog/DocumentUploadForm.jsx
+++ ui/src/dialogs/DocumentUploadDialog/DocumentUploadForm.jsx
@@ -86,7 +86,18 @@ export class DocumentUploadForm extends PureComponent {
             }}
           />
         </p>
-      </div>
+        <p className="DocumentUploadForm__info">
+          <FormattedMessage
+            id="document.upload.info"
+            defaultMessage="If you need to upload a large amount of files (100+) consider {link}."
+            values={{
+              link: (
+                <a href="https://docs.aleph.occrp.org/developers/alephclient/#importing-all-files-from-a-directory">alephclient</a>
+              ),
+            }}
+          />
+        </p>
+      </div >
     );
   }
 }
