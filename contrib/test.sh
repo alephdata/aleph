4d7fed027c58faf46e252da299be9f481627f6e9
Christian Stefanescu <217554+stchris@users.noreply.github.com>
2024-04-04 14:56:42 +0200

    Merge pull request #3673 from alephdata/feature/pytest
    Use pytest instead of 👃

--- contrib/test.sh
+++ contrib/test.sh
@@ -3,4 +3,4 @@
 psql -c "DROP DATABASE IF EXISTS aleph_test;" $ALEPH_DATABASE_URI
 psql -c "CREATE DATABASE aleph_test;" $ALEPH_DATABASE_URI
 
-nosetests --with-coverage --cover-package=aleph --cover-erase $@
+pytest aleph/ --cov=aleph --cov-report html --cov-report term $@
