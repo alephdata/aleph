87ce636641f3c47ba12f3271a5e1b7b05b788e39
Till Prochaska <1512805+tillprochaska@users.noreply.github.com>
2024-05-06 10:32:18 +0200

    Add CLI command to reset API key expiration of non-expiring API keys

--- aleph/manage.py
+++ aleph/manage.py
@@ -20,6 +20,7 @@ from aleph.worker import get_worker
 from aleph.queues import get_status, cancel_queue
 from aleph.queues import get_active_dataset_status
 from aleph.index.admin import delete_index
+from aleph.logic.api_keys import reset_api_key_expiration as _reset_api_key_expiration
 from aleph.index.entities import iter_proxies
 from aleph.index.util import AlephOperationalException
 from aleph.logic.collections import create_collection, update_collection
@@ -566,3 +567,9 @@ def evilshit():
     delete_index()
     destroy_db()
     upgrade()
+
+
+@cli.command()
+def reset_api_key_expiration():
+    """Reset the expiration date of all legacy, non-expiring API keys."""
+    _reset_api_key_expiration()
