a63d2089b89f4cda61e8b9ca9367dc47c2aa57ce
Till Prochaska <1512805+tillprochaska@users.noreply.github.com>
2024-05-27 17:41:19 +0200

    Bump version: 3.17.0-rc2 → 3.17.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aleph",
-    version="3.17.0-rc2",
+    version="3.17.0",
     description="Document sifting web frontend",
     classifiers=[
         "Intended Audience :: Developers",
