4fff38cc50113fb42be80edd10347ecedf20e3f3
Alex Stefanescu <alex.stefanescu@protonmail.com>
2024-05-30 19:08:22 +0200

    Bump version: 4.0.0-rc11 → 4.0.0-rc12

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aleph",
-    version="4.0.0-rc11",
+    version="4.0.0-rc12",
     description="Document sifting web frontend",
     classifiers=[
         "Intended Audience :: Developers",
