36aa43f0f557f80904bdedcf2ede803b97f4b31b
Till Prochaska <1512805+tillprochaska@users.noreply.github.com>
2024-08-13 23:08:49 +0200

    Test

--- contrib/test.sh
+++ contrib/test.sh
@@ -3,4 +3,4 @@
 psql -c "DROP DATABASE IF EXISTS aleph_test;" $ALEPH_DATABASE_URI
 psql -c "CREATE DATABASE aleph_test;" $ALEPH_DATABASE_URI
 
-pytest aleph/ --cov=aleph --cov-report html --cov-report term $@
+pytest aleph/tests/test_entities_api.py -k entity_tags --cov=aleph --cov-report html --cov-report term $@
