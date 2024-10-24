7b89c2c53f72b3390c0ebcb31fa6ba8705fb849e
Christian Stefanescu <christian.stefanescu@occrp.org>
2024-10-24 09:26:58 +0200

    feat: run tests in dev mode

--- contrib/test.sh
+++ contrib/test.sh
@@ -3,4 +3,4 @@
 psql -c "DROP DATABASE IF EXISTS aleph_test;" $ALEPH_DATABASE_URI
 psql -c "CREATE DATABASE aleph_test;" $ALEPH_DATABASE_URI
 
-pytest aleph/ --cov=aleph --cov-report html --cov-report term $@
+PYTHONDEVMODE=1 PYTHONTRACEMALLOC=1 pytest aleph/ --cov=aleph --cov-report html --cov-report term $@
