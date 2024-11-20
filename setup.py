9b648bbeaa0412f2c54eb5aa5023a7f51a1da2e1
Till Prochaska <1512805+tillprochaska@users.noreply.github.com>
2024-11-20 17:52:56 +0100

    Bump version: 4.1.0-rc1 → 4.1.0-rc2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aleph",
-    version="4.1.0-rc1",
+    version="4.1.0-rc2",
     description="Document sifting web frontend",
     classifiers=[
         "Intended Audience :: Developers",
