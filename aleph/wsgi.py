7e7fec7d6c167343c94966cf0b96e14f28d82989
Christian Stefanescu <christian.stefanescu@occrp.org>
2025-02-13 08:35:53 +0100

    log when aleph inits the sentry sdk

--- aleph/wsgi.py
+++ aleph/wsgi.py
@@ -5,6 +5,7 @@ from aleph import __version__ as aleph_version
 import sentry_sdk
 from sentry_sdk.integrations.flask import FlaskIntegration
 
+log = logging.getLogger(__name__)
 
 if SETTINGS.SENTRY_DSN:
     sentry_sdk.init(
@@ -17,4 +18,5 @@ if SETTINGS.SENTRY_DSN:
         environment=SETTINGS.SENTRY_ENVIRONMENT,
         send_default_pii=False,
     )
+log.info("aleph.wsgi initialized Sentry SDK")
 app = create_app()
