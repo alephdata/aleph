acd5a6fbf0c8c33247a2c046b40c2aec0fd54a48
Till Prochaska <1512805+tillprochaska@users.noreply.github.com>
2025-05-05 22:37:38 +0200

    Remove intl polyfills
    Any fairly recent browser version should support these natively nowadays.
    Also the dynamic imports caused problems when migrating from Webpack to
    Vite.

--- ui/src/app/Translator.jsx
+++ ui/src/app/Translator.jsx
@@ -1,53 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { IntlProvider } from 'react-intl';
-import { shouldPolyfill } from '@formatjs/intl-relativetimeformat/should-polyfill';
-import { Classes } from '@blueprintjs/core';
 
 import { selectLocale } from '/src/selectors';
-import { SectionLoading } from '/src/components/common/index.jsx';
 import translations from '/src/content/translations.json';
 
 class Translator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.requiresPolyfill = shouldPolyfill();
-    this.state = { isPending: this.requiresPolyfill };
-  }
-
-  componentDidMount() {
-    if (this.requiresPolyfill) {
-      this.fetchPolyfill();
-    }
-  }
-
-  componentDidUpdate(prevProps) {
-    if (this.requiresPolyfill && prevProps.locale !== this.props.locale) {
-      this.fetchLocale();
-    }
-  }
-
-  fetchPolyfill = async () => {
-    await import('@formatjs/intl-locale/polyfill');
-    await import('@formatjs/intl-pluralrules/polyfill');
-    await import('@formatjs/intl-relativetimeformat/polyfill');
-    this.fetchLocale();
-  };
-
-  fetchLocale = async () => {
-    const { locale } = this.props;
-    await import(`@formatjs/intl-pluralrules/locale-data/${locale}`);
-    await import(`@formatjs/intl-relativetimeformat/locale-data/${locale}`);
-    this.setState({ isPending: false });
-  };
-
   render() {
     const { children, locale } = this.props;
-    const { isPending } = this.state;
-
-    if (isPending) {
-      return <SectionLoading className={Classes.LARGE} />;
-    }
 
     //  override arabic locale to marocan version
     // We want all dates and numbers in latin instead of default ar eastern digits
